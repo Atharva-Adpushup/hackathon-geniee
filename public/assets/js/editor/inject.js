6d3d4d038a224bffef99299a5bb9e4a462b397ab
Dhiraj Singh <dhiraj@adpushup.com>
2016-10-15 23:54:10 +0530

    Removed Old Editor

--- public/assets/js/editor/inject.js
+++
@@ -1,8 +0,0 @@
-var InnerController = require("./innerController");
-var CommonConsts = require("./commonConsts");
-
-
-module.exports = (function (Controller, CommonConsts) {
-    window.ADP = CommonConsts;
-    var controller = new Controller();
-})( InnerController, CommonConsts );
