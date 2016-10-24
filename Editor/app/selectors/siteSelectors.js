8416b40b49a10582c7970730bd8acc84f174b43d
Dhiraj Singh <dhiraj@adpushup.com>
2016-10-24 19:45:10 +0530

    `getFinalJson` function which denormalize the whole data is implemented.

--- Editor/app/selectors/siteSelectors.js
+++ Editor/app/selectors/siteSelectors.js
@@ -1,3 +1,22 @@
-const getAfterSaveLoaderState = (state) => state.site.afterSaveLoader.status;
+import { createSelector } from 'reselect';
+import _ from 'lodash';
+import { getAllChannels } from './channelSelectors';
+import { getAllVariations } from './variationSelectors';
+import { getAllSections } from './sectionSelectors';
+import { getAllAds } from './adsSelectors';
 
-export { getAfterSaveLoaderState };
+const getAfterSaveLoaderState = (state) => state.site.afterSaveLoader.status,
+
+	getFinalJson = createSelector([getAllChannels, getAllVariations, getAllSections, getAllAds], (allChannels = {}, allVariations = {}, allSections = {}, allAds = {}) => (
+		_.map(allChannels, (channel) => {
+			const channelVariations = _.map(channel.variations, (variationId) => allVariations[variationId]);
+			return {
+				...channel,
+				variations: _.map(channelVariations, (variation) => {
+					const sections = _.map(variation.sections, (sectionId) => allSections[sectionId]);
+					return { ...variation, sections: _.map(sections, (section) => ({ ...section, ads: _.map(section.ads, (sectionAdId) => allAds[sectionAdId]) })) };
+				}) };
+		})
+	));
+
+export { getAfterSaveLoaderState, getFinalJson };
