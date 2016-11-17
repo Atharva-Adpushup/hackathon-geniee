bc42eeddf0465ce686a1a604338b5ce93db441bc
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-11-17 13:22:01 +0530

    Gitignore updated

--- app.js
+++ app.js
@@ -77,7 +77,6 @@ couchBaseService.connectToAppBucket().then(function () {
 
 	// Setting template local variables for jade
 	app.use(function (req, res, next) {
-		app.locals.siteId = 123;
 		app.locals.isSuperUser = (req.session.isSuperUser) ? true : false;
 		app.locals.usersList = (req.session.usersList) ? req.session.usersList : [];
 		app.locals.environment = config.development.HOST_ENV;
