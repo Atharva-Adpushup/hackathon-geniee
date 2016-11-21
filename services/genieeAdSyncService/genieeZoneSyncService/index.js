173e997ad380df8273ced396efe8a0786ae62457
Dhiraj Singh <dhiraj@adpushup.com>
2016-11-21 23:21:35 +0530

    [Enhancement] Removed `oauth-nonce` and `oauth-signature` package, implemented custom signature generator to support geniee compatible oauth signatures.

--- services/genieeAdSyncService/genieeZoneSyncService/index.js
+++ services/genieeAdSyncService/genieeZoneSyncService/index.js
@@ -3,10 +3,9 @@ var config = require('../../../configs/config'),
 	Promise = require('bluebird'),
 	_ = require('lodash'),
 	rp = require('request-promise'),
-	ouathNonce = require('oauth_nonce'),
 	processBatches = require('process-batches'),
-	oauthSignature = require('oauth-signature'),
-	crypto = require('crypto-js'),
+	signatureGenerator = require('./signatureGenerator'),
+	crypto = require('crypto'),
 	retry = require('bluebird-retry');
 
 module.exports = function (site) {
@@ -39,6 +38,9 @@ module.exports = function (site) {
 				return finalZones;
 			});
 		},
+		getNonce = function () {
+			return crypto.randomBytes(32).toString('base64').replace(/[^\w]/g, '');
+		},
 		syncChannelZones = function (pageGroupId, zonesToSync) {
 			var zones = _.map(zonesToSync, function (zone) {
 				return {
@@ -48,36 +50,25 @@ module.exports = function (site) {
 				}
 			});
 			return processBatches.fromArray(zones, 5, function (batch) {
-				var json = {
-					pageGroupId: 52,
-					zones: JSON.stringify([
-						{ zoneName: "test zone api1", sizeWidth: 300, sizeHeight: 250, zoneType: 1, zonePosition: 0, firstView: 1, useFriendlyIFrameFlag: 0 },
-						{ zoneName: "test zone api2", sizeWidth: 300, sizeHeight: 250, zoneType: 1, zonePosition: 0, firstView: 1, useFriendlyIFrameFlag: 0 }
-					])
-				}, httpMethod = 'POST',
+				var json = { "pageGroupId": 52, "zones": [{ "zoneName": "test zone api0", "sizeWidth": 300, "sizeHeight": 250, "zoneType": 1, "zonePosition": 0, "firstView": 1, "useFriendlyIFrameFlag": 0 }, { "zoneName": "test zone api1", "sizeWidth": 300, "sizeHeight": 250, "zoneType": 1, "zonePosition": 0, "firstView": 1, "useFriendlyIFrameFlag": 0 }] },
+					httpMethod = 'POST',
 					url = 'https://beta-aladdin.geniee.jp/beta2/aladdin/adpushup/zone/create/',
-					nounce = "57ff01d3e4c68"/*ouathNonce()*/,
+					nounce = "57ff01d3e4c68"/*getNonce()*/,
 					parameters = {
 						oauth_consumer_key: "NDJiOGRmYTJmMGVhMzU1ZQ==",
-						oauth_nonce: '57ff01d3e4c68',
+						oauth_nonce: nounce,
 						oauth_signature_method: "HMAC-SHA1",
 						oauth_timestamp: "1476329939",
-						oauth_version: "1.0"
+						oauth_version: "1.0",
+						pageGroupId: json.pageGroupId,
+						zones: json.zones
 					},
-					consumerSecret = 'MDc0N2MzMDYzYTQ2NDk5MDUzNzQ0YjIwMTJkY2UzZDA=';
-				var signatureBaseString = new oauthSignature.SignatureBaseString(httpMethod, url, _.extend(parameters, json)).generate(),
-					signature = crypto.HmacSHA1(signatureBaseString, consumerSecret + '&').toString(crypto.enc.Base64);
+					consumerSecret = 'MDc0N2MzMDYzYTQ2NDk5MDUzNzQ0YjIwMTJkY2UzZDA=',
+					signature = signatureGenerator(httpMethod, url, parameters, consumerSecret);
 
-				baseString = 'POST&http%3A%2F%2Fbeta-aladdin.geniee.jp%2Fbeta2%2Faladdin%2Fadpushup%2Fzone%2Fcreate%2F&oauth_consumer_key%3DNDJiOGRmYTJmMGVhMzU1ZQ%3D%3D%26oauth_nonce%3D57ff01d3e4c68%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1476329939%26oauth_version%3D1.0%26pageGroupId%3D52%26zones%3D%5B%7B%22zoneName%22%3A%22test%20zone%20api0%22%2C%22sizeWidth%22%3A300%2C%22sizeHeight%22%3A250%2C%22zoneType%22%3A1%2C%22zonePosition%22%3A0%2C%22firstView%22%3A1%2C%22useFriendlyIFrameFlag%22%3A0%7D%2C%7B%22zoneName%22%3A%22test%20zone%20api1%22%2C%22sizeWidth%22%3A300%2C%22sizeHeight%22%3A250%2C%22zoneType%22%3A1%2C%22zonePosition%22%3A0%2C%22firstView%22%3A1%2C%22useFriendlyIFrameFlag%22%3A0%7D%5D';
-
-				signature = crypto.HmacSHA1(baseString, consumerSecret + '&').toString(crypto.enc.Base64);
-				//signature = "ew8j+JzW3BUkx/0hzFvumEpIVOE="
-
-
-				//json = { "pageGroupId": 52, "zones": [{ "zoneName": "test zone api0", "sizeWidth": 300, "sizeHeight": 250, "zoneType": 1, "zonePosition": 0, "firstView": 1, "useFriendlyIFrameFlag": 0 }, { "zoneName": "test zone api1", "sizeWidth": 300, "sizeHeight": 250, "zoneType": 1, "zonePosition": 0, "firstView": 1, "useFriendlyIFrameFlag": 0 }] };
 				return rp({
-					method: 'POST',
-					uri: 'https://beta-aladdin.geniee.jp/beta2/aladdin/adpushup/zone/create/',
+					method: httpMethod,
+					uri: url,
 					body: json,
 					json: true,
 					headers: {
---
+++ services/genieeAdSyncService/genieeZoneSyncService/signatureGenerator.js
@@ -0,0 +1,28 @@
+var _ = require('lodash'),
+    cryptoJs = require('crypto-js'),
+    encode = function(data) {
+        // using implementation from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent?redirectlocale=en-US&redirectslug=JavaScript%2FReference%2FGlobal_Objects%2FencodeURIComponent
+        return encodeURIComponent(data)
+            .replace(/[!'()]/g, escape)
+            .replace(/\*/g, "%2A");
+    },
+    stringFromParams = function(params) {
+        params = _.map(params, function(val, key) {
+            if (typeof val == 'string') {
+                return (key + '=' + val);
+            } else {
+                return key + '=' + JSON.stringify(val)
+            }
+        });
+        params.sort(function(a, b) {
+            return a.localeCompare(b);
+        });
+        return params.join('&');
+    };
+
+module.exports = function(httpMethod, url, parameters, key) {
+    httpMethod = encode(httpMethod);
+    url = encode(url);
+    parameters = encode(stringFromParams(parameters));
+    return cryptoJs.HmacSHA1([httpMethod, url, parameters].join('&'), key + '&').toString(cryptoJs.enc.Base64)
+}
