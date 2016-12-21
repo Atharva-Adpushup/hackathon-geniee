b804c72cfb597a2e09834d5229f49eebda3579df
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-12-21 18:19:16 +0530

    Init code panel added in global settings

--- public/assets/js/libs/custom/settings.js
+++ public/assets/js/libs/custom/settings.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
-    (function (w) {
-        var autoOptimise;
-        $('#autoOptimise').on('change', function() {
+    (function (w, d) {
+        var autoOptimise,
+            headerCode = "(function(w, d) { var s = d.createElement('script'); s.src = '//cdn.adpushup.com/" + w.selectedSiteId + "/adpushup.js'; s.type = 'text/javascript'; s.async = true; (d.getElementsByTagName('head')[0] || d.getElementsByTagName('body')[0]).appendChild(s); })(window, document);";
+        $('#header-code').text('<script data-cfasync="false" type="text/javascript">' + headerCode + '</script>');
+
+        $('#autoOptimise').on('change', function () {
             autoOptimise = $(this).prop('checked');
         });
 
         function getFormData(values, type) {
-            switch(type) {
+            switch (type) {
                 case 'pageGroups':
                     var pageGroups = [];
 
-                    for(var i=0; i<values.length; i+=2) {
+                    for (var i = 0; i < values.length; i += 2) {
                         var json = {};
-                        if(values[i].name === 'pageGroupPattern') {
-                            json[values[i].value] = values[i+1].value;
+                        if (values[i].name === 'pageGroupPattern') {
+                            json[values[i].value] = values[i + 1].value;
                             pageGroups.push(json);
                         }
                     }
@@ -21,15 +24,43 @@ $(document).ready(function () {
                     return pageGroups;
                 case 'other':
                     var otherSettings = {};
-                    for(var i=0; i<values.length; i++) {
-                        if(values[i].name !== 'pageGroupPattern') {
-                           otherSettings[values[i].name] = values[i].value;
+                    for (var i = 0; i < values.length; i++) {
+                        if (values[i].name !== 'pageGroupPattern') {
+                            otherSettings[values[i].name] = values[i].value;
                         }
                     }
-                    
+
                     return otherSettings;
             };
         };
+
+        $(d).on('click', '#clipboardCopy, #header-code', function () {
+            $('.clipboard-copy').fadeIn();
+            setTimeout(function () {
+                $('.clipboard-copy').fadeOut();
+            }, 1500);
+            $('#header-code').select();
+            d.execCommand('copy');
+        });
+
+        $('#sendCodeForm').submit(function (e) {
+            e.preventDefault();
+            $('#headerCodeInput').val($('#header-code').val());
+            var data = $(this).serialize();
+
+            $('#sendCodeSubmit').prop('disabled', true).html('Sending...');
+            $.post('/user/sendCode', data, function (res) {
+                if (res.success) {
+                    $('#sendCodeSubmit').css('opacity', 1).html('Code sent successfully!');
+                    setTimeout(function () {
+                        $('#sendToDevModal').modal('toggle');
+                        $('#sendCodeSubmit').prop('disabled', false).html('Send');
+                    }, 2000);
+                } else {
+                    alert('Some error occurred!');
+                }
+            });
+        });
 
         $('#saveSiteSettings').on('submit', function (e) {
             e.preventDefault();
@@ -50,5 +81,5 @@ $(document).ready(function () {
                 }
             });
         });
-    })(window);
+    })(window, document);
 });
