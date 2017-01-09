f10c214864504cfaacf44e731de39e9616e9bb26
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-01-10 00:07:39 +0530

    Adbox size styles added to overwrite.scss

--- Editor/app/components/inner/adBox.jsx
+++ Editor/app/components/inner/adBox.jsx
@@ -4,19 +4,6 @@ import Utils from 'libs/utils';
 import _ from 'lodash';
 
 const highLighterClass = '_APD_highlighter',
-	adBoxSizeStyles = {
-		background: '#eb575c !important',
-		borderRadius: '3px !important',
-		color: '#fff !important',
-		fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif !important",
-		fontSize: '12px !important',
-		fontWeight: '400 !important',
-		left: '5px !important',
-		padding: '4px !important',
-		position: 'absolute !important',
-		top: '5px !important',
-		pointerEvents: 'none'
-	},
 	listStyle = {
 		position: 'absolute',
 		width: '100%',
@@ -50,10 +37,7 @@ const highLighterClass = '_APD_highlighter',
 		return (
 			<div style={listStyle}>
 				<div id={`ad-${id}`} className={highLighterClass} onClick={clickHandler} style={adBoxStyles}>
-					<div className="_AP_adSize _ap_reject" ref={(node) => {
-						if (node) {
-							node.style.setProperty("pointerEvents", "none", "important");
-					}}}>
+					<div className="_AP_adSize _ap_reject _ap_size_label">
 						{adBoxSizeContent}
 					</div>
 				</div>
