fe5267ad75c067a3918ed8e61f47cf5f41af9496
ZahinAdPushup <zahin@adpushup.com>
2017-01-16 20:07:04 +0530

    [Code changes] (Backend: Service) Geniee Reports: Code refactoring

---
+++ reports/partners/geniee/modules/utils/index.js
@@ -0,0 +1,50 @@
+var _ = require('lodash'),
+	extend = require('extend');
+
+module.exports = {
+	setHighChartsData: function(currentDate, metric, mainObj, computedObj) {
+		var collectionIndex = -1,
+			collectionDataIndex = -1, computedItem;
+
+		_.forEach(mainObj[metric], function(metricObj, index) {
+			if (metricObj.name == computedObj[metric].name) {
+				collectionIndex = index;
+			}
+		});
+
+		if (collectionIndex > -1) {
+			computedItem = mainObj[metric][collectionIndex];
+
+			_.forEach(computedItem.data, function(dataArr, idx) {
+				if (dataArr.indexOf(currentDate) > -1) {
+					collectionDataIndex = idx;
+				}
+			});
+
+			if (collectionDataIndex > -1) {
+				mainObj[metric][collectionIndex].data[collectionDataIndex][1] += computedObj[metric].data[0][1];
+			} else {
+				mainObj[metric][collectionIndex].data.push(computedObj[metric].data[0]);
+			}
+		} else {
+			mainObj[metric].push(extend(true, {}, computedObj[metric]));
+		}
+	},
+	setDateWithEmptyValue: function(date, metric, mainObj) {
+		var numericDate = Number(date);
+
+		_.forEach(mainObj[metric], function(metricObj, index) {
+			var computedDateIndex = -1;
+
+			_.forEach(metricObj.data, function(dataArr, dateIndex) {
+				if (dataArr.indexOf(numericDate) > -1) {
+					computedDateIndex = dateIndex;
+				}
+			});
+
+			if (computedDateIndex == -1) {
+				mainObj[metric][index].data.push([numericDate, 0]);
+			}
+		});
+	}
+};
---
+++ reports/partners/geniee/modules/variation/index.js
@@ -0,0 +1,158 @@
+var _ = require('lodash'),
+	extend = require('extend'),
+	moment = require('moment'),
+	Promise = require('bluebird'),
+	utils = require('../utils/index');
+
+module.exports = {
+	setVariationMetrics: function(pageGroupData) {
+		var computedData = extend(true, {}, pageGroupData);
+
+		_.forOwn(pageGroupData, function(pageGroupObj, pageGroupKey) {
+			_.forOwn(pageGroupObj.variationData, function(variationObj, variationKey) {
+				computedData[pageGroupKey].variationData[variationKey] = extend(true, {}, variationObj, { 'click': 0, 'impression': 0, 'revenue': 0.0, 'ctr': 0.0, "pageViews": 0, "pageRPM": 0, "pageCTR": 0 });
+
+				_.forEach(variationObj.zones, function(zoneObj) {
+					computedData[pageGroupKey].variationData[variationKey].click += Number(zoneObj.click);
+					computedData[pageGroupKey].variationData[variationKey].impression += Number(zoneObj.impression);
+					computedData[pageGroupKey].variationData[variationKey].revenue += Number(zoneObj.revenue);
+					computedData[pageGroupKey].variationData[variationKey].ctr += Number(zoneObj.ctr);
+
+					computedData[pageGroupKey].variationData[variationKey].revenue = Math.round(computedData[pageGroupKey].variationData[variationKey].revenue);
+					computedData[pageGroupKey].variationData[variationKey].ctr = Number(computedData[pageGroupKey].variationData[variationKey].ctr.toFixed(2));
+				});
+			});
+		});
+		
+		return computedData;
+	},
+	removeRedundantVariationsObj: function(pageGroupData) {
+		var computedData = extend(true, {}, pageGroupData);
+
+		_.forOwn(computedData, function(pageGroupObj, pageGroupKey) {
+			computedData[pageGroupKey].variations = extend(true, {}, computedData[pageGroupKey].variationData);
+			delete computedData[pageGroupKey].variationData;
+		});
+
+		return computedData;
+	},
+	setVariationsHighChartsData: function(pageGroupData) {
+		var self = this,
+            computedData = extend(true, {}, pageGroupData),
+			highChartsData = {
+				highCharts: {
+					revenue: [],
+					pageviews: [],
+					clicks: [],
+					pagerpm: [],
+					pagectr: []
+				}
+			},
+			datesObj = {
+				revenue: {},
+				pageviews: {},
+				clicks: {},
+				pagerpm: {},
+				pagectr: {}
+			},
+			currentComputedObj = {}, currentDate;
+
+		_.forOwn(computedData, function(pageGroupObj, pageGroupKey) {
+			_.forOwn(pageGroupObj.variations, function(variationObj, variationKey) {
+				_.forEach(variationObj.zones, function(zonesObj) {
+					currentDate = moment(zonesObj.date).valueOf();
+
+					currentComputedObj.revenue = {
+						name: (variationObj.name.replace(" ", "-")),
+						data: [[currentDate, Number(zonesObj.revenue)]]
+					};
+					datesObj.revenue[currentDate] = currentComputedObj.revenue.name;
+					
+					// currentComputedObj.pageviews = {
+					// 	name: (pageGroupObj.pageGroup + '-' + pageGroupObj.device),
+					// 	data: [[currentDate, 0]],
+					// 	tooltip: {valueDecimals: 2}
+					// };
+					currentComputedObj.clicks = {
+						name: (variationObj.name.replace(" ", "-")),
+						data: [[currentDate, Number(zonesObj.click)]]
+					};
+					datesObj.clicks[currentDate] = currentComputedObj.clicks.name;
+					// currentComputedObj.pagerpm = {
+					// 	name: (pageGroupObj.pageGroup + '-' + pageGroupObj.device),
+					// 	data: [[currentDate, 0]],
+					// 	tooltip: {valueDecimals: 2}
+					// };
+					// currentComputedObj.pagectr = {
+					// 	name: (pageGroupObj.pageGroup + '-' + pageGroupObj.device),
+					// 	data: [[currentDate, 0]],
+					// 	tooltip: {valueDecimals: 2}
+					// };
+
+					utils.setHighChartsData(currentDate, 'revenue', highChartsData.highCharts, currentComputedObj);
+					utils.setHighChartsData(currentDate, 'clicks', highChartsData.highCharts, currentComputedObj);
+				});
+			});
+
+			// Add date with empty values
+			_.forOwn(datesObj.revenue, function(revenueData, dateKey) {
+				utils.setDateWithEmptyValue(dateKey, 'revenue', highChartsData.highCharts);
+			});
+
+			_.forOwn(datesObj.clicks, function(clicksData, dateKey) {
+				utils.setDateWithEmptyValue(dateKey, 'clicks', highChartsData.highCharts);
+			});
+
+			computedData[pageGroupKey].variations.data = extend(true, computedData[pageGroupKey].variations.data, highChartsData);
+		});
+			
+		return Promise.resolve(computedData);
+	},
+	setVariationsTabularData: function(pageGroupData) {
+		var computedData = extend(true, {}, pageGroupData),
+			variationsTabularData = {
+				table: {
+					header: ['NAME', 'TRAFFIC DISTRIBUTION', 'REVENUE', 'IMPRESSIONS', 'PAGE VIEWS', 'CLICKS', 'PAGE RPM', 'PAGE CTR', 'REVENUE CONTRIBUTION (%)'],
+					rows: [],
+					footer: ['TOTAL', 0, 0, 0, 0, 0, 0, 0, 0]
+				}
+			};
+
+		_.forOwn(computedData, function(pageGroupObj, pageGroupKey) {
+			_.forOwn(pageGroupObj.variations, function(variationObj, variationKey) {
+				var rowItem = [];
+
+				rowItem[0] = variationObj.name;
+				rowItem[1] = variationObj.trafficDistribution;
+				variationsTabularData.table.footer[1] += Number(variationObj.trafficDistribution);
+
+				rowItem[2] = variationObj.revenue;
+				variationsTabularData.table.footer[2] += Number(variationObj.revenue);
+				
+				rowItem[3] = variationObj.impression;
+				variationsTabularData.table.footer[3] += Number(variationObj.impression);
+				
+				rowItem[4] = variationObj.pageViews;
+				variationsTabularData.table.footer[4] += Number(variationObj.pageViews);
+				
+				rowItem[5] = variationObj.click;
+				variationsTabularData.table.footer[5] += Number(variationObj.click);
+				
+				rowItem[6] = variationObj.pageRPM;
+				variationsTabularData.table.footer[6] += Number(variationObj.pageRPM);
+				
+				rowItem[7] = variationObj.pageCTR;
+				variationsTabularData.table.footer[7] += Number(variationObj.pageCTR);
+				
+				rowItem[8] = Math.floor((variationObj.revenue / pageGroupObj.revenue) * 100);
+				variationsTabularData.table.footer[8] += Number(rowItem[8]);
+
+				variationsTabularData.table.rows.push(rowItem);
+			});
+
+			computedData[pageGroupKey].variations.data = extend(true, {}, variationsTabularData);
+		});
+
+		return computedData;
+	}
+};
