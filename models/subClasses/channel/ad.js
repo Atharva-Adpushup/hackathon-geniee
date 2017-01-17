8098b275c65abe8993c0b885d1e13fbab8589eb5
ZahinAdPushup <zahin@adpushup.com>
2017-01-17 15:24:11 +0530

    [Code Changes] (Backend: Framework) Sub-classes: Removed unnecessary keys and dependencies

--- models/subClasses/channel/ad.js
+++ models/subClasses/channel/ad.js
@@ -1,12 +1,5 @@
-var model = require('../../../helpers/model'),
-	consts = require('../../../configs/commonConsts'),
-	Ad = {
-		merging: true,
-		mergingPriority: consts.enums.priorities.NEW_OBJECT,
-		mergingKey: 'id',
-		keys: ['id', 'adCode', 'css', 'height', 'width', 'network'],
-		clientKeys: ['id', 'adCode', 'css', 'height', 'width', 'network'],
-		defaults: {}
+var Ad = {
+		keys: ['id', 'adCode', 'css', 'height', 'width', 'network']
 	};
 
 module.exports = Ad;
