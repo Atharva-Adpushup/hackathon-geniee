15079ec85b59d8e33b369803ae4ca8ff0f63be58
ZahinAdPushup <zahin@adpushup.com>
2017-01-18 20:33:56 +0530

    [Code changes] (Front-end: Visual Editor) Site selector: Formatting changes

--- Editor/app/selectors/siteSelectors.js
+++ Editor/app/selectors/siteSelectors.js
@@ -8,35 +8,35 @@ import { getAllAds } from './adsSelectors';
 const getMode = (state) => state.site.modeStatus.mode,
 	getPartner = (state) => state.site.siteData.partner,
 	getFinalJson = createSelector([getAllChannels, getAllVariations, getAllSections, getAllAds, getMode], (allChannels = {}, allVariations = {}, allSections = {}, allAds = {}, siteMode = 2) =>
-		 ({
-			siteMode,
-			siteId: window.ADP_SITE_ID,
-			siteDomain: window.ADP_SITE_DOMAIN,
-			channels: (_.map(allChannels, (channel) => {
-				const channelVariations = {};
+	({
+		siteMode,
+		siteId: window.ADP_SITE_ID,
+		siteDomain: window.ADP_SITE_DOMAIN,
+		channels: (_.map(allChannels, (channel) => {
+			const channelVariations = {};
 
-				_.forEach(channel.variations, (variationId) => {
-					const sections = {};
+			_.forEach(channel.variations, (variationId) => {
+				const sections = {};
 
-					channelVariations[variationId] = allVariations[variationId];
+				channelVariations[variationId] = allVariations[variationId];
 
-					_.forEach(channelVariations[variationId].sections, (sectionId) => {
-						sections[sectionId] = allSections[sectionId];
+				_.forEach(channelVariations[variationId].sections, (sectionId) => {
+					sections[sectionId] = allSections[sectionId];
 
-						const ads = {};
-						_.forEach(sections[sectionId].ads, (sectionAdId) => {
-							ads[sectionAdId] = allAds[sectionAdId];
-						});
-
-						sections[sectionId].ads = ads;
+					const ads = {};
+					_.forEach(sections[sectionId].ads, (sectionAdId) => {
+						ads[sectionAdId] = allAds[sectionAdId];
 					});
-					channelVariations[variationId].sections = sections;
-				});
 
-				channel.variations = channelVariations;
-				return channel;
-			}))
-		})
+					sections[sectionId].ads = ads;
+				});
+				channelVariations[variationId].sections = sections;
+			});
+
+			channel.variations = channelVariations;
+			return channel;
+		}))
+	})
 	);
 
 export { getFinalJson, getMode, getPartner };
