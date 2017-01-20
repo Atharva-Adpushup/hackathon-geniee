bb1e40ee97d87dc431061c64547ccc4296644b2e
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-01-20 15:48:28 +0530

    Header bidding setup script refactoring

--- public/assets/js/libs/custom/header-bidding-setup.js
+++ public/assets/js/libs/custom/header-bidding-setup.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
                 geo: 'country'
             },
 
-            // Dynamic UI templates
+            // Setup UI templates and selectors
             templates: {
                 selectBoxes: {
                     geoSelect: '<div class="row"> <div class="col-sm-3"> <div class="input-name">Select type of Geography</div></div><div class="col-sm-3"> <div class="styleSelect select-box-lg"> <select name="geoType" required="required" class="geo-selector"> <option value="country">Country</option> <option value="continent">Continent</option> <option value="all">All</option> </select> </div></div></div>',
@@ -20,6 +20,17 @@ $(document).ready(function () {
                     continentSelect: '<div class="row select-geo-continent"> <div class="col-sm-3"> <div class="input-name">Select Continent</div></div><div class="col-sm-3"> <div class="styleSelect select-box-lg"> <select name="continent" required="required" class="geo-continent"></select> </div></div></div>',
                     adSizesSelect: '<div class="row"> <div class="col-sm-3"> <div class="input-name">Select Ad Size</div></div><div class="col-sm-3"> <div class="styleSelect select-box-lg"> <select name="adSize" required="required" class="ad-size"></select> </div></div></div>',
                     hbPartnerSelect: '<div class="row"> <div class="col-sm-3"> <div class="input-name">Select Header Bidding Partner</div></div><div class="col-sm-3"> <div class="styleSelect select-box-lg"> <select class="hb-partner text-capitalize" name="hbPartner" required="required"></select> </div></div></div>'
+                },
+                closeBtn: '<button type="button" class="close hb-close-pane">x</button>',
+                defaultSelectBoxOption: '<option selected value="">Select partner</option>',
+                buttons: {
+                    addPartner: '<button type="button" class="add-partner mT-10 btn btn-lightBg btn-default">Add another partner</button>',
+                    addSetup: '<button type="button" class="add-setup mT-10 btn btn-lightBg btn-default">Add another Setup</button>',
+                    addSize: '<button type="button" class="add-size mT-10 btn btn-lightBg btn-default">Add another size</button>'
+                },
+                selectors: {
+                    country: '.select-geo-country',
+                    continent: '.select-geo-continent'
                 }
             },
 
@@ -47,44 +58,45 @@ $(document).ready(function () {
                 w.hbPartners.forEach(function (partner) {
                     $(hbPartnerSelect).append('<option value=' + partner + '>' + partner + '</option>');
                 });
-                $(hbPartnerSelect).prepend('<option selected value="">Select partner</option>');
+                $(hbPartnerSelect).prepend(this.templates.defaultSelectBoxOption);
+            },
+
+            // Function to dynamically generate input templates for given partner config
+            generateInputTemplate: function (obj, i) {
+                var g = obj[i],
+                    type = g.validations && g.validations.type ? g.validations.type : 'text',
+                    required = g.validations && g.validations.required ? '" required="' + g.validations.required : ""
+                readonly = 'isEditable' in g && !g.isEditable ? '" readonly="' + g.isEditable : ""
+                value = g.default ? g.default : '',
+                    name = i;
+
+                var input = '<input class="form-control" type="' + type + required + readonly + '" value="' + value + '" name="' + name + '" placeholder="Please enter ' + g.alias + '"/>';
+                return '<div class="row"><div class="col-sm-3 input-name">' + g.alias + '</div><div class="col-sm-3">' + input + '</div></div>';
             },
 
             // Function to render partner options pane
-            generatePartnerSpecificOptionsPane: function(pt) {
+            generatePartnerSpecificOptionsPane: function (pt) {
                 var partnerInputTempl = {};
-
-                function getInputTemplate(obj, i) {
-                    var g = obj[i],
-                        type = g.validations && g.validations.type ? g.validations.type : 'text',
-                        required = g.validations && g.validations.required ? '" required="' + g.validations.required : ""
-                        readonly = 'isEditable' in g && !g.isEditable ? '" readonly="' + g.isEditable : ""
-                        value = g.default ? g.default : '',
-                        name = i;
-
-                    var input = '<input class="form-control" type="' + type + required + readonly + '" value="' + value + '" name="' + name + '" placeholder="Please enter '+g.alias+'"/>';
-                    return '<div class="row"><div class="col-sm-3 input-name">' + g.alias + '</div><div class="col-sm-3">' + input + '</div></div>';
-                };
 
                 for (var p in w.hbConfig) {
                     var partner = w.hbConfig[p], globals = '', locals = '';
 
                     if (partner.isHb) {
                         for (var global in partner.global) {
-                            globals += getInputTemplate(partner.global, global);
+                            globals += this.generateInputTemplate(partner.global, global);
                         }
 
                         for (var local in partner.local) {
-                            locals += getInputTemplate(partner.local, local);
+                            locals += this.generateInputTemplate(partner.local, local);
                         }
                     }
 
                     partnerInputTempl[p] = {
-                        globalTempl : globals,
+                        globalTempl: globals,
                         localTempl: locals
                     };
                 }
-                
+
                 return partnerInputTempl[pt];
             },
 
@@ -94,81 +106,57 @@ $(document).ready(function () {
                 wrapper.find('.partner-settings').html(inputs.globalTempl + inputs.localTempl);
             },
 
-            // Function to render hb partner setup panel
-            renderHbPartnerSetupPanel: function (el, type) {
-                var w = $('<div class="hb-config-pane mT-10 select-partner-settings">' + this.templates.selectBoxes.hbPartnerSelect + '<div class="partner-settings"></div></div>');
-                switch(type) {
+            // Generic function to render new setting panel
+            renderNewPanel: function(el, wrapper, action, selector, otherEl) {
+                if(otherEl) {
+                    wrapper.append(otherEl);
+                }
+
+                switch (action) {
                     case 'insertBefore':
-                        w.insertBefore(el);
+                        wrapper.insertBefore(el);
                         break;
                     default:
-                        el.append(w);
+                        el.append(wrapper);
                         break;
                 }
-                
-                if(!$(el).children().length) {
-                    var otherPanels = $(el).parent().children('.select-partner-settings');
 
-                    for(var i = 1; i < otherPanels.length; i++) {
-                        $(otherPanels[i]).prepend('<button type="button" class="close hb-close-pane">x</button>');
+                if (!$(el).children().length) {
+                    var otherPanels = $(el).parent().children(selector);
+
+                    for (var i = 1; i < otherPanels.length; i++) {
+                        $(otherPanels[i]).prepend(this.templates.closeBtn);
                     }
                 }
-                
+            },
+
+            // Function to render hb partner setup panel
+            renderHbPartnerSetupPanel: function (el, action) {
+                var w = $('<div class="hb-config-pane mT-10 select-partner-settings">' + this.templates.selectBoxes.hbPartnerSelect + '<div class="partner-settings"></div></div>');
+                this.renderNewPanel(el, w, action, '.select-partner-settings');
+
                 var hbPartner = w.find('.hb-partner');
                 this.setHbPartnersSelectBoxData(hbPartner);
             },
 
             // Function to render multi-config panel
-            renderMultiConfigPanel: function(el, action) {
+            renderMultiConfigPanel: function (el, action) {
                 var w = $('<div class="hb-config-pane mT-10 select-partner"></div>');
+                this.renderNewPanel(el, w, action, '.select-partner');
 
-                switch(action) {
-                    case 'insertBefore':
-                        $(w).insertBefore(el);
-                        break;
-                    default:
-                        el.append(w);
-                        break;
-                }
-
-                if(!$(el).children().length) {
-                    var otherPanels = $(el).parent().children('.select-partner');
-
-                    for(var i = 1; i < otherPanels.length; i++) {
-                        $(otherPanels[i]).prepend('<button type="button" class="close hb-close-pane">x</button>');
-                    }
-                }
-                
                 this.renderHbPartnerSetupPanel(w);
-                $(w).append('<button type="button" class="add-partner mT-10 btn btn-lightBg btn-default">Add another partner</button>');
+                $(w).append(this.templates.buttons.addPartner);
 
-                if(el.children('.select-partner').length === 1) {
-                    el.append('<button type="button" class="add-setup mT-10 btn btn-lightBg btn-default">Add another Setup</button>')
-                } 
+                if (el.children('.select-partner').length === 1) {
+                    el.append(this.templates.buttons.addSetup);
+                }
             },
 
             // Function to render ad size setup panel
             renderAdSizeSetupPanel: function (el, action) {
                 var w = $('<div class="hb-config-pane mT-10 select-size">'),
                     s = this.templates.selectBoxes.adSizesSelect;
-                w.append(s);
-
-                switch (action) {
-                    case 'append':
-                        $(el).append(w);
-                        break;
-                    case 'insertBefore':
-                        $(w).insertBefore(el);
-                        break;
-                }
-
-                if(!$(el).children().length) {
-                    var otherPanels = $(el).parent().children('.select-size');
-
-                    for(var i = 1; i < otherPanels.length; i++) {
-                        $(otherPanels[i]).prepend('<button type="button" class="close hb-close-pane">x</button>');
-                    }
-                }
+                this.renderNewPanel(el, w, action, '.select-size', s);
 
                 var adSize = w.find('.ad-size');
                 this.setAdSizeSelectBoxOptions(adSize);
@@ -189,24 +177,24 @@ $(document).ready(function () {
 
                 var otherPanels = $(w).parent().children('.select-geo-wrapper');
 
-                for(var i = 1; i < otherPanels.length; i++) {
-                    $(otherPanels[i]).prepend('<button type="button" class="close hb-close-pane">x</button>');
+                for (var i = 1; i < otherPanels.length; i++) {
+                    $(otherPanels[i]).prepend(this.templates.closeBtn);
                 }
 
                 this.setGeoSubSelect(this.defaults.geo, $('.geo-selector'));
-                this.renderAdSizeSetupPanel(w, 'append');
-                $(w).append('<button type="button" class="add-size mT-10 btn btn-lightBg btn-default">Add another size</button>');
+                this.renderAdSizeSetupPanel(w);
+                $(w).append(this.templates.buttons.addSize);
             },
 
             // Function to parse header bidding form data
-            parseHbFormData: function(form) {
+            parseHbFormData: function (form) {
                 var data = [];
-                [].slice.call(form.find('.select-geo-wrapper')).forEach(function(geoWrapper) {
+                [].slice.call(form.find('.select-geo-wrapper')).forEach(function (geoWrapper) {
                     var obj = {},
                         geo = $(geoWrapper).find('.geo-selector').val();
-                        
+
                     obj['type'] = geo;
-                    switch(geo) {
+                    switch (geo) {
                         case 'country':
                             obj[geo] = $(geoWrapper).find('.geo-country').val();
                             break;
@@ -216,23 +204,23 @@ $(document).ready(function () {
                     }
 
                     obj['info'] = {};
-                    [].slice.call($(geoWrapper).find('.select-size')).forEach(function(sizeWrapper) {
+                    [].slice.call($(geoWrapper).find('.select-size')).forEach(function (sizeWrapper) {
                         var size = $(sizeWrapper).find('.ad-size').val();
-                        
+
                         var setups = [];
-                        [].slice.call($(sizeWrapper).find('.select-partner')).forEach(function(partnerWrapper) {
+                        [].slice.call($(sizeWrapper).find('.select-partner')).forEach(function (partnerWrapper) {
 
                             var arr = [];
-                            [].slice.call($(partnerWrapper).find('.select-partner-settings')).forEach(function(partnerSetting) {
-                                
-                                [].slice.call($(partnerSetting).find('.partner-settings')).forEach(function(partnerData) {
+                            [].slice.call($(partnerWrapper).find('.select-partner-settings')).forEach(function (partnerSetting) {
+
+                                [].slice.call($(partnerSetting).find('.partner-settings')).forEach(function (partnerData) {
                                     var params = {};
 
-                                    [].slice.call($(partnerData).children()).forEach(function(setting) {
+                                    [].slice.call($(partnerData).children()).forEach(function (setting) {
                                         var key = $(setting).find('input').attr('name'),
                                             value = $(setting).find('input').val();
 
-                                         params[key] = value;
+                                        params[key] = value;
                                     });
 
                                     var data = {
@@ -242,18 +230,15 @@ $(document).ready(function () {
 
                                     arr.push(data);
                                 });
-
-                                
                             });
-
                             setups.push(arr);
-                        }); 
-
+                        });
                         obj.info[size] = setups;
                     });
-
-                    console.log(obj);
+                    data.push(obj);
                 });
+
+                return data;
             },
 
             // Switch geo selection dropdown in UI
@@ -265,22 +250,22 @@ $(document).ready(function () {
                         case 'country':
                             geoSelect.show();
                             geoSelect.find('option[value=' + that.defaults.country + ']').attr('selected', true);
-                            geoSelect.siblings('.select-geo-continent').hide();
+                            geoSelect.siblings(that.templates.selectors.continent).hide();
                             break;
                         case 'continent':
                             geoSelect.show();
-                            geoSelect.siblings('.select-geo-country').hide();
+                            geoSelect.siblings(that.templates.selectors.country).hide();
                             break;
                         case 'all':
-                            $(el).closest('.row').siblings('.select-geo-country').hide()
-                            $(el).closest('.row').siblings('.select-geo-continent').hide();
+                            $(el).closest('.row').siblings(that.templates.selectors.country).hide();
+                            $(el).closest('.row').siblings(that.templates.selectors.continent).hide();
                             break;
                     }
                 });
             },
 
             saveHeaderBiddingSetup: function (form) {
-                this.parseHbFormData(form);
+                var data = this.parseHbFormData(form);
             },
 
             // Initialise header bidding setup
@@ -290,11 +275,17 @@ $(document).ready(function () {
         };
         ap.headerBiddingSetup.init();
 
+
+
+        // Header bidding setup triggers
+        
+        // Geo-selector change trigger
         $('body').on('change', '.geo-selector', function (e) {
             var geo = $(this).val();
             ap.headerBiddingSetup.setGeoSubSelect(geo, $(this)[0]);
         });
 
+        // Hb partner selection change trigger
         $('body').on('change', '.hb-partner', function (e) {
             var hbPartner = $(this).val(),
                 topMostConfigWrapper = $(this).closest('.hb-config-pane');
@@ -302,26 +293,32 @@ $(document).ready(function () {
             hbPartner ? ap.headerBiddingSetup.renderPartnerSetupPanel(hbPartner, topMostConfigWrapper) : null;
         });
 
+        // Geo panel addition trigger
         $('#addgeo').on('click', function () {
             ap.headerBiddingSetup.renderGeoSetupPanel();
         });
 
+        // Size panel addition trigger
         $('body').on('click', '.add-size', function () {
             ap.headerBiddingSetup.renderAdSizeSetupPanel($(this), 'insertBefore');
         });
 
+        // Partner panel addition trigger
         $('body').on('click', '.add-partner', function () {
             ap.headerBiddingSetup.renderHbPartnerSetupPanel($(this), 'insertBefore');
         });
 
+        // Setup panel addition trigger
         $('body').on('click', '.add-setup', function () {
             ap.headerBiddingSetup.renderMultiConfigPanel($(this), 'insertBefore');
         });
 
+        // Close panel trigger
         $('body').on('click', '.hb-close-pane', function () {
             $(this).parent().remove();
         });
 
+        // Setup form submit trigger
         $('#hbform').on('submit', function (e) {
             e.preventDefault();
             ap.headerBiddingSetup.saveHeaderBiddingSetup($(this));
