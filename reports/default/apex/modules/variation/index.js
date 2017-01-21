d7dc63e22c88e54f9035b2978b2e2fe4ece8be4b
ZahinAdPushup <zahin@adpushup.com>
2017-01-22 02:53:43 +0530

    [Fix] (Backend: Service) Apex Reports: Variation: Get metrics report data fix

--- reports/default/apex/modules/variation/index.js
+++ reports/default/apex/modules/variation/index.js
@@ -7,12 +7,12 @@ module.exports = {
 	getMetrics: function(reportData) {
 		var computedData = {};
 
-		if (!reportData || !reportData.rows || !reportData.trafficDistribution) {
+		if (!reportData || !reportData.data || !reportData.data.rows || !reportData.data.trafficDistribution) {
 			throw new AdPushupError('Apex service data should not be empty');
 		}
 
-		_.forEach(reportData.rows, function(rowData) {
-			_.forOwn(reportData.trafficDistribution, function(variationObj, variationKey) {
+		_.forEach(reportData.data.rows, function(rowData) {
+			_.forOwn(reportData.data.trafficDistribution, function(variationObj, variationKey) {
 				if (!_.has(computedData, variationKey)) {
 					computedData[variationKey] = {
 						id: variationObj.id,
@@ -23,7 +23,7 @@ module.exports = {
 
 				if (variationObj.name === rowData[1]) {
 					computedData[variationKey].ctr = rowData[2];
-					computedData[variationKey].click = reportData.tracked[rowData[2]].adClicks;
+					computedData[variationKey].click = reportData.data.tracked[rowData[2]].adClicks;
 				}
 			});
 		});
