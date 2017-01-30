c1d9b6ef0b3df20d0d592ffba051eef958a48671
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-01-30 19:27:20 +0530

    Inline edit component refactoring

--- Editor/app/components/shared/inlineEdit/index.jsx
+++ Editor/app/components/shared/inlineEdit/index.jsx
@@ -1,6 +1,8 @@
+// Inline edit component
+
 import React, { PropTypes } from 'react';
-import { Row, Col, Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import './inlineEdit.scss';
+import { renderDropdownList, renderActionButtons, renderNormalMode, renderInlineEditPanel } from './renderMethods.js';
 
 class InlineEdit extends React.Component {
 	constructor(props) {
@@ -70,63 +72,15 @@ class InlineEdit extends React.Component {
 	}
 
 	renderDropdownList() {
-		return (
-			<div className="inline-dropdown-wrapper">
-				<button onClick={this.hideDropdown.bind(this)}>x</button>
-				<ul className="inline-dropdown" style={(this.props.dropdownList && this.props.dropdownList.length > 3) ? { overflowY: 'scroll', overflowX: 'hidden' } : {}}>
-					{
-						this.props.dropdownList.map((xpath, key) => (
-							<li onClick={this.selectDropdownValue.bind(this, xpath)} key={key}>
-								{xpath}
-							</li>
-						))
-					}
-				</ul>
-			</div>
-		);
+		return renderDropdownList(this);
 	}
 
 	renderActionButtons() {
-		return (
-			this.props.compact ? (
-				<div>
-					<Col className="u-padding-r5px" xs={8}>
-						<Button disabled={this.state.disableSave} onClick={this.submitValue.bind(this)} className="btn-lightBg btn-save btn-block btn btn-default">Save</Button>
-					</Col>
-					<Col className="u-padding-r10px " xs={4}>
-						<Button onClick={this.cancelEdit.bind(this)} className="btn-lightBg btn-cancel btn-ie-cancel btn-block btn btn-default"></Button>
-					</Col>
-				</div>
-			) : (
-				<div>
-					<Col className="u-padding-r5px" xs={4}>
-						<Button onClick={this.submitValue.bind(this)} className="btn-lightBg btn-save btn-block btn btn-default">Save</Button>
-					</Col>
-					<Col className="u-padding-r10px " xs={2}>
-						<Button onClick={this.cancelEdit.bind(this)} className="btn-lightBg btn-cancel btn-ie-cancel btn-block btn btn-default"></Button>
-					</Col>
-				</div>
-			)
-		);
+		return renderActionButtons(this);
 	}
 
 	renderNormalMode(adCodeStyles, adCodeEdit) {
-		return (
-			<div>
-				<strong style={{ fontWeight: this.props.font ? this.props.font : 700 }}>{
-					this.props.value ? (
-						<span style={this.props.adCode ? adCodeStyles : {}}> {this.props.adCode ? atob(this.props.value) : this.props.value} </span>
-					) : `Edit ${this.props.text}`
-				}</strong>
-				{
-					this.props.text ? (
-						<OverlayTrigger placement="bottom" overlay={<Tooltip id="edit-variation-tooltip">Edit {this.props.text}</Tooltip>}>
-							<button style={this.props.adCode ? adCodeEdit : {}} onClick={this.triggerEdit.bind(this)} className="btn-icn-edit"></button>
-						</OverlayTrigger>
-					) : <button onClick={this.triggerEdit.bind(this)} className="btn-icn-edit"></button>
-				}
-			</div>
-		);
+		return renderNormalMode(this, adCodeStyles, adCodeEdit)
 	}
 
 	render() {
@@ -143,35 +97,7 @@ class InlineEdit extends React.Component {
 			},
 			adCodeCheck = this.props.adCode ? (!this.props.value ? '' : atob(this.props.value)) : this.props.value;
 
-		return (
-			<div>
-				{
-					this.state.editMode ? (
-						<Row style={{ margin: 0 }}>
-							<Col className="u-padding-r10px" xs={colCompact} style={{ position: 'relative' }}>
-								<input type="text" ref="editedText" 
-									placeholder={this.props.text} 
-									defaultValue={adCodeCheck} 
-									onKeyUp={this.props.keyUpHandler ? this.keyUp.bind(this) : () => { }}
-									onFocus={this.props.changeHandler ? this.changeValue.bind(this) : () => { } } 
-									onChange={this.props.changeHandler ? this.changeValue.bind(this) : () => { } } />
-								{
-									this.state.showDropdownList ? (
-										this.renderDropdownList()
-									) : null
-								}
-								<span className="error-message">{this.state.inputError ? (this.props.validationError ? this.props.validationError : this.props.errorMessage) : ''}</span>
-							</Col>
-							{
-								this.renderActionButtons()
-							}
-						</Row>
-					) : (
-						this.renderNormalMode(adCodeStyles, adCodeEdit)
-					)
-				}
-			</div>
-		);
+		return renderInlineEditPanel(this, colCompact, adCodeStyles, adCodeEdit, adCodeCheck);
 	}
 }
 
---
+++ Editor/app/components/shared/inlineEdit/renderMethods.js
@@ -0,0 +1,94 @@
+// Inline edit render methods
+
+import { Row, Button, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+const renderDropdownList = that => {
+	return (
+		<div className="inline-dropdown-wrapper">
+			<button onClick={that.hideDropdown.bind(that)}>x</button>
+			<ul className="inline-dropdown" style={(that.props.dropdownList && that.props.dropdownList.length > 3) ? { overflowY: 'scroll', overflowX: 'hidden' } : {}}>
+				{
+					that.props.dropdownList.map((xpath, key) => (
+						<li onClick={that.selectDropdownValue.bind(that, xpath)} key={key}>
+							{xpath}
+						</li>
+					))
+				}
+			</ul>
+		</div>
+	);
+},
+	renderActionButtons = that => {
+		return (
+			that.props.compact ? (
+				<div>
+					<Col className="u-padding-r5px" xs={8}>
+						<Button disabled={that.state.disableSave} onClick={that.submitValue.bind(that)} className="btn-lightBg btn-save btn-block btn btn-default">Save</Button>
+					</Col>
+					<Col className="u-padding-r10px " xs={4}>
+						<Button onClick={that.cancelEdit.bind(that)} className="btn-lightBg btn-cancel btn-ie-cancel btn-block btn btn-default"></Button>
+					</Col>
+				</div>
+			) : (
+					<div>
+						<Col className="u-padding-r5px" xs={4}>
+							<Button onClick={that.submitValue.bind(that)} className="btn-lightBg btn-save btn-block btn btn-default">Save</Button>
+						</Col>
+						<Col className="u-padding-r10px " xs={2}>
+							<Button onClick={that.cancelEdit.bind(that)} className="btn-lightBg btn-cancel btn-ie-cancel btn-block btn btn-default"></Button>
+						</Col>
+					</div>
+				)
+		);
+	},
+	renderNormalMode = (that, adCodeStyles, adCodeEdit) => {
+		return (
+			<div>
+				<strong style={{ fontWeight: that.props.font ? that.props.font : 700 }}>{
+					that.props.value ? (
+						<span style={that.props.adCode ? adCodeStyles : {}}> {that.props.adCode ? atob(that.props.value) : that.props.value} </span>
+					) : `Edit ${that.props.text}`
+				}</strong>
+				{
+					that.props.text ? (
+						<OverlayTrigger placement="bottom" overlay={<Tooltip id="edit-variation-tooltip">Edit {that.props.text}</Tooltip>}>
+							<button style={that.props.adCode ? adCodeEdit : {}} onClick={that.triggerEdit.bind(that)} className="btn-icn-edit"></button>
+						</OverlayTrigger>
+					) : <button onClick={that.triggerEdit.bind(that)} className="btn-icn-edit"></button>
+				}
+			</div>
+		);
+	},
+	renderInlineEditPanel = (that, colCompact, adCodeStyles, adCodeEdit, adCodeCheck) => {
+		return (
+			<div>
+				{
+					that.state.editMode ? (
+						<Row style={{ margin: 0 }}>
+							<Col className="u-padding-r10px" xs={colCompact} style={{ position: 'relative' }}>
+								<input type="text" ref="editedText"
+									placeholder={that.props.text}
+									defaultValue={adCodeCheck}
+									onKeyUp={that.props.keyUpHandler ? that.keyUp.bind(that) : () => { } }
+									onFocus={that.props.changeHandler ? that.changeValue.bind(that) : () => { } }
+									onChange={that.props.changeHandler ? that.changeValue.bind(that) : () => { } } />
+								{
+									that.state.showDropdownList ? (
+										that.renderDropdownList()
+									) : null
+								}
+								<span className="error-message">{that.state.inputError ? (that.props.validationError ? that.props.validationError : that.props.errorMessage) : ''}</span>
+							</Col>
+							{
+								that.renderActionButtons()
+							}
+						</Row>
+					) : (
+							that.renderNormalMode(adCodeStyles, adCodeEdit)
+						)
+				}
+			</div>
+		);
+	};
+
+export { renderDropdownList, renderActionButtons, renderNormalMode, renderInlineEditPanel };
