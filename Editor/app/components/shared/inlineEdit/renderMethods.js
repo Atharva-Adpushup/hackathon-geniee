4fb4532d18e36c71e5418849d7ae752fbac0a4fc
Yomesh Gupta <yomesh.gupta@adpushup.com>
2017-02-02 16:51:11 +0530

    Added functionality to hide dropdown list on document click

--- Editor/app/components/shared/inlineEdit/renderMethods.js
+++ Editor/app/components/shared/inlineEdit/renderMethods.js
@@ -5,11 +5,10 @@ import { Row, Button, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
 const renderDropdownList = that => {
 	return (
 		<div className="inline-dropdown-wrapper">
-			<button onClick={that.hideDropdown.bind(that)}>x</button>
 			<ul className="inline-dropdown" style={(that.props.dropdownList && that.props.dropdownList.length > 3) ? { overflowY: 'scroll', overflowX: 'hidden' } : {}}>
 				{
 					that.props.dropdownList.map((xpath, key) => (
-						<li onClick={that.selectDropdownValue.bind(that, xpath)} key={key}>
+						<li onClick={() => that.selectDropdownValue(xpath)} key={key}>
 							{xpath}
 						</li>
 					))
@@ -23,19 +22,19 @@ const renderDropdownList = that => {
 			that.props.compact ? (
 				<div>
 					<Col className="u-padding-r5px" xs={8}>
-						<Button disabled={that.state.disableSave} onClick={that.submitValue.bind(that)} className="btn-lightBg btn-save btn-block btn btn-default">Save</Button>
+						<Button disabled={that.state.disableSave} onClick={() => that.submitValue()} className="btn-lightBg btn-save btn-block btn btn-default">Save</Button>
 					</Col>
 					<Col className="u-padding-r10px " xs={4}>
-						<Button onClick={that.cancelEdit.bind(that)} className="btn-lightBg btn-cancel btn-ie-cancel btn-block btn btn-default"></Button>
+						<Button onClick={() => that.cancelEdit()} className="btn-lightBg btn-cancel btn-ie-cancel btn-block btn btn-default"></Button>
 					</Col>
 				</div>
 			) : (
 					<div>
 						<Col className="u-padding-r5px" xs={4}>
-							<Button onClick={that.submitValue.bind(that)} className="btn-lightBg btn-save btn-block btn btn-default">Save</Button>
+							<Button onClick={() => that.submitValue()} className="btn-lightBg btn-save btn-block btn btn-default">Save</Button>
 						</Col>
 						<Col className="u-padding-r10px " xs={2}>
-							<Button onClick={that.cancelEdit.bind(that)} className="btn-lightBg btn-cancel btn-ie-cancel btn-block btn btn-default"></Button>
+							<Button onClick={() => that.cancelEdit()} className="btn-lightBg btn-cancel btn-ie-cancel btn-block btn btn-default"></Button>
 						</Col>
 					</div>
 				)
@@ -53,9 +52,9 @@ const renderDropdownList = that => {
 				{
 					that.props.text ? (
 						<OverlayTrigger placement="bottom" overlay={<Tooltip id="edit-variation-tooltip">Edit {that.props.text}</Tooltip>}>
-							<button style={that.props.adCode ? adCodeEdit : {}} onClick={that.triggerEdit.bind(that)} className="btn-icn-edit"></button>
+							<button style={that.props.adCode ? adCodeEdit : {}} onClick={() => that.triggerEdit()} className="btn-icn-edit"></button>
 						</OverlayTrigger>
-					) : <button onClick={that.triggerEdit.bind(that)} className="btn-icn-edit"></button>
+					) : <button onClick={() => that.triggerEdit()} className="btn-icn-edit"></button>
 				}
 			</div>
 		);
@@ -70,9 +69,9 @@ const renderDropdownList = that => {
 								<input type="text" ref="editedText"
 									placeholder={that.props.text}
 									defaultValue={adCodeCheck}
-									onKeyUp={that.props.keyUpHandler ? that.keyUp.bind(that) : () => { } }
-									onFocus={that.props.changeHandler ? that.changeValue.bind(that) : () => { } }
-									onChange={that.props.changeHandler ? that.changeValue.bind(that) : () => { } } />
+									onKeyUp={that.props.keyUpHandler ? () => that.keyUp() : () => { } }
+									onFocus={that.props.changeHandler ? () => that.changeValue() : () => { } }
+									onChange={that.props.changeHandler ? () => that.changeValue() : () => { } } />
 								{
 									that.state.showDropdownList ? (
 										that.renderDropdownList()
