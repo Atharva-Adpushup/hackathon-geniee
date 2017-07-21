2cec45f5cb3a2c16f26f979dff0bf1aae6262260
ZahinAdPushup <zahin@adpushup.com>
2017-07-21 17:08:21 +0530

    [Feature] (Frontend: Ads Replay) Geniee Revenue: Zone revenue computation logic added

--- services/genieeAdSyncService/genieeAp/src/genieeObject.js
+++ services/genieeAdSyncService/genieeAp/src/genieeObject.js
@@ -1,8 +1,44 @@
 // Below object will hold all Geniee partner specific functionality
 var genieeObject = {
 	// Get zone id and ecpm values for every successful zone impression
-	registerZoneECPM: function (id, ecpm) {
-		console.log('Zone id: ', id, ', ecpm: ', ecpm);
+	registerZoneECPM: function (inputZoneId, inputZoneECPM) {
+		if (!inputZoneId || !inputZoneECPM) { return false; }
+
+		inputZoneId = parseInt(inputZoneId, 10);
+		inputZoneECPM = parseFloat(inputZoneECPM);
+
+		var globalConfig = window.adpushup.config,
+			getMatchedAdId = function (adsArray, zoneId) {
+				var adId = null;
+
+				if (!adsArray.length || !zoneId) { return null; }
+
+				adsArray.forEach(function (adObject) {
+					var isGenieeObject = !!(adObject && adObject.network && (adObject.network === 'geniee') && adObject.networkData),
+						isNetworkData = !!(isGenieeObject && adObject.networkData.zoneId),
+						isZoneIdMatch = !!(isNetworkData && (adObject.networkData.zoneId === zoneId));
+
+					if (isZoneIdMatch) {
+						adId = adObject.id;
+						return false;
+					}
+				});
+
+				return adId;
+			},
+			matchedAdId = getMatchedAdId(globalConfig.ads, inputZoneId),
+			resultObject;
+
+		if (!matchedAdId) { return false; }
+
+		resultObject = {
+			eventType: 11,
+			adId: matchedAdId,
+			revenue: inputZoneECPM,
+			adZoneId: inputZoneId
+		};
+
+		console.log('Result object is: ', resultObject);
 	}
 };
 
