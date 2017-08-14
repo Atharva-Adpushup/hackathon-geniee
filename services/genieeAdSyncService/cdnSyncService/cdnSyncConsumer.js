979b7ed72d4f20f18c4f8b33a0b723b4fe2bd65c
ZahinAdPushup <zahin@adpushup.com>
2017-08-14 22:27:16 +0530

    [Code changes] (Backend: Cdn sync service) Added report data existence check

--- services/genieeAdSyncService/cdnSyncService/cdnSyncConsumer.js
+++ services/genieeAdSyncService/cdnSyncService/cdnSyncConsumer.js
@@ -70,17 +70,20 @@ module.exports = function (site) {
         getVariationsPayload = function (site, reportData) {
             var finalJson = {},
                 pageGroupPattern = site.get('apConfigs').pageGroupPattern,
-                isPageGroupPattern = !!(pageGroupPattern && _.isObject(pageGroupPattern));
+                isPageGroupPattern = !!(pageGroupPattern && _.isObject(pageGroupPattern)),
+                isReportData = !!(reportData && _.isObject(reportData) && _.keys(reportData).length);
 
             return site.getAllChannels().then(function (allChannels) {
                 _.each(allChannels, function (channel) {
-                    var platform, pageGroup, channelKey, pageGroupData;
+                    var platform, pageGroup, channelKey, pageGroupData,
+                        isValidData;
 
                     platform = channel.platform; // last element is platform
                     pageGroup = channel.pageGroup; // join remaing to form pageGroup
                     // channelKey sample name HOME_DESKTOP
                     channelKey = pageGroup + '_' + platform;
-                    pageGroupData = channelKey ? reportData.pageGroups[channelKey] : null;
+                    isValidData = !!(channelKey && isReportData);
+                    pageGroupData = isValidData ? reportData.pageGroups[channelKey] : null;
 
                     if (!finalJson[platform]) {
                         finalJson[platform] = {};
