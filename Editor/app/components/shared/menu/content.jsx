d0b132743f30f32b855258142930718a14b44dc6
YomeshAdPushup <yomesh.gupta@adpushup.com>
2017-08-17 15:12:14 +0530

    Empty Menu Render

--- Editor/app/components/shared/menu/content.jsx
+++ Editor/app/components/shared/menu/content.jsx
@@ -1,19 +1,25 @@
 import React, { PropTypes } from 'react';
 
+function emptyRender() {
+    return (
+        <div className="emptyChannelMenu">
+            <i className="fa fa-exclamation-triangle" aria-hidden="true"></i>
+            <p>All channels already open</p>
+        </div>
+    );
+}
+
 const MenuContent = (props) => (
-	<div className="MenuBarContainerWrapper">
+    <div className="MenuBarContainerWrapper">
 		{props.contentHeading ? (<h5 className="head"><a>{props.contentHeading}</a><i className={`pull-right fa  + ${props.icon}`} /></h5>) : null}
 		<div className="MenuBarContainer">
 			<div className="MenuBarInner">
                 {
-                    props.children && props.children.props.channels.length
-                    ? props.children
-                    : (
-                        <div className="emptyChannelMenu">
-                            <i className="fa fa-exclamation-triangle" aria-hidden="true"></i>
-                            <p>All channels already open</p>
-                        </div>
-                    )
+                    props.children
+                    ? props.children.props.channels && typeof props.children.props.channels == "object" && !props.children.props.channels.length 
+                      ? emptyRender()
+                      : props.children
+                    : emptyRender()
                 }
             </div>
 		</div>
