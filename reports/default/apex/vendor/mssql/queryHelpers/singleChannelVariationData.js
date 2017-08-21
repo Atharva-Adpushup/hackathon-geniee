9b82f2914d9b90f13fb63431fbd6291c78377fb4
ZahinAdPushup <zahin@adpushup.com>
2017-08-21 17:03:37 +0530

    [Code changes] (Backend: Sql report integration) SingleChannelVariationHelper: Moved common methods into same file, changed variation selection logic to incorporate deleted page groups as well

--- reports/default/apex/vendor/mssql/queryHelpers/singleChannelVariationData.js
+++ reports/default/apex/vendor/mssql/queryHelpers/singleChannelVariationData.js
@@ -1,9 +1,72 @@
 const fullSiteDataQueryHelper = require('./fullSiteData'),
 	AdPushupError = require('../../../../../../helpers/AdPushupError'),
 	siteModel = require('../../../../../../models/siteModel'),
-	pageGroupModule = require('../../../../../partners/geniee/modules/pageGroup/index'),
 	Promise = require('bluebird'),
-	extend = require('extend');
+	extend = require('extend'),
+	_ = require('lodash');
+
+function transformAllPageGroupsData(inputChannelData) {
+	const emptyChannelDataStr = 'Channel data should not be empty';
+	if (!inputChannelData || !inputChannelData.length) { throw new AdPushupError(emptyChannelDataStr); }
+
+	const computedData = inputChannelData.concat([]);
+
+	return computedData.reduce((accumulatorObject, channelObject) => {
+		accumulatorObject[channelObject.channelName] = {
+			id: channelObject.id,
+			sampleUrl: channelObject.sampleUrl,
+			pageGroup: channelObject.pageGroup,
+			device: channelObject.platform,
+			channelName: channelObject.channelName,
+			variations: extend(true, {}, channelObject.variations)
+		};
+
+		return accumulatorObject;
+	}, {});
+}
+
+function updatePageGroupData(siteId, sqlReportData, allChannelsData) {
+	const allPageGroupData = extend(true, {}, allChannelsData),
+		sqlReportPageGroupData = extend(true, {}, sqlReportData[siteId].pageGroups),
+		channelKeys = _.keys(allPageGroupData),
+		computedData = {};
+
+	_.forEach(channelKeys, (channelKey) => {
+		const isChannelKeyInSqlReportData = !!(sqlReportPageGroupData.hasOwnProperty(channelKey) && sqlReportPageGroupData[channelKey]),
+			initialMetrics = { dayWisePageViews: {}, days: {}, tracked: { pageViews: 0, click: 0, pageCTR: 0.0, impression: 0 }, click: 0, impression: 0, revenue: 0.0, pageViews: 0, pageRPM: 0.0, pageCTR: 0.0 };
+		let channelPageGroupObject = extend(true, {}, allPageGroupData[channelKey]);
+
+		if (isChannelKeyInSqlReportData) {
+			const sqlReportPageGroupObject = extend(true, {}, sqlReportPageGroupData[channelKey]),
+				sqlReportPageGroupVariationKeys = _.keys(sqlReportPageGroupObject.variations);
+
+			//NOTE: A deep extend will also effect source objects, they also extend each other properties
+			// sqlReportPageGroupObject will extend channelPageGroupObject properties and vice versa
+			computedData[channelKey] = extend(true, sqlReportPageGroupObject, channelPageGroupObject);
+
+			// Iterate over all page group variations and delete the variation which is
+			// non-existent in Sql reports variation data, i.e., Only include the variations which
+			// are present in both sql report and database channels data
+			_.forOwn(channelPageGroupObject.variations, (variationObject, variationKey) => {
+				const doesVariationKeyMatch = _.includes(sqlReportPageGroupVariationKeys, variationKey);
+
+				if (!doesVariationKeyMatch) { delete computedData[channelKey].variations[variationKey]; }
+			});
+		} else {
+			const computedPageGroupObject = extend(true, {}, channelPageGroupObject);
+
+			_.forOwn(computedPageGroupObject.variations, (variationObject) => {
+				const variationMetrics = extend(true, {}, initialMetrics);
+
+				extend(true, variationObject, variationMetrics);
+			});
+
+			computedData[channelKey] = extend(true, {}, computedPageGroupObject);
+		}
+	});
+
+	return Promise.resolve(computedData);
+}
 
 function getVariationsData(channelName, reportData) {
 	const pageGroupObject = extend(true, {}, reportData),
@@ -20,10 +83,10 @@ function getVariationsData(channelName, reportData) {
 function getMatchedVariationsData(siteId, channelKey, sqlReportData) {
 	const getSiteModel = siteModel.getSiteById(siteId),
 		getAllChannels = getSiteModel.then(site => site.getAllChannels()),
-		getTransformedChannelData = getAllChannels.then(pageGroupModule.transformAllPageGroupsData);
+		getTransformedChannelData = getAllChannels.then(transformAllPageGroupsData);
 
 	return Promise.join(getTransformedChannelData, (allChannelsData) => {
-		return pageGroupModule.updatePageGroupData(siteId, sqlReportData, allChannelsData)
+		return updatePageGroupData(siteId, sqlReportData, allChannelsData)
 			.then(getVariationsData.bind(null, channelKey));
 	});
 }
