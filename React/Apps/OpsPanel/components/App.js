26a5dbc2569699103013f66677ba49f1660fcc40
YomeshAdPushup <yomesh.gupta@adpushup.com>
2017-10-31 17:35:00 +0530

    Ops Panel stopped for react-date bug fix

--- React/Apps/OpsPanel/components/App.js
+++ React/Apps/OpsPanel/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LinkList from './LinkList';
+import LiveSitesMapping from './LiveSitesMapping';
 
 class App extends Component {
 	constructor(props) {
@@ -12,7 +13,7 @@ class App extends Component {
 			<BrowserRouter>
 				<Switch>
 					<Route exact path="/ops" component={LinkList} />
-					<Route exact path="/ops/liveSitesMapping" component={LiveSitesMapping} />
+					<Route path="/ops/liveSitesMapping" component={LiveSitesMapping} />
 				</Switch>
 			</BrowserRouter>
 		);
