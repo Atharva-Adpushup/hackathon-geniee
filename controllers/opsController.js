1f406ea124aab295a33d55f15b324e6e171f1eb1
YomeshAdPushup <yomesh.gupta@adpushup.com>
2017-11-02 19:38:48 +0530

    Added Fetch All Sites Mapping Route

--- controllers/opsController.js
+++ controllers/opsController.js
@@ -1,16 +1,49 @@
 const express = require('express'),
 	Promise = require('bluebird'),
 	_ = require('lodash'),
-	router = express.Router();
+	{ couchbaseService } = require('node-utils'),
+	config = require('../configs/config');
+(router = express.Router()),
+	(appBucket = couchbaseService(
+		'couchbase://127.0.0.1/apAppBucket',
+		config.couchBase.DEFAULT_BUCKET,
+		config.couchBase.DEFAULT_BUCKET_PASSWORD
+	));
 
-router.get(['/', '/liveSitesMapping', 'couchbaseEditor'], (req, res) => {
-	const { session, params } = req;
-
-	if (session.isSuperUser) {
-		return res.render('opsPanel');
-	} else {
-		return res.render('404');
+const fn = {
+	getAllSitesFromCouchbase: () => {
+		let query =
+			"select a.siteId, a.siteDomain, a.ownerEmail, a.step, a.channels, a.apConfigs, a.dateCreated, b.adNetworkSettings[0].pubId, b.adNetworkSettings[0].adsenseEmail from apAppBucket a join apAppBucket b on keys 'user::' || a.ownerEmail where meta(a).id like 'site::%'";
+		return appBucket.queryDb(query);
 	}
-});
+};
+
+router
+	.get(['/', '/liveSitesMapping', 'couchbaseEditor'], (req, res) => {
+		const { session, params } = req;
+
+		if (session.isSuperUser) {
+			return res.render('opsPanel');
+		} else {
+			return res.render('404');
+		}
+	})
+	.get('/getAllSites', (req, res) => {
+		let response = {
+			error: false
+		};
+		return fn
+			.getAllSitesFromCouchbase()
+			.then(sites => res.send(Object.assign(response, { sites: sites })))
+			.catch(err => {
+				console.log(err);
+				res.send(
+					Object.assign(response, {
+						error: true,
+						message: 'Message failed'
+					})
+				);
+			});
+	});
 
 module.exports = router;
