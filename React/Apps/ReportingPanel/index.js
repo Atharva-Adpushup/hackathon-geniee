ccfb95f19aea2ce14d8c046672e256828b672c30
Dhiraj Singh <dhiraj@adpushup.com>
2017-11-10 17:54:40 +0530

    Added loader in reporting panel

--- React/Apps/ReportingPanel/index.js
+++ React/Apps/ReportingPanel/index.js
@@ -3,3 +3,4 @@ import ReactDOM from 'react-dom';
 import ReportingPanel from './components/ReportingPanel.jsx';
 
 ReactDOM.render(<ReportingPanel />, document.getElementById('reportingPanel'));
+document.querySelector('.spinner').style.display = 'none';
