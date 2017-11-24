7644985ebb28bfa3a22b34cb8af6ad260143a48a
ZahinAdPushup <zahin@adpushup.com>
2017-11-24 19:25:00 +0530

    [Feature] (Backend: Site Top Url query) Following changes are done: - CommonFunctions JS: Added a 'SiteTopUrls' API, prettier formatting changes and corrected 'dateFormat' constants path - Utils JS: Corrected billion number formatter suffix value - tests/siteTopUrlsCommonFunctions JS: Added common function test file - adpTags/constants JS: Added sql query - adpTags/index.js: Exported 'executeQuery' function that will enable complex queries for module - queries/siteTopUrls JS: Query implementation - tests/siteTopUrls JS: Tests added for query implementation

--- helpers/utils.js
+++ helpers/utils.js
@@ -16,7 +16,7 @@ var url = require('url'),
 		},
 		numberFormatter: num => {
 			if (num >= 1000000000) {
-				return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+				return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
 			}
 			if (num >= 1000000) {
 				return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
