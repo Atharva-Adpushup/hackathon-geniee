4a9ab3fa477d7b35754b44e49d1da2207b0e30aa
ZahinAdPushup <zahin@adpushup.com>
2017-11-30 06:31:47 +0530

    [Code changes] (Backend: Weekly Email Report Service) Following changes are done:
    - weeklyEmailReportService/modules/base64ToImg/index JS: Added Device Revenue Pie base 64 to image conversion logic
    - weeklyEmailReportService/modules/highCharts/index JS: Added Device Revenue Pie base 64 generation logic

--- services/weeklyEmailReportService/modules/base64ToImg/index.js
+++ services/weeklyEmailReportService/modules/base64ToImg/index.js
@@ -19,13 +19,19 @@ module.exports = {
 				name: 'adNetworkRevenuePieChart',
 				data: inputData.report.charts.adNetworkRevenuePie.base64,
 				path: `${__dirname}/`
+			},
+			{
+				name: 'deviceRevenuePieChart',
+				data: inputData.report.charts.deviceRevenuePie.base64,
+				path: `${__dirname}/`
 			}
 		];
 
 		return convertBase64ToImageModule.init(base64Collection).then(imagesCollection => {
 			const cpmLineImageInfo = imagesCollection[0],
 				adNetworkCPMLineImageInfo = imagesCollection[1],
-				adNetworkRevenuePieImageInfo = imagesCollection[2];
+				adNetworkRevenuePieImageInfo = imagesCollection[2],
+				deviceRevenuePieImageInfo = imagesCollection[3];
 
 			inputData.report.charts.cpmLine.base64 = '';
 			inputData.report.charts.cpmLine.imageName = cpmLineImageInfo.name;
@@ -41,6 +47,11 @@ module.exports = {
 			inputData.report.charts.adNetworkRevenuePie.imageName = adNetworkRevenuePieImageInfo.name;
 			inputData.report.charts.adNetworkRevenuePie.imagePath = adNetworkRevenuePieImageInfo.path;
 			inputData.report.charts.adNetworkRevenuePie.cid = adNetworkRevenuePieImageInfo.cid;
+
+			inputData.report.charts.deviceRevenuePie.base64 = '';
+			inputData.report.charts.deviceRevenuePie.imageName = deviceRevenuePieImageInfo.name;
+			inputData.report.charts.deviceRevenuePie.imagePath = deviceRevenuePieImageInfo.path;
+			inputData.report.charts.deviceRevenuePie.cid = deviceRevenuePieImageInfo.cid;
 
 			return inputData;
 		});
