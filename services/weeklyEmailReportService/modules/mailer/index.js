9ec6317d58fdefb6b3e3405590cca44b010f39a4
ZahinAdPushup <zahin@adpushup.com>
2017-12-04 02:05:21 +0530

    [Fix] (Backend: Weekly Email Reports Service) Following changes are done:
    - helpers/utils JS: Modified 'getMetricComparison' method to incorporate zero numbers, added appropriate text for unchanged comparison use case
    - weeklyEmailReportService/mailer JS: Removed test file
    - weeklyEmailReportService/modules/highCharts/index JS: Corrected category computation for CPM line contribution chart
    - weeklyEmailReportService/modules/mailer/index JS: Top Urls decoded, revenue metric numbers to be shown in 2 decimal values if less than one

--- services/weeklyEmailReportService/modules/mailer/index.js
+++ services/weeklyEmailReportService/modules/mailer/index.js
@@ -25,6 +25,14 @@ function setKeyValuesInTemplate(template, config) {
 	);
 }
 
+function sanitiseUrl(url) {
+	return decodeURI(utils.domanize(url));
+}
+
+function isNumberLessThanOne(number) {
+	return !!(Number(number) < 1);
+}
+
 function getTemplateConfig(inputData) {
 	const topUrlsObject = inputData.report.topUrls,
 		firstUrlObject = topUrlsObject[0],
@@ -37,8 +45,14 @@ function getTemplateConfig(inputData) {
 		eigthUrlObject = topUrlsObject[7],
 		ninthUrlObject = topUrlsObject[8],
 		tenthUrlObject = topUrlsObject[9],
-		revenueLastWeekOriginalNumber = Math.round(inputData.report.metricComparison.revenue.lastWeekOriginal),
-		revenueThisWeekOriginalNumber = Math.round(inputData.report.metricComparison.revenue.thisWeekOriginal),
+		revenueLastWeekNumber = inputData.report.metricComparison.revenue.lastWeekOriginal,
+		revenueThisWeekNumber = inputData.report.metricComparison.revenue.thisWeekOriginal,
+		revenueLastWeekOriginalNumber = isNumberLessThanOne(revenueLastWeekNumber)
+			? utils.toFloat(revenueLastWeekNumber)
+			: Math.round(revenueLastWeekNumber),
+		revenueThisWeekOriginalNumber = isNumberLessThanOne(revenueThisWeekNumber)
+			? utils.toFloat(revenueThisWeekNumber)
+			: Math.round(revenueThisWeekNumber),
 		changeImgPathObject = {
 			decreased: 'http://console.adpushup.com/assets/images/down-arrow.png',
 			increased: 'http://console.adpushup.com/assets/images/up-arrow.png'
@@ -51,43 +65,43 @@ function getTemplateConfig(inputData) {
 		'@__revenue_change_text__@': inputData.report.metricComparison.revenue.change,
 		'@__revenue_change_value__@': inputData.report.metricComparison.revenue.percentage,
 		'@__revenue_change_img__@': changeImgPathObject[inputData.report.metricComparison.revenue.change],
+		'@__revenue_lastWeek_value__@': revenueLastWeekOriginalNumber,
+		'@__revenue_thisWeek_value__@': revenueThisWeekOriginalNumber,
 		'@__impression_lastWeek_value__@': inputData.report.metricComparison.impressions.lastWeek,
 		'@__impression_thisWeek_value__@': inputData.report.metricComparison.impressions.thisWeek,
 		'@__impression_change_img__@': changeImgPathObject[inputData.report.metricComparison.impressions.change],
 		'@__cpm_lastWeek_value__@': inputData.report.metricComparison.cpm.lastWeek,
 		'@__cpm_thisWeek_value__@': inputData.report.metricComparison.cpm.thisWeek,
 		'@__cpm_change_img__@': changeImgPathObject[inputData.report.metricComparison.cpm.change],
-		'@__revenue_lastWeek_value__@': revenueLastWeekOriginalNumber,
-		'@__revenue_thisWeek_value__@': revenueThisWeekOriginalNumber,
 		'@__topUrl_first_link__@': firstUrlObject.url,
-		'@__topUrl_first_value__@': utils.domanize(firstUrlObject.url),
+		'@__topUrl_first_value__@': sanitiseUrl(firstUrlObject.url),
 		'@__topUrl_first_hits__@': firstUrlObject.count,
 		'@__topUrl_second_link__@': secondUrlObject.url,
-		'@__topUrl_second_value__@': utils.domanize(secondUrlObject.url),
+		'@__topUrl_second_value__@': sanitiseUrl(secondUrlObject.url),
 		'@__topUrl_second_hits__@': secondUrlObject.count,
 		'@__topUrl_third_link__@': thirdUrlObject.url,
-		'@__topUrl_third_value__@': utils.domanize(thirdUrlObject.url),
+		'@__topUrl_third_value__@': sanitiseUrl(thirdUrlObject.url),
 		'@__topUrl_third_hits__@': thirdUrlObject.count,
 		'@__topUrl_fourth_link__@': fourthUrlObject.url,
-		'@__topUrl_fourth_value__@': utils.domanize(fourthUrlObject.url),
+		'@__topUrl_fourth_value__@': sanitiseUrl(fourthUrlObject.url),
 		'@__topUrl_fourth_hits__@': fourthUrlObject.count,
 		'@__topUrl_fifth_link__@': fifthUrlObject.url,
-		'@__topUrl_fifth_value__@': utils.domanize(fifthUrlObject.url),
+		'@__topUrl_fifth_value__@': sanitiseUrl(fifthUrlObject.url),
 		'@__topUrl_fifth_hits__@': fifthUrlObject.count,
 		'@__topUrl_sixth_link__@': sixthUrlObject.url,
-		'@__topUrl_sixth_value__@': utils.domanize(sixthUrlObject.url),
+		'@__topUrl_sixth_value__@': sanitiseUrl(sixthUrlObject.url),
 		'@__topUrl_sixth_hits__@': sixthUrlObject.count,
 		'@__topUrl_seventh_link__@': seventhUrlObject.url,
-		'@__topUrl_seventh_value__@': utils.domanize(seventhUrlObject.url),
+		'@__topUrl_seventh_value__@': sanitiseUrl(seventhUrlObject.url),
 		'@__topUrl_seventh_hits__@': seventhUrlObject.count,
 		'@__topUrl_eigth_link__@': eigthUrlObject.url,
-		'@__topUrl_eigth_value__@': utils.domanize(eigthUrlObject.url),
+		'@__topUrl_eigth_value__@': sanitiseUrl(eigthUrlObject.url),
 		'@__topUrl_eigth_hits__@': eigthUrlObject.count,
 		'@__topUrl_ninth_link__@': ninthUrlObject.url,
-		'@__topUrl_ninth_value__@': utils.domanize(ninthUrlObject.url),
+		'@__topUrl_ninth_value__@': sanitiseUrl(ninthUrlObject.url),
 		'@__topUrl_ninth_hits__@': ninthUrlObject.count,
 		'@__topUrl_tenth_link__@': tenthUrlObject.url,
-		'@__topUrl_tenth_value__@': utils.domanize(tenthUrlObject.url),
+		'@__topUrl_tenth_value__@': sanitiseUrl(tenthUrlObject.url),
 		'@__topUrl_tenth_hits__@': tenthUrlObject.count
 	};
 }
