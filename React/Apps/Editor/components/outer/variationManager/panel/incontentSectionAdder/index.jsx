a99cff09b4077b3eb50986b742d1cf57e73af9af
YomeshAdPushup <yomesh.gupta@adpushup.com>
2017-12-08 16:55:20 +0530

    Position compulsory for Geniee and partner data removed

--- React/Apps/Editor/components/outer/variationManager/panel/incontentSectionAdder/index.jsx
+++ React/Apps/Editor/components/outer/variationManager/panel/incontentSectionAdder/index.jsx
@@ -124,21 +124,20 @@ const mapStateToProps = (state, ownProps) => ({
 				);
 				return false;
 			}
-			console.log(networkInfo);
 			const notNear = getNotNearData(values.notNear),
-				isCustomZoneId = !!values.customZoneId,
+				// isCustomZoneId = !!values.customZoneId,
 				sectionPayload = {
 					sectionNo: values.section,
 					name: values.name,
 					minDistanceFromPrevAd: values.minDistanceFromPrevAd,
 					float: values.float,
-					notNear,
-					partnerData: {
-						position: 0,
-						firstFold: 0,
-						asyncTag: 1,
-						customZoneId: values.customZoneId ? values.customZoneId : ''
-					}
+					notNear
+					// partnerData: {
+					// 	position: 0,
+					// 	firstFold: 0,
+					// 	asyncTag: 1,
+					// 	customZoneId: values.customZoneId ? values.customZoneId : ''
+					// }
 				},
 				adPayload = {
 					adCode: btoa(values.adCode),
@@ -147,7 +146,7 @@ const mapStateToProps = (state, ownProps) => ({
 					networkData: {}
 				};
 
-			isCustomZoneId ? (adPayload.networkData.zoneId = values.customZoneId) : null;
+			// isCustomZoneId ? (adPayload.networkData.zoneId = values.customZoneId) : null;
 
 			adPayload.networkData = {
 				...adPayload.networkData,
