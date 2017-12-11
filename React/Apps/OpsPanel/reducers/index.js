654d8a94411df1181f24c95cc96ae27a22caac6e
YomeshAdPushup <yomesh.gupta@adpushup.com>
2017-12-11 16:22:01 +0530

    LiveSitesMapping Reducer and Actions added. Bug fixes

--- React/Apps/OpsPanel/reducers/index.js
+++ React/Apps/OpsPanel/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import sites from './siteMappingReducer';
+import liveSites from './liveSitesMappingReducer';
 
 export default combineReducers({
-	sites
+	sites,
+	liveSites
 });
