fcc104c725f520aaba624b30e3befb4f1862f2c5
ArunAdPushup <arun.dsouza@adpushup.com>
2017-12-11 17:08:04 +0530

    Basic events module added

--- services/interactiveAds/src/commonConsts.js
+++ services/interactiveAds/src/commonConsts.js
@@ -1,10 +1,13 @@
 // Common constants
 
 var commonConsts = {
-    EVENTS: {
-        PAGE_LOAD: 'load',
-        DOM_LOAD: 'DOMContentLoaded'
-    }
+	EVENTS: {
+		PAGE_LOAD: 'load',
+		DOM_LOAD: 'DOMContentLoaded'
+	},
+	SIZES: {
+		STICKY_FOOTER: [729, 90]
+	}
 };
 
 module.exports = commonConsts;
