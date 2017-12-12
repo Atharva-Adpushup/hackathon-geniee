0d3861caf42ebce5d7f82892197df12e4a9671c6
ZahinAdPushup <zahin@adpushup.com>
2017-12-13 05:13:13 +0530

    [Code changes] (Frontend: OPS Panel charts) Following changes are done:
    - scss/layout/_opsPanel SCSS: Replaced fixed height with min height for charts container
    - OpsPanel/components/MetricCharts/NetworkWise JS: Added high charts render logic
    - OpsPanel/configs/commonConsts JS: Added high charts (Line and PieCharts) config

--- React/Apps/OpsPanel/components/MetricCharts/NetworkWise.js
+++ React/Apps/OpsPanel/components/MetricCharts/NetworkWise.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import { Panel } from 'react-bootstrap';
 import moment from 'moment';
+import _ from 'lodash';
+import extend from 'extend';
+import ReactHighCharts from 'react-highcharts';
 import PaneLoader from '../../../../Components/PaneLoader.jsx';
+import { LINE_CHART_CONFIG } from '../../configs/commonConsts';
 
 const FooterTitle = <h4>Network Wise Performance Chart</h4>;
+
+function generateHighChartConfig(inputData, metricName) {
+	const chartConfig = extend(true, {}, LINE_CHART_CONFIG),
+		categories = [],
+		contributionData = extend(true, {}, inputData.dayWise);
+
+	chartConfig.yAxis.title.text = `${metricName.toUpperCase()} ($)`;
+
+	_.forOwn(contributionData, (adNetworkDayWiseReport, adNetworkKey) => {
+		const seriesObject = {
+				name: adNetworkKey,
+				data: []
+			},
+			// Below condition is added to avoid 'categories' array stuffed with redundant
+			// day values.
+			isCategoriesValidLength = !!(categories && categories.length + 1 <= 7),
+			isChartConfigCategoriesEmptyLength = !!chartConfig.xAxis.categories.length;
+
+		_.forOwn(adNetworkDayWiseReport, (dayWiseObject, dateKey) => {
+			if (isCategoriesValidLength) {
+				const dayCategory = moment(dateKey).format('MMM DD');
+				categories.push(dayCategory);
+			}
+
+			seriesObject.data.push(dayWiseObject[metricName]);
+		});
+
+		chartConfig.series.push(seriesObject);
+
+		if (isCategoriesValidLength || !isChartConfigCategoriesEmptyLength) {
+			chartConfig.xAxis.categories = chartConfig.xAxis.categories.concat(categories);
+		}
+	});
+
+	return chartConfig;
+}
 
 class NetworkWise extends Component {
 	constructor(props) {
@@ -18,6 +58,7 @@ class NetworkWise extends Component {
 
 		this.state = {
 			isDataLoaded,
+			data: isDataLoaded ? this.props.data : null,
 			startDate: moment()
 				.subtract(7, 'days')
 				.startOf('day'),
@@ -25,6 +66,7 @@ class NetworkWise extends Component {
 				.startOf('day')
 				.subtract(1, 'day')
 		};
+		this.renderHighCharts = this.renderHighCharts.bind(this);
 	}
 
 	componentDidMount() {
@@ -39,11 +81,23 @@ class NetworkWise extends Component {
 
 	componentWillReceiveProps(nextProps) {
 		let isDataLoaded =
-			nextProps.data && Object.keys(nextProps.data).length && nextProps.data.aggregated && nextProps.data.dayWise
-				? true
-				: false;
+				nextProps.data &&
+				Object.keys(nextProps.data).length &&
+				nextProps.data.aggregated &&
+				nextProps.data.dayWise
+					? true
+					: false,
+			data = isDataLoaded ? Object.assign(nextProps.data) : null;
 
-		this.setState({ isDataLoaded });
+		this.setState({ isDataLoaded, data });
+	}
+
+	renderHighCharts() {
+		let inputData = this.state.data,
+			metricName = 'cpm',
+			config = generateHighChartConfig(inputData, metricName);
+
+		return <ReactHighCharts config={config} />;
 	}
 
 	render() {
@@ -51,7 +105,7 @@ class NetworkWise extends Component {
 
 		return (
 			<Panel className="mb-20 metricsChart" header={FooterTitle}>
-				{this.state.isDataLoaded ? 'Metrics Chart will be shown here' : <PaneLoader />}
+				{this.state.isDataLoaded ? this.renderHighCharts() : <PaneLoader />}
 			</Panel>
 		);
 	}
