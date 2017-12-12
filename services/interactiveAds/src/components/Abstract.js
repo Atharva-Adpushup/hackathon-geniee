326caed2a80b59bff4ae3df4842570d164f8eed9
YomeshAdPushup <yomesh.gupta@adpushup.com>
2017-12-12 18:27:34 +0530

    Added StickyFooter, Abstract compoents, Preact

---
+++ services/interactiveAds/src/components/Abstract.js
@@ -0,0 +1,16 @@
+import { h, Component } from 'preact';
+
+class Abstract extends Component {
+	constructor(props) {
+		super(props);
+		this.width = 720;
+		this.height = 90;
+		this.adCode = null;
+	}
+	/*
+	show() {}
+	hide() {}
+	*/
+}
+
+export default Abstract;
