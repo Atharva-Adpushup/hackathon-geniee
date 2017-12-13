430570a68c2147e0094a870c47c0a6b6ac317405
ArunAdPushup <arun.dsouza@adpushup.com>
2017-12-13 17:03:13 +0530

    Lazy load enabled for adpPageAds script + config based model added

--- services/interactiveAds/src/events.js
+++ services/interactiveAds/src/events.js
@@ -1,16 +1,20 @@
 // Events module
 
-var commonConsts = require('./commonConsts'),
-	emitter = require('./emitter');
+import commonConsts from './commonConsts';
+import emitter from './emitter';
+// import $ from 'jquery';
 
-window.addEventListener(commonConsts.EVENTS.PAGE_LOAD, function(data) {
-	emitter.publish(commonConsts.EVENTS.PAGE_LOAD, data);
-});
+(w => {
+	// $(w).scroll(data => {
+	// 	emitter.publish(commonConsts.EVENTS.SCROLL, data);
+	// });
 
-window.addEventListener(commonConsts.EVENTS.DOM_LOAD, function(data) {
-	emitter.publish(commonConsts.EVENTS.DOM_LOAD, data);
-});
+	// w.addEventListener(commonConsts.EVENTS.PAGE_LOAD, function(data) {
+	// 	emitter.publish(commonConsts.EVENTS.PAGE_LOAD, data);
+	// });
 
-window.addEventListener(commonConsts.EVENTS.SCROLL, function(data) {
-	emitter.publish(commonConsts.EVENTS.SCROLL, data);
-});
+	w.addEventListener(commonConsts.EVENTS.DOM_LOAD, function(data) {
+		console.log('DOM loaded');
+		emitter.publish(commonConsts.EVENTS.DOM_LOAD, data);
+	});
+})(window);
