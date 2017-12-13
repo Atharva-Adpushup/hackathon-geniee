4206b979c1f651262e12efc63490287a994e044f
YomeshAdPushup <yomesh.gupta@adpushup.com>
2017-12-13 18:48:25 +0530

    Interactive UI Custom CSS added

--- React/Apps/Editor/components/outer/variationManager/panel/interactiveAds/adpushupAds.js
+++ React/Apps/Editor/components/outer/variationManager/panel/interactiveAds/adpushupAds.js
@@ -20,18 +20,21 @@ class AdPushupAds extends Component {
 			placement: false,
 			format: false,
 			size: false,
+			css: {},
 			containsADP: true,
 			adpKeyValues: this.props.variation.adpKeyValues || null
 		};
-		this.checkADP = this.checkADP.bind(this);
+		// this.checkADP = this.checkADP.bind(this);
 		this.submitHandler = this.submitHandler.bind(this);
 		this.eventChangeHandler = this.eventChangeHandler.bind(this);
 		this.formatChangeHandler = this.formatChangeHandler.bind(this);
 		this.sizeChangeHandler = this.sizeChangeHandler.bind(this);
+		this.cssChangeHandler = this.cssChangeHandler.bind(this);
 		this.renderEventOptions = this.renderEventOptions.bind(this);
 		this.renderFormatSelect = this.renderFormatSelect.bind(this);
 		this.renderFormatOptions = this.renderFormatOptions.bind(this);
 		this.renderInput = this.renderInput.bind(this);
+		this.renderNetwork = this.renderNetwork.bind(this);
 	}
 
 	// componentWillReceiveProps(nextProps) {
@@ -65,13 +68,17 @@ class AdPushupAds extends Component {
 		});
 	}
 
+	cssChangeHandler(css) {
+		this.setState({ css });
+	}
+
 	renderInput(label, name, type, value) {
 		return (
 			<Row className="mT-15">
-				<Col xs={6}>
+				<Col xs={5}>
 					<strong>{label}</strong>
 				</Col>
-				<Col xs={6}>
+				<Col xs={7}>
 					<input
 						className="inputMinimal"
 						type={type}
@@ -111,14 +118,16 @@ class AdPushupAds extends Component {
 	}
 
 	renderFormatSelect() {
+		let limit = this.props.platform == 'DESKTOP' ? 2 : 1,
+			types = interactiveAds.types.slice(0, limit);
 		return (
 			<Row className="mT-15">
-				<Col xs={6}>
+				<Col xs={5}>
 					<strong>Select Format : </strong>
 				</Col>
-				<Col xs={6}>
+				<Col xs={7}>
 					<SelectBox value={this.state.format} label="Select Format" onChange={this.formatChangeHandler}>
-						{interactiveAds.types.map((item, index) => (
+						{types.map((item, index) => (
 							<option key={index} value={item}>
 								{item.charAt(0).toUpperCase() + item.slice(1).replace(/([A-Z])/g, ' $1')}
 							</option>
@@ -131,49 +140,54 @@ class AdPushupAds extends Component {
 
 	renderFormatOptions() {
 		return (
-			<Row className="mT-15">
-				<Col xs={6}>
-					<strong>Select Size : </strong>
-				</Col>
-				<Col xs={6}>
-					<SelectBox value={this.state.size} label="Select Format" onChange={this.sizeChangeHandler}>
-						{interactiveAds.sizes[this.props.platform][this.state.type][this.state.placement].map(
-							(item, index) => (
-								<option key={index} value={item}>
-									{item}
-								</option>
-							)
-						)}
-					</SelectBox>
-				</Col>
-				<Col xs={6}>
-					<strong>Custom CSS</strong>
-				</Col>
-				<Col xs={6}>
-					<CodeBox
-						customId={`${this.props.variation.id}adpKeyValues`}
-						showButtons
-						textEdit
-						parentExpanded={this.props.ui.variationPanel.expanded}
-						textEditBtn="Save Key Values"
-						code={this.state.adpKeyValues ? JSON.stringify(this.state.adpKeyValues) : '{}'}
-						onSubmit={this.submitHandler}
-					/>
-				</Col>
-			</Row>
+			<div>
+				<Row className="mT-15">
+					<Col xs={5}>
+						<strong>Select Size : </strong>
+					</Col>
+					<Col xs={7}>
+						<SelectBox value={this.state.size} label="Select Size" onChange={this.sizeChangeHandler}>
+							{interactiveAds.sizes[this.props.platform][this.state.type][this.state.placement].map(
+								(item, index) => (
+									<option key={index} value={item}>
+										{item}
+									</option>
+								)
+							)}
+						</SelectBox>
+					</Col>
+				</Row>
+				<Row className="mT-15">
+					<Col xs={5}>
+						<strong>Custom CSS : </strong>
+					</Col>
+					<Col xs={7}>
+						<CodeBox
+							customId={`${this.props.variation.id}interactiveAds`}
+							showButtons={false}
+							textEdit
+							parentExpanded={this.props.ui.variationPanel.expanded}
+							code={this.state.css ? JSON.stringify(this.state.css) : '{}'}
+							onChange={this.cssChangeHandler}
+						/>
+					</Col>
+				</Row>
+			</div>
 		);
 	}
 
-	checkADP(sections) {
-		return sections
-			? sections.some(section => {
-					let ad = section.ads[0];
-					if (ad.network && ad.network == 'adpTags') {
-						return true;
-					}
-				})
-			: false;
-	}
+	// checkADP(sections) {
+	// 	return sections
+	// 		? sections.some(section => {
+	// 				let ad = section.ads[0];
+	// 				if (ad.network && ad.network == 'adpTags') {
+	// 					return true;
+	// 				}
+	// 			})
+	// 		: false;
+	// }
+
+	renderNetwork() {}
 
 	submitHandler(value) {
 		try {
@@ -196,13 +210,13 @@ class AdPushupAds extends Component {
 
 	render() {
 		return this.state.containsADP ? (
-			<div className="pdAll-10">
+			<div style={{ padding: '20px 0px' }}>
 				<Col xs={7}>
 					<Row>
-						<Col xs={6}>
+						<Col xs={5}>
 							<strong>Select Event : </strong>
 						</Col>
-						<Col xs={6}>
+						<Col xs={7}>
 							<div className="interactiveAdsRow">
 								<SelectBox
 									value={this.state.event}
@@ -220,8 +234,10 @@ class AdPushupAds extends Component {
 					</Row>
 					{this.state.event ? this.renderEventOptions() : null}
 					{this.state.event ? this.renderFormatSelect() : null}
-					{this.state.format ? this.renderFormatOptions() : null}
+					{this.state.event && this.state.format ? this.renderFormatOptions() : null}
+					{this.renderNetwork()}
 				</Col>
+				<div style={{ clear: 'both' }}>&nbsp;</div>
 			</div>
 		) : (
 			<div>Variation must contain one ADP section in order to set Key-Values</div>
