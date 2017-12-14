3de18e4567e2dcbfbaeb8854748f9d92b09d67df
ZahinAdPushup <zahin@adpushup.com>
2017-12-14 18:12:22 +0530

    [Code changes] (App: OPS Panel charts) Global Metrics chart implementation added

--- React/Apps/OpsPanel/containers/globalMetricChartsContainer.js
+++ React/Apps/OpsPanel/containers/globalMetricChartsContainer.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
-import { fetchNetworkWiseData } from '../actions/globalMetricChartsActions';
+import { fetchNetworkWiseData, fetchMetricsData } from '../actions/globalMetricChartsActions';
 import ModuleWrapper from '../components/ModuleWrapper.jsx';
 
 const mapStateToProps = (state, ownProps) => ({
 		charts: state.globalMetricCharts
 	}),
 	mapDispatchToProps = dispatch => ({
-		fetchNetworkWiseData: params => dispatch(fetchNetworkWiseData(params))
+		fetchNetworkWiseData: params => dispatch(fetchNetworkWiseData(params)),
+		fetchMetricsData: params => dispatch(fetchMetricsData(params))
 	});
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModuleWrapper);
