e94bf4b649b9a61e00693ecd00efaee6b7be8874
ZahinAdPushup <zahin@adpushup.com>
2017-12-19 18:21:08 +0530

    [Code changes] (App: OPS panel charts) Top 10 sites added

--- controllers/opsController.js
+++ controllers/opsController.js
@@ -9,7 +9,8 @@ const express = require('express'),
 		getGlobalNetworkWiseDataContributionReport,
 		getGlobalMetricsDataContributionReport,
 		getGlobalModeWiseTrafficContributionReport,
-		getGlobalTop10CountriesContributionQuery
+		getGlobalTop10CountriesContributionQuery,
+		getGlobalTop10SitesContributionReport
 	} = require('../helpers/commonFunctions'),
 	router = express.Router(),
 	appBucket = couchbaseService(
@@ -189,6 +190,43 @@ router
 			});
 
 		return getGlobalTop10CountriesContributionQuery(params)
+			.then(responseData => {
+				response.data = responseData;
+				response.data.dateFormat = dateFormatCollection;
+				return res.send(response);
+			})
+			.catch(err => {
+				return res.send(Object.assign(response, { error: true }));
+			});
+	})
+	.post('/getGlobalTop10Sites', (req, res) => {
+		let response = {
+				error: false,
+				data: []
+			},
+			bodyParameters = req.body,
+			params = {
+				transform: true,
+				fromDate:
+					bodyParameters && bodyParameters.fromDate
+						? moment(bodyParameters.fromDate).format('YYYY-MM-DD')
+						: moment()
+								.subtract(7, 'days')
+								.format('YYYY-MM-DD'),
+				toDate:
+					bodyParameters && bodyParameters.toDate
+						? moment(bodyParameters.toDate).format('YYYY-MM-DD')
+						: moment()
+								.subtract(1, 'days')
+								.format('YYYY-MM-DD')
+			},
+			dateFormatCollection = utils.getDateFormatCollection({
+				fromDate: params.fromDate,
+				toDate: params.toDate,
+				format: 'MMM DD'
+			});
+
+		return getGlobalTop10SitesContributionReport(params)
 			.then(responseData => {
 				response.data = responseData;
 				response.data.dateFormat = dateFormatCollection;
