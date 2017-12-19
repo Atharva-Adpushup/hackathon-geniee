4f105fdef37e2822844a541dad8c50f3be4440bb
ZahinAdPushup <zahin@adpushup.com>
2017-12-19 18:21:08 +0530

    [Code changes] (App: OPS panel charts) Top 10 sites added

--- React/Apps/OpsPanel/components/MetricChartPanels.jsx
+++ React/Apps/OpsPanel/components/MetricChartPanels.jsx
@@ -5,6 +5,7 @@ import Metrics from './MetricCharts/Metrics';
 import NetworkWise from './MetricCharts/NetworkWise';
 import ModeWiseTraffic from './MetricCharts/ModeWiseTraffic';
 import Top10Countries from './MetricCharts/Top10Countries';
+import Top10Sites from './MetricCharts/Top10Sites';
 
 const MetricChartPanels = props => {
 	return (
@@ -12,6 +13,7 @@ const MetricChartPanels = props => {
             <div className="pd-20">
                 <Metrics fetchData={props.fetchMetricsData} data={props.charts.metrics} />
                 <NetworkWise fetchData={props.fetchNetworkWiseData} data={props.charts.networkWise} />
+                <Top10Sites fetchData={props.fetchTop10SitesData} data={props.charts.top10Sites} />
                 <ModeWiseTraffic fetchData={props.fetchModeWiseTrafficData} data={props.charts.modeWiseTraffic} />
                 <Top10Countries fetchData={props.fetchTop10CountriesData} data={props.charts.top10Countries} />
             </div>
