1fabe12f10d834974f00c16e3459035b8920d355
ZahinAdPushup <zahin@adpushup.com>
2017-12-21 15:55:33 +0530

    [Code changes] (Frontend: OPS Panel charts) Charts: Fixed date range selection bug

--- React/Apps/OpsPanel/components/MetricCharts/ModeWiseTraffic.js
+++ React/Apps/OpsPanel/components/MetricCharts/ModeWiseTraffic.js
@@ -45,12 +45,8 @@ class ModeWiseTraffic extends Component {
 		this.state = {
 			isDataLoaded,
 			data: isDataLoaded ? this.props.data : null,
-			startDate: moment()
-				.subtract(7, 'days')
-				.startOf('day'),
-			endDate: moment()
-				.startOf('day')
-				.subtract(1, 'day')
+			startDate: moment().subtract(7, 'days'),
+			endDate: moment().subtract(1, 'days')
 		};
 		this.renderHighCharts = this.renderHighCharts.bind(this);
 		this.generateHeaderTitle = this.generateHeaderTitle.bind(this);
