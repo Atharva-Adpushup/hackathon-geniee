e08939bed7c3bd1f20372e3c38c6479adfc690ff
Arun Michael Dsouza <arun.dsouza@adpushup.com>
2018-01-03 16:01:04 +0530

    Demo report site id updated

--- configs/commonConsts.js
+++ configs/commonConsts.js
@@ -3,7 +3,7 @@ module.exports = {
 	BASE_URL: 'http://console.adpushup.com',
 	PROXY_ORIGIN: 'http://proxy.app.adpushup.com',
 	DEMO_ACCOUNT_EMAIL: 'demo@adpushup.com',
-	DEMO_REPORT_SITE_ID: 28822,
+	DEMO_REPORT_SITE_ID: 31764,
 	DEMO_PAGEGROUPS: ['MIC', 'WEBCAM'],
 	REPORT_API: {
 		SELECT_PARAMS: ['total_requests', 'total_impressions', 'total_revenue', 'report_date', 'siteid'],
