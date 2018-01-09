f679a7ee7767c6a7291b9d0ebbc145350031dd36
ZahinAdPushup <zahin@adpushup.com>
2018-01-10 00:25:56 +0530

    [Code changes] (Frontend: Site Vitals) Following changes are done:
    - OpsPanel/components/SiteMetricChartPanels JSX: Added Browser Wise Traffic chart component
    - OpsPanel/components/MetricCharts/BrowserWiseTraffic JS: Component implementation added

--- React/Apps/OpsPanel/components/SiteMetricChartPanels.jsx
+++ React/Apps/OpsPanel/components/SiteMetricChartPanels.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Panel } from 'react-bootstrap';
+import BrowserWiseTraffic from './MetricCharts/BrowserWiseTraffic';
 
 const SiteMetricChartPanels = props => {
 	return (
             <div className="pd-20">
+                <BrowserWiseTraffic data={{}} />
             </div>
 	);
 };
