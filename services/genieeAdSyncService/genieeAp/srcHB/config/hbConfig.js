6ef16a0a4081d4948ab316a0e1163e41dac9efdb
ArunAdPushup <arun.dsouza@adpushup.com>
2018-01-29 16:02:14 +0530

    srcHB removed

--- services/genieeAdSyncService/genieeAp/srcHB/config/hbConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'siteId': __HB_SITE_ID__,
-	'siteDomains': __HB_SITE_DOMAINS__,
-	'biddingPartners': __HB_BIDDING_PARTNERS__,
-	'prebidTimeout': __HB_PREBID_TIMEOUT__,
-	'e3FeedbackUrl': __HB_FEEDBACK_URL__
-};
