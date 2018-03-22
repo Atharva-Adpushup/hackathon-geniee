4e85a06ee53baaab508900288cae00e1b074dc1d
ArunAdPushup <arun.dsouza@adpushup.com>
2018-03-22 16:07:05 +0530

    CDN upload script updated

--- services/interactiveAds/cdnUploadService/index.js
+++ services/interactiveAds/cdnUploadService/index.js
@@ -1,38 +1,49 @@
 const config = require('../../../configs/config'),
-    path = require('path'),
-    fs = require('fs'),
-    Promise = require('bluebird'),
-    readFile = Promise.promisify(fs.readFile),
-    PromiseFtp = require('promise-ftp'),
-    adpInteractiveScriptName = 'adpInteractiveAdstest',
-    adpInteractiveTplPath = path.join(__dirname, '..', '..', '..', 'public', 'assets', 'js', 'builds', 'adpInteractiveAds.min.js'),
-    ftp = new PromiseFtp(),
-    getjSFile = readFile(adpInteractiveTplPath, 'utf8'),
-    connectToServer = () => {
-        if (ftp.getConnectionStatus() === 'connected') {
-            return Promise.resolve(true);
-        }
-        return ftp.connect({
-            host: config.cacheFlyFtp.HOST,
-            user: config.cacheFlyFtp.USERNAME,
-            password: config.cacheFlyFtp.PASSWORD
-        });
-    },
-    uploadToCDN = function () {
-        return connectToServer()
-            .then(() => getjSFile)
-            .then(jsFileContents => {
-                return Promise.all([
-                    ftp.put(jsFileContents, `${adpInteractiveScriptName}.js`),
-                    ftp.put(jsFileContents, `${adpInteractiveScriptName}.min.js`)
-                ]);
-            });
-    };
+	path = require('path'),
+	fs = require('fs'),
+	Promise = require('bluebird'),
+	readFile = Promise.promisify(fs.readFile),
+	PromiseFtp = require('promise-ftp'),
+	adpInteractiveScriptName = 'adpInteractiveAdstest',
+	adpInteractiveTplPath = path.join(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'public',
+		'assets',
+		'js',
+		'builds',
+		'adpInteractiveAds.min.js'
+	),
+	ftp = new PromiseFtp(),
+	getjSFile = readFile(adpInteractiveTplPath, 'utf8'),
+	connectToServer = () => {
+		if (ftp.getConnectionStatus() === 'connected') {
+			return Promise.resolve(true);
+		}
+		return ftp.connect({
+			host: config.cacheFlyFtp.HOST,
+			user: config.cacheFlyFtp.USERNAME,
+			password: config.cacheFlyFtp.PASSWORD
+		});
+	},
+	uploadToCDN = function() {
+		return connectToServer()
+			.then(() => getjSFile)
+			.then(jsFileContents => {
+				return Promise.all([
+					ftp.put(jsFileContents, `${adpInteractiveScriptName}.js`),
+					ftp.put(jsFileContents, `${adpInteractiveScriptName}.min.js`)
+				]);
+			});
+	};
 
 return uploadToCDN()
-    .then(() => {
-        console.log('Adp interactive script uploaded to CDN');
-        process.exit();
-    }).catch(err => {
-        throw new Error(err);
-    });
+	.then(() => {
+		console.log('Adp interactive script uploaded to CDN');
+		process.exit();
+	})
+	.catch(err => {
+		throw new Error(err);
+	});
