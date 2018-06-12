776ccf8c1a46308c9005c1e5e0a1fff5e94ee0c9
ArunAdPushup <arun.dsouza@adpushup.com>
2018-06-12 21:06:04 +0530

    Feedback call change

--- services/adpTags/hbScript/src/feedback.js
+++ services/adpTags/hbScript/src/feedback.js
@@ -100,9 +100,9 @@ var logger = require('../helpers/logger'),
 					winner: config.ADSENSE.bidderName
 				});
 		}
-		if (feedback.data.winner && feedback.data.winner !== config.DEFAULT_WINNER) {
-			utils.sendDataToKeenIO(feedback);
-		}
+		//if (feedback.data.winner && feedback.data.winner !== config.DEFAULT_WINNER) {
+		utils.sendDataToKeenIO(feedback);
+		//}
 		logger.log(
 			'Winner for div ' + feedback.data.containerId + ': ' + feedback.data.winner,
 			feedback.data.winningRevenue * 1000
