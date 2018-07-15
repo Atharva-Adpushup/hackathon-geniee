1636baeb1bea2785f46c35186415f007bc5d7d2f
ZahinAdPushup <zahin@adpushup.com>
2018-07-16 02:28:18 +0530

    [Code changes] (Frontend: Visual Editor) Multiple Ad Sizes: Following changes are done
    - EditMenu/adDescriptor JSX: Added relevant props to child components
    - EditMenu/AdDetails JSX: Added 'change ad size' UI functionality in edit menu screen for structural sections
    - EditMenu/index JSX: Added relevant props to child components
    - Consts/commonConsts JS: Added relevant constant property
    - Containers/editMenuContainer JS: Added post message helper for 'change ad size' functionality, formatting changes
    - Containers/insertMenuContainer JS: Formatting changes
    - Inner/domManager JS: 'change ad size' DOM specific functionality
    - Inner/messengerHelper JS: 'change ad size' post message listener & action added

--- React/Apps/Editor/containers/editMenuContainer.js
+++ React/Apps/Editor/containers/editMenuContainer.js
@@ -1,15 +1,21 @@
 import { connect } from 'react-redux';
 import EditMenu from 'editMenu/index';
 import { hideEditMenu as hideMenu } from 'actions/uiActions';
+import { getActiveChannelId } from 'selectors/channelSelectors';
 import { getEditMenuState } from 'selectors/uiSelectors';
 import { getSectionWithAds } from 'selectors/sectionSelectors';
 import { messengerCommands } from 'consts/commonConsts';
+import { sendMessage } from '../scripts/messengerHelper';
 import { updatePartnerData } from 'actions/sectionActions';
 import { showNotification } from 'actions/uiActions';
 
 const mapStateToProps = state => {
 		const json = getEditMenuState(state);
-		return { ...json, section: getSectionWithAds(state, { sectionId: json.sectionId }) };
+		return {
+			...json,
+			section: getSectionWithAds(state, { sectionId: json.sectionId }),
+			channelId: getActiveChannelId(state)
+		};
 	},
 	mapDispatchToProps = dispatch => ({
 		hideMenu: () => {
@@ -18,7 +24,13 @@ const mapStateToProps = state => {
 		updateSettings: (sectionId, adId, partnerData) => {
 			dispatch(updatePartnerData(sectionId, adId, partnerData));
 		},
+		updateAdSize: (channelId, adId, sizeObject) => {
+			sendMessage(channelId, messengerCommands.UPDATE_AD_SIZE, { adId, sizeObject });
+		},
 		showNotification: params => dispatch(showNotification(params))
 	});
 
-export default connect(mapStateToProps, mapDispatchToProps)(EditMenu);
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(EditMenu);
