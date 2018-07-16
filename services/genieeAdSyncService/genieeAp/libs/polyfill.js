2031aad057ff5a9d8bb74f812c6fd00fbdc0b89c
ArunAdPushup <arun.dsouza@adpushup.com>
2018-07-16 16:56:22 +0530

    Unwanted window.btoa polyfill removed from polyfills script. Polyfill is already being added by the Base64 package used in utils.

--- services/genieeAdSyncService/genieeAp/libs/polyfill.js
+++ services/genieeAdSyncService/genieeAp/libs/polyfill.js
@@ -53,43 +53,6 @@ if (!Object.values) {
 	};
 }
 
-if (!window.btoa) {
-	var object = window;
-
-	var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-
-	function InvalidCharacterError(message) {
-		this.message = message;
-	}
-	InvalidCharacterError.prototype = new Error();
-	InvalidCharacterError.prototype.name = 'InvalidCharacterError';
-
-	// encoder
-	// [https://gist.github.com/999166] by [https://github.com/nignag]
-	object.btoa = function(input) {
-		var str = String(input);
-		for (
-			// initialize result and counter
-			var block, charCode, idx = 0, map = chars, output = '';
-			// if the next str index does not exist:
-			//   change the mapping table to "="
-			//   check if d has no fractional digits
-			str.charAt(idx | 0) || ((map = '='), idx % 1);
-			// "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
-			output += map.charAt(63 & (block >> (8 - (idx % 1) * 8)))
-		) {
-			charCode = str.charCodeAt((idx += 3 / 4));
-			if (charCode > 0xff) {
-				throw new InvalidCharacterError(
-					"'btoa' failed: The string to be encoded contains characters outside of the Latin1 range."
-				);
-			}
-			block = (block << 8) | charCode;
-		}
-		return output;
-	};
-}
-
 if (!Array.isArray) {
 	Array.isArray = function(arg) {
 		return Object.prototype.toString.call(arg) === '[object Array]';
