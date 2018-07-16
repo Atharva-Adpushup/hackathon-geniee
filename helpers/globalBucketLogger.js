2f8edba9425ab7c26562e37538779a7f30084db5
ZahinAdPushup <zahin@adpushup.com>
2018-07-16 17:12:56 +0530

    [Code changes] (Backend: Loggers) GlobalBucketLogger JS: Added 30 days expiry parameter to logs

--- helpers/globalBucketLogger.js
+++ helpers/globalBucketLogger.js
@@ -8,21 +8,23 @@ const uuid = require('uuid'),
 	);
 
 function logger(logData) {
-	return dbHelper
-		.createDoc(
-			`slog::${uuid.v4()}`,
-			{
-				date: +new Date(),
-				source: logData.source || 'CONSOLE LOGS',
-				message: logData.message,
-				type: logData.type || 3,
-				details: logData.details || 'N/A',
-				debugData: logData.debugData || 'N/A'
-			},
-			{}
-		)
-		.then(() => console.log('Log written to apGlobalBucket'))
-		.catch(err => console.log('Error writing log to apGlobalBucket : ', err.message));
+    return dbHelper
+        .createDoc(
+            `slog::${uuid.v4()}`,
+            {
+                date: +new Date(),
+                source: logData.source || 'CONSOLE LOGS',
+                message: logData.message,
+                type: logData.type || 3,
+                details: logData.details || 'N/A',
+                debugData: logData.debugData || 'N/A'
+            },
+            {
+                expiry: Math.floor(new Date() / 1000) + 2592000
+            }
+        )
+        .then(() => console.log('Log written to apGlobalBucket'))
+        .catch(err => console.log('Error writing log to apGlobalBucket : ', err.message));
 }
 
 module.exports = logger;
