b0de9c8dcb328547cc501188837e4ac4914ed378
TanveeAdpushup <tanvee.gujral@adpushup.com>
2018-07-24 10:18:15 +0530

    added babel transform-class-properties plugin in webpack production

--- React/webpack-prod.js
+++ React/webpack-prod.js
@@ -248,7 +248,7 @@ module.exports = [
 					loader: 'babel-loader',
 					exclude: /node_modules/,
 					query: {
-						plugins: ['lodash'],
+						plugins: ['lodash', 'transform-class-properties'],
 						presets: ['es2015', 'react', 'stage-2']
 					}
 				},
