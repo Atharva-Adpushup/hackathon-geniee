5274ae0192e167971f3af3f9b7b97f35609344c8
ArunAdPushup <arun.dsouza@adpushup.com>
2018-08-16 18:47:26 +0530

    Element in viewport detection functioanlity added

--- services/interactiveAds/src/renderer/components/InView/index.js
+++ services/interactiveAds/src/renderer/components/InView/index.js
@@ -1,17 +1,33 @@
 // In-view ad component
 
-import commonConsts from '../../../commonConsts';
+import $ from '../../../$';
 import Component from '../Component';
 
 class InView extends Component {
 	constructor(parentNode, interactiveAd, adCode) {
 		super(parentNode, interactiveAd, adCode);
 
+		this.elementInViewport = this.elementInViewport.bind(this);
 		this.initScrollListener = this.initScrollListener.bind(this);
 	}
 
+	elementInViewport(el) {
+		var elementTop = $(el).offset().top;
+		var elementBottom = elementTop + $(el).outerHeight();
+
+		var viewportTop = $(window).scrollTop();
+		var viewportBottom = viewportTop + $(window).height();
+
+		return elementBottom > viewportTop && elementTop < viewportBottom;
+	}
+
 	initScrollListener(interactiveAd) {
-		console.log(interactiveAd);
+		const xPath = '.box_wrapper:eq(2)';
+
+		if (this.elementInViewport(xPath)) {
+			console.log(interactiveAd);
+		}
+
 		//parentNode = createParentNode(xPath, interactiveAd);
 		// const inView = new InView(parentNode, interactiveAd, adCode);
 	}
