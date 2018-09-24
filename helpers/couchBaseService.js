efa38ebbc5f509ce786118579a62c43ae44c3ef4
Zahin Omar Alwa <zahin@adpushup.com>
2018-09-24 16:52:50 +0530

    [Feature] (Backend: App) Couchbase V5 Upgrade: Following changes are done: - app JS: Replaced 'bucket, password' parameters with 'db' value, incremented timeout values to a reasonable limit - package JSON: Upgraded couchbase NodeJS sdk package version - helpers\couchBaseService JS: Added compatibility code

--- helpers/couchBaseService.js
+++ helpers/couchBaseService.js
@@ -1,7 +1,7 @@
 /**
  * Created by Dhiraj on 3/2/2016.
  */
-var couchbase = require('couchbase-promises'),
+var couchbase = require('couchbase'),
 	Promise = require('bluebird'),
 	config = require('../configs/config'),
 	API = null,
@@ -14,21 +14,23 @@ state.cluster = new couchbase.Cluster('couchbase://' + config.couchBase.HOST, {
 	operation_timeout: 5000
 });
 
-//TODO: Add functionality for connecting to remote host when host parameter is present
-// 'host' to be added as an optional third parameter
-function connect(bucket, password) {
+// RBAC (Role Based Access Control) Authentication,
+// See https://docs.couchbase.com/server/5.1/security/security-rbac-user-management.html
+state.cluster.authenticate(config.couchBase.DEFAULT_USER_NAME, config.couchBase.DEFAULT_USER_NAME);
+
+function connect(bucket) {
 	return new Promise(function(resolve, reject) {
 		if (state[bucket]) {
 			resolve(state[bucket]);
 			return;
 		}
 
-		state[bucket] = state.cluster.openBucket(bucket, password, function(err) {
+		state[bucket] = state.cluster.openBucket(bucket, function(err) {
 			if (err) {
 				reject(err);
 				return;
 			}
-			state[bucket] = Promise.promisifyAll(state[bucket], { suffix: 'Promise' });
+			state[bucket] = Promise.promisifyAll(state[bucket]);
 			resolve(state[bucket]);
 			return;
 		});
@@ -64,9 +66,10 @@ API = {
 	},
 	queryViewFromAppBucket: function(query) {
 		return API.connectToAppBucket().then(function(appBucket) {
-			return appBucket.queryPromise(query);
+			return appBucket.queryAsync(query);
 		});
-	}
+	},
+	cluster: state.cluster
 };
 
 module.exports = API;
