38e3d4fdd4901f70063065aa99067e2eb1e15ffc
ArunAdPushup <arun.dsouza@adpushup.com>
2018-10-08 17:54:31 +0530

    HB feedback endpoint updated

--- services/adpTags/hbScript/src/config.js
+++ services/adpTags/hbScript/src/config.js
@@ -43,7 +43,7 @@ module.exports = {
 		pixelId: 1236239
 	},
 	DEFAULT_WINNER: 'adx',
-	FEEDBACK_URL: '//apdc1-staging-1.eastus2.cloudapp.azure.com:8080/ApHbWebService/feedback',
+	FEEDBACK_URL: '//staging.adpushup.com/ApHbWebService/feedback',
 	POSTBID_PASSBACKS: {
 		'*': 'PGgxPkJPTyBZQUghPC9oMT4='
 	},
