96f839975d7f2842221e76489612b343f48e01ba
YomeshAdPushup <yomesh.gupta@adpushup.com>
2018-11-28 01:53:47 +0530

    Moved Editor Notifications to Bottom Right as Ops Feedback

--- React/Apps/Editor/components/outer/notifications/index.js
+++ React/Apps/Editor/components/outer/notifications/index.js
@@ -5,22 +5,11 @@ class Notification extends Component {
 	constructor(props) {
 		super(props);
 		this.createNotification = this.createNotification.bind(this);
-		// this.onAdd = this.onAdd.bind(this);
-		// this.onRemove = this.onRemove.bind(this);
 	}
 
 	componentDidMount() {
 		this._notificationSystem = this.refs.notificationSystem;
 	}
-
-	// onAdd(notification) {
-	// 	this.props.onAdd ? this.props.onAdd(notification) : null;
-	// }
-
-	// onRemove(notification) {
-	// 	this.props.onRemove ? this.props.onRemove(notification) : null;
-	// 	this.props.hideNotification();
-	// }
 
 	createNotification() {
 		const { mode, message, title } = this.props;
@@ -30,16 +19,24 @@ class Notification extends Component {
 			level: mode,
 			dismissible: true,
 			onRemove: notification => this.props.hideNotification()
-			// onAdd: notification => this.onAdd(notification),
-			// onRemove: notification => this.onRemove(notification)
 		});
 	}
 
 	render() {
+		const style = {
+			Containers: {
+				DefaultStyle: {
+					top: 'auto',
+					bottom: '0',
+					left: 'auto',
+					right: '0'
+				}
+			}
+		};
 		return (
 			<div style={{ position: 'relative', zIndex: 1000000 }}>
 				{this.props.isVisible ? this.createNotification() : null}
-				<NotificationSystem ref="notificationSystem" allowHTML={true} />
+				<NotificationSystem ref="notificationSystem" allowHTML={true} style={style} />
 			</div>
 		);
 	}
