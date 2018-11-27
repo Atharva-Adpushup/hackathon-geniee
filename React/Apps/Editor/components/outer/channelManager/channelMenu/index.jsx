b18bd0a4dfa895b8897db57dff1140a898fafe7d
YomeshAdPushup <yomesh.gupta@adpushup.com>
2018-11-27 13:19:11 +0530

    Converted ChannelMenu to Class Component for Active Item Handling

--- React/Apps/Editor/components/outer/channelManager/channelMenu/index.jsx
+++ React/Apps/Editor/components/outer/channelManager/channelMenu/index.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import Menu from 'shared/menu/menu.jsx';
 import MenuItem from 'shared/menu/menuItem.jsx';
 import Info from './info.jsx';
@@ -6,74 +6,101 @@ import CloseChannel from './closeChannel.jsx';
 import { uiCollections } from 'consts/commonConsts';
 import TrafficPanel from './trafficPanel.jsx';
 
-const channelMenu = ({
-	isVisible,
-	activeChannelId,
-	allTrafficDistributions,
-	editTrafficDistribution,
-	position,
-	hideMenu,
-	saveSampleUrl,
-	updateAutoptimize,
-	channel,
-	partner,
-	closeChannel,
-	changeContentSelector
-}) => {
-	if (!isVisible) {
-		return null;
+class channelMenu extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			activeItem: 0
+		};
+		this.updateActiveItem = this.updateActiveItem.bind(this);
 	}
 
-	const items = [],
-		closeChannelById = () => {
-			closeChannel(activeChannelId);
-		},
-		saveTrafficDistributions = config => {
-			Object.keys(config).forEach(key => {
-				const value = config[key];
-				editTrafficDistribution(key, value);
-			});
-		},
-		trafficDistributionConfig = {
-			description: uiCollections.trafficDistribution.description,
-			sumMismatchErrorMessage: uiCollections.trafficDistribution.errorMessage.sumMismatch
-		};
+	updateActiveItem(value) {
+		this.setState({ activeItem: value });
+	}
 
-	trafficDistributionConfig.sumMismatchErrorMessage = trafficDistributionConfig.sumMismatchErrorMessage.map(
-		(text, id) => <span key={`td-error-${id}`}>{text}</span>
-	);
-	trafficDistributionConfig.sumMismatchErrorMessage.push(<strong key="td-error-max-value">100</strong>);
+	render() {
+		const {
+			isVisible,
+			activeChannelId,
+			allTrafficDistributions,
+			editTrafficDistribution,
+			position,
+			hideMenu,
+			saveSampleUrl,
+			updateAutoptimize,
+			channel,
+			partner,
+			closeChannel,
+			changeContentSelector
+		} = this.props;
 
-	items.push(
-		<MenuItem key={1} icon="fa fa-info" contentHeading="Page Group Info">
-			<Info onContentSelectorChange={changeContentSelector} onSampleUrlChange={saveSampleUrl} channel={channel} />
-		</MenuItem>
-	);
+		if (!isVisible) {
+			return null;
+		}
 
-	items.push(
-		<MenuItem key={2} icon="fa fa-exchange" contentHeading="Traffic Distribution">
-			<TrafficPanel
-				trafficDistributionConfig={trafficDistributionConfig}
-				onAutoptimizeChange={updateAutoptimize}
-				allTrafficDistributions={allTrafficDistributions}
-				saveTrafficDistributions={saveTrafficDistributions}
-				channel={channel}
-			/>
-		</MenuItem>
-	);
+		const items = [],
+			closeChannelById = () => {
+				closeChannel(activeChannelId);
+			},
+			saveTrafficDistributions = config => {
+				Object.keys(config).forEach(key => {
+					const value = config[key];
+					editTrafficDistribution(key, value);
+				});
+			},
+			trafficDistributionConfig = {
+				description: uiCollections.trafficDistribution.description,
+				sumMismatchErrorMessage: uiCollections.trafficDistribution.errorMessage.sumMismatch
+			};
 
-	items.push(
-		<MenuItem key={3} icon="fa fa-times" contentHeading="Close Channel">
-			<CloseChannel closeChannelById={closeChannelById} />
-		</MenuItem>
-	);
+		trafficDistributionConfig.sumMismatchErrorMessage = trafficDistributionConfig.sumMismatchErrorMessage.map(
+			(text, id) => <span key={`td-error-${id}`}>{text}</span>
+		);
+		trafficDistributionConfig.sumMismatchErrorMessage.push(<strong key="td-error-max-value">100</strong>);
 
-	return (
-		<Menu id="channelMenu" position={position} arrow="top" activeItem={1} onGlassClick={hideMenu}>
-			{items}
-		</Menu>
-	);
-};
+		items.push(
+			<MenuItem key={1} icon="fa fa-info" contentHeading="Page Group Info">
+				<Info
+					onContentSelectorChange={changeContentSelector}
+					onSampleUrlChange={saveSampleUrl}
+					channel={channel}
+				/>
+			</MenuItem>
+		);
+
+		items.push(
+			<MenuItem key={2} icon="fa fa-exchange" contentHeading="Traffic Distribution">
+				<TrafficPanel
+					trafficDistributionConfig={trafficDistributionConfig}
+					onAutoptimizeChange={updateAutoptimize}
+					allTrafficDistributions={allTrafficDistributions}
+					saveTrafficDistributions={saveTrafficDistributions}
+					channel={channel}
+				/>
+			</MenuItem>
+		);
+
+		items.push(
+			<MenuItem key={3} icon="fa fa-times" contentHeading="Close Channel">
+				<CloseChannel closeChannelById={closeChannelById} />
+			</MenuItem>
+		);
+
+		return (
+			<Menu
+				id="channelMenu"
+				position={position}
+				arrow="top"
+				activeItem={this.state.activeItem}
+				onMenuItemClick={this.updateActiveItem}
+				onGlassClick={hideMenu}
+			>
+				{items}
+			</Menu>
+		);
+	}
+}
 
 channelMenu.propTypes = {
 	isVisible: PropTypes.bool.isRequired,
