ca33965574b796da025be8fc43c4753b029bae77
YomeshAdPushup <yomesh.gupta@adpushup.com>
2018-11-28 15:17:05 +0530

    Auto Optimise Master Save Check added

--- React/Apps/Editor/selectors/channelSelectors.js
+++ React/Apps/Editor/selectors/channelSelectors.js
@@ -12,8 +12,30 @@ const getAllChannels = state => state.channelData.byIds,
 		[getAllChannels],
 		channelByIds => (_.find(channelByIds, 'sampleUrl') ? _.find(channelByIds, 'sampleUrl').sampleUrl : false)
 	),
-	getOpenChannels = createSelector([getAllChannels], channelByIds => _.filter(channelByIds, { isOpen: true })),
-	getClosedChannels = createSelector([getAllChannels], channelByIds => _.filter(channelByIds, { isOpen: false }));
+	getOpenChannels = createSelector(
+		[getAllChannels],
+		channelByIds => _.filter(channelByIds, { isOpen: true })
+	),
+	getClosedChannels = createSelector(
+		[getAllChannels],
+		channelByIds => _.filter(channelByIds, { isOpen: false })
+	),
+	checkTrafficDistribution = (channels, variations) => {
+		let response = true;
+		_.forEach(channels, channel => {
+			if (channel.hasOwnProperty('autoOptimise') && !channel.autoOptimise) {
+				const variationIds = channel.variations;
+				const traffic = variationIds.reduce((acc, variationId) => {
+					return acc + variations[variationId].trafficDistribution;
+				}, 0);
+				if (traffic !== 100) {
+					response = false;
+					return false;
+				}
+			}
+		});
+		return response;
+	};
 
 export {
 	getOpenChannels,
@@ -22,5 +44,6 @@ export {
 	getActiveChannelId,
 	getSampleUrl,
 	getChannel,
-	getClosedChannels
+	getClosedChannels,
+	checkTrafficDistribution
 };
