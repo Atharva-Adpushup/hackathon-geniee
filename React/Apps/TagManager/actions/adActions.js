504d31d4dab8d09476e6a9b20bc47319760aef5f
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-01-04 17:19:36 +0530

    Delete Ad Typo Fix

--- React/Apps/TagManager/actions/adActions.js
+++ React/Apps/TagManager/actions/adActions.js
@@ -1,8 +1,8 @@
 import { adActions, uiActions, globalActions } from '../configs/commonConsts';
 import { ajax } from '../../../common/helpers';
 
-const createAd = params => (dispatch, getState) => {
-		return ajax({
+const createAd = params => (dispatch, getState) =>
+		ajax({
 			url: '/tagManager/createAd',
 			method: 'POST',
 			data: JSON.stringify(params)
@@ -10,15 +10,13 @@ const createAd = params => (dispatch, getState) => {
 			if (response.error) {
 				return alert('Ad creation failed');
 				// dispatch({ type: uiActions.SET_CREATE_AD_ERROR, value: true });
-			} else {
-				// dispatch({ type: uiActions.SET_CREATE_AD_ERROR, value: false });
-				dispatch({ type: adActions.UPDATE_ADS_LIST, data: { ...params.ad, id: response.data.id } });
-				dispatch({ type: globalActions.SET_CURRENT_AD, currentAd: response.data.id });
 			}
-		});
-	},
-	fetchAds = params => (dispatch, getState) => {
-		return ajax({
+			// dispatch({ type: uiActions.SET_CREATE_AD_ERROR, value: false });
+			dispatch({ type: adActions.UPDATE_ADS_LIST, data: { ...params.ad, id: response.data.id } });
+			dispatch({ type: globalActions.SET_CURRENT_AD, currentAd: response.data.id });
+		}),
+	fetchAds = params => (dispatch, getState) =>
+		ajax({
 			url: '/tagManager/fetchAds',
 			method: 'GET',
 			data: params
@@ -26,36 +24,31 @@ const createAd = params => (dispatch, getState) => {
 			if (response.error) {
 				return alert('Ad fetching failed');
 				// dispatch({ type: uiActions.SET_FETCH_ADS_ERROR, value: true });
-			} else {
-				// dispatch({ type: uiActions.SET_FETCH_ADS_ERROR, value: false });
-				dispatch({ type: adActions.REPLACE_ADS_LIST, data: response.data.ads });
 			}
-		});
-	},
-	deleteAd = parmas => (dispatch, getState) => {
-		return ajax({
+			// dispatch({ type: uiActions.SET_FETCH_ADS_ERROR, value: false });
+			dispatch({ type: adActions.REPLACE_ADS_LIST, data: response.data.ads });
+		}),
+	deleteAd = params => (dispatch, getState) =>
+		ajax({
 			url: '/tagManager/deleteAd',
 			method: 'POST',
 			data: JSON.stringify(params)
 		}).then(response => {
 			if (response.error) {
 				return alert('Delete Ad failed');
-			} else {
-				dispatch({ type: adActions.DELETE_AD, adId: params.adId });
 			}
-		});
-	},
-	updateAd = (adId, data) => (dispatch, getState) => {
-		return dispatch({
+			dispatch({ type: adActions.DELETE_AD, adId: params.adId });
+		}),
+	updateAd = (adId, data) => (dispatch, getState) =>
+		dispatch({
 			type: adActions.UPDATE_AD,
 			data: {
 				id: adId,
 				updateThis: data
 			}
-		});
-	},
-	modifyAdOnServer = (adId, data) => (dispatch, getState) => {
-		return ajax({
+		}),
+	modifyAdOnServer = (adId, data) => (dispatch, getState) =>
+		ajax({
 			url: '/tagManager/modifyAd',
 			method: 'POST',
 			data: JSON.stringify({ siteId: window.siteId, adId, data })
@@ -71,6 +64,5 @@ const createAd = params => (dispatch, getState) => {
 				}
 			});
 		});
-	};
 
 export { createAd, fetchAds, deleteAd, updateAd, modifyAdOnServer };
