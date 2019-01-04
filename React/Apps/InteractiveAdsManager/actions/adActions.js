0865f04ab53ddc79468bfb37fc2007098899d965
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-01-04 17:15:16 +0530

    Minor changes

--- React/Apps/InteractiveAdsManager/actions/adActions.js
+++ React/Apps/InteractiveAdsManager/actions/adActions.js
@@ -1,47 +1,57 @@
-import { adActions, uiActions, globalActions } from '../configs/commonConsts';
+import { adActions, globalActions } from '../configs/commonConsts';
 import { ajax } from '../../../common/helpers';
 
-const createAd = params => (dispatch, getState) => {
-		return ajax({
-			url: '/interactiveAdsManager/data/createAd',
-			method: 'POST',
-			data: JSON.stringify(params)
-		}).then(response => {
-			if (response.error) {
-				return alert('Ad creation failed');
-			} else {
-				dispatch({ type: adActions.UPDATE_ADS_LIST, data: { ...params.ad, id: response.data.id } });
-				dispatch({ type: globalActions.SET_CURRENT_AD, currentAd: response.data.id });
-			}
-		});
-	},
-	fetchAds = params => (dispatch, getState) => {
-		return ajax({
-			url: '/tagManager/fetchAds',
-			method: 'GET',
-			data: params
-		}).then(response => {
-			if (response.error) {
-				return alert('Ad fetching failed');
-			} else {
-				dispatch({ type: adActions.REPLACE_ADS_LIST, data: response.data.ads });
-			}
-		});
-	},
-	deleteAd = parmas => (dispatch, getState) => {
-		return ajax({
-			url: '/tagManager/deleteAd',
-			method: 'POST',
-			data: JSON.stringify(params)
-		}).then(response => {
-			if (response.error) {
-				return alert('Delete Ad failed');
-			} else {
-				dispatch({ type: adActions.DELETE_AD, adId: params.adId });
-			}
-		});
-	},
-	updateAd = (adId, data) => (dispatch, getState) => {
+const createAd = params => dispatch =>
+	ajax({
+		url: '/interactiveAdsManager/data/createAd',
+		method: 'POST',
+		data: JSON.stringify(params)
+	}).then(response => {
+		if (response.error) {
+			return alert('Ad creation failed');
+		}
+		dispatch({ type: adActions.UPDATE_ADS_LIST, data: { ...params.ad, id: response.data.id } });
+		dispatch({ type: globalActions.SET_CURRENT_AD, currentAd: response.data.id });
+	});
+const fetchAds = params => dispatch =>
+	ajax({
+		url: '/tagManager/fetchAds',
+		method: 'GET',
+		data: params
+	}).then(response => {
+		if (response.error) {
+			return alert('Ad fetching failed');
+		}
+		dispatch({ type: adActions.REPLACE_ADS_LIST, data: response.data.ads });
+	});
+const deleteAd = params => dispatch =>
+	ajax({
+		url: '/tagManager/deleteAd',
+		method: 'POST',
+		data: JSON.stringify(params)
+	}).then(response => {
+		if (response.error) {
+			return alert('Delete Ad failed');
+		}
+		dispatch({ type: adActions.DELETE_AD, adId: params.adId });
+	});
+const updateAd = (adId, data) => dispatch =>
+	dispatch({
+		type: adActions.UPDATE_AD,
+		data: {
+			id: adId,
+			updateThis: data
+		}
+	});
+const modifyAdOnServer = (adId, data) => dispatch =>
+	ajax({
+		url: '/tagManager/modifyAd',
+		method: 'POST',
+		data: JSON.stringify({ siteId: window.siteId, adId, data })
+	}).then(response => {
+		if (response.error) {
+			return alert(response.data.message);
+		}
 		return dispatch({
 			type: adActions.UPDATE_AD,
 			data: {
@@ -49,24 +59,6 @@ const createAd = params => (dispatch, getState) => {
 				updateThis: data
 			}
 		});
-	},
-	modifyAdOnServer = (adId, data) => (dispatch, getState) => {
-		return ajax({
-			url: '/tagManager/modifyAd',
-			method: 'POST',
-			data: JSON.stringify({ siteId: window.siteId, adId, data })
-		}).then(response => {
-			if (response.error) {
-				return alert(response.data.message);
-			}
-			return dispatch({
-				type: adActions.UPDATE_AD,
-				data: {
-					id: adId,
-					updateThis: data
-				}
-			});
-		});
-	};
+	});
 
 export { createAd, fetchAds, deleteAd, updateAd, modifyAdOnServer };
--- React/Apps/InteractiveAdsManager/actions/globalActions.js
+++ React/Apps/InteractiveAdsManager/actions/globalActions.js
@@ -1,26 +1,26 @@
-import { uiActions, globalActions } from '../configs/commonConsts';
+import { globalActions } from '../configs/commonConsts';
 import { ajax } from '../../../common/helpers';
 
-const masterSave = siteId => (dispatch, getState) => {
-		const data = { siteId, ads: getState().ads.content };
-		return ajax({
-			url: '/tagManager/masterSave',
-			method: 'POST',
-			data: JSON.stringify({
-				...data,
-				siteDomain: window.currentUser.sites.filter(site => site.siteId === siteId)[0].domain
-			})
-		}).then(response => {
-			if (response.error) {
-				alert('Some error occurred');
-				return;
-			}
-			return alert('Save successful');
-		});
-	},
-	resetCurrentAd = () => (dispatch, getState) => {
-		dispatch({ type: globalActions.SET_CURRENT_AD, currentAd: null });
-		return true;
-	};
+const masterSave = siteId => (_, getState) => {
+	const data = { siteId, ads: getState().ads.content };
+	return ajax({
+		url: '/tagManager/masterSave',
+		method: 'POST',
+		data: JSON.stringify({
+			...data,
+			siteDomain: window.currentUser.sites.filter(site => site.siteId === siteId)[0].domain
+		})
+	}).then(response => {
+		if (response.error) {
+			return alert('Some error occurred');
+		}
+		return alert('Save successful');
+	});
+};
+
+const resetCurrentAd = () => dispatch => {
+	dispatch({ type: globalActions.SET_CURRENT_AD, currentAd: null });
+	return true;
+};
 
 export { masterSave, resetCurrentAd };
