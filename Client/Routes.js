4a69fb5a5e9b4a672fef18c1ea95319ac56b78f6
ravijagga <mr.ravi.jagga@gmail.com>
2019-01-24 15:55:20 +0530

    Configured eslint and prettier

--- Client/Routes.js
+++ Client/Routes.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
 import Dashboard from './Pages/Dashboard';
 
 const Routes = () => (
   <Router>
-    <Route exact path="/dashboard" component={Dashboard} />
+    <Switch>
+      <Route exact path="/" render={() => <Redirect to="/dashboard" />} />
+      <Route exact path="/dashboard" component={Dashboard} />
+    </Switch>
   </Router>
 );
 
