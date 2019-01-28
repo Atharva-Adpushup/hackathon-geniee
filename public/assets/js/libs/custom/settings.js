0198f34201487fc79c21376482adf44c546d529b
ZahinAdPushup <zahin@adpushup.com>
2019-01-28 20:12:11 +0530

    [Feature] (Frontend, Backend: App) Currency module support for Prebid JS: Implementation code added

--- public/assets/js/libs/custom/settings.js
+++ public/assets/js/libs/custom/settings.js
@@ -102,18 +102,20 @@ $(document).ready(function() {
 						'Pagegroup pattern cannot be blank. Please provide valid regex patterns for all the pagegroups.'
 					);
 				} else {
-					var activeDFPNetwork = this.parseFormData(formValues, 'other').activeDFPNetwork,
-						autoOpt = this.parseFormData(formValues, 'other').autoOptimise ? true : false,
-						isSPA = this.parseFormData(formValues, 'other').isSPA ? true : false,
-						spaPageTransitionTimeout = this.parseFormData(formValues, 'other').spaPageTransitionTimeout,
+					var parsedFormValues = this.parseFormData(formValues, 'other'),
+						activeDFPNetwork = parsedFormValues.activeDFPNetwork,
+						activeDFPCurrencyCode = parsedFormValues.activeDFPCurrencyCode,
+						autoOpt = parsedFormValues.autoOptimise ? true : false,
+						isSPA = parsedFormValues.isSPA ? true : false,
+						spaPageTransitionTimeout = parsedFormValues.spaPageTransitionTimeout,
 						dfpInfo = activeDFPNetwork ? activeDFPNetwork.split('-') : [],
 						activeDFPNetwork = dfpInfo.length ? dfpInfo[0] : '',
 						activeDFPParentId = dfpInfo.length ? dfpInfo[1] : '',
 						pageGroupPattern = JSON.stringify(parsedPageGroups),
-						otherSettings = JSON.stringify(this.parseFormData(formValues, 'other')),
-						gdprCompliance = this.parseFormData(formValues, 'other').gdprCompliance ? true : false,
-						cookieControlConfig = this.parseFormData(formValues, 'other').cookieControlConfig
-							? this.parseFormData(formValues, 'other').cookieControlConfig
+						otherSettings = JSON.stringify(parsedFormValues),
+						gdprCompliance = parsedFormValues.gdprCompliance ? true : false,
+						cookieControlConfig = parsedFormValues.cookieControlConfig
+							? parsedFormValues.cookieControlConfig
 							: {};
 
 					$error.html('');
@@ -127,6 +129,7 @@ $(document).ready(function() {
 							spaPageTransitionTimeout: spaPageTransitionTimeout,
 							activeDFPNetwork: activeDFPNetwork,
 							activeDFPParentId: activeDFPParentId,
+							activeDFPCurrencyCode: activeDFPCurrencyCode,
 							gdprCompliance: gdprCompliance,
 							cookieControlConfig: cookieControlConfig,
 							blocklist: JSON.stringify(w.blocklist),
@@ -398,6 +401,26 @@ $(document).ready(function() {
 				ele.classList.remove('red');
 				ele.classList.add(toAdd);
 			});
+		});
+
+		// Active DFP Network change handler
+		$('#activeDFPNetwork').on('change', function(e) {
+			var $checkedOption = $('option:selected', this),
+				currencyCode = $checkedOption.attr('data-currencyCode'),
+				$currencyTable = $('.js-currency-table'),
+				$currencyTd = $('#currencyValue'),
+				$activeDFPCurrencyCodeHiddenInput = $('#activeDFPCurrencyCode');
+
+			if (!currencyCode) {
+				$currencyTable.addClass('u-hide');
+				$currencyTd.text('');
+				$activeDFPCurrencyCodeHiddenInput.val('');
+				return;
+			}
+
+			$currencyTd.text(currencyCode);
+			$activeDFPCurrencyCodeHiddenInput.val(currencyCode);
+			$currencyTable.removeClass('u-hide');
 		});
 
 		// Copy to clipboard trigger
--- services/genieeAdSyncService/cdnSyncService/cdnSyncConsumer.js
+++ services/genieeAdSyncService/cdnSyncService/cdnSyncConsumer.js
@@ -143,13 +143,22 @@ module.exports = function(site, externalData = {}) {
 
 						case CC.SERVICES.HEADER_BIDDING:
 							serviceScript = serviceScript.substring(50, serviceScript.trim().length - 1);
+							const isValidHBConfig = !!(
+									serviceConfig &&
+									serviceConfig.hbcf.value &&
+									serviceConfig.hbcf.value.hbConfig &&
+									serviceConfig.hbAds.length
+								),
+								isValidCurrencyConfig = !!(
+									isValidHBConfig &&
+									serviceConfig.currency &&
+									Object.keys(serviceConfig.currency).length &&
+									serviceConfig.currency.adServerCurrency &&
+									serviceConfig.currency.granularityMultiplier &&
+									serviceConfig.currency.rates
+								);
 
-							if (
-								serviceConfig &&
-								serviceConfig.hbcf.value &&
-								serviceConfig.hbcf.value.hbConfig &&
-								serviceConfig.hbAds.length
-							) {
+							if (isValidHBConfig) {
 								jsFile = _.replace(jsFile, '__PREBID_SCRIPT__', serviceScript);
 								uncompressedJsFile = _.replace(uncompressedJsFile, '__PREBID_SCRIPT__', serviceScript);
 
@@ -160,8 +169,22 @@ module.exports = function(site, externalData = {}) {
 									deviceConfig = '';
 								}
 
+								let prebidCurrencyConfig = serviceConfig.currency;
+								if (isValidCurrencyConfig) {
+									prebidCurrencyConfig = ',currency: ' + JSON.stringify(prebidCurrencyConfig);
+								} else {
+									prebidCurrencyConfig = '';
+								}
+
 								jsFile = _.replace(jsFile, '__SIZE_CONFIG__', deviceConfig);
+								jsFile = _.replace(jsFile, '__PREBID_CURRENCY_CONFIG__', prebidCurrencyConfig);
+
 								uncompressedJsFile = _.replace(uncompressedJsFile, '__SIZE_CONFIG__', deviceConfig);
+								uncompressedJsFile = _.replace(
+									uncompressedJsFile,
+									'__PREBID_CURRENCY_CONFIG__',
+									prebidCurrencyConfig
+								);
 							} else {
 								jsFile = _.replace(jsFile, '__PREBID_SCRIPT__', '');
 								uncompressedJsFile = _.replace(uncompressedJsFile, '__PREBID_SCRIPT__', '');
@@ -231,9 +254,42 @@ module.exports = function(site, externalData = {}) {
 			) {
 				let { apConfigs, adpTagsConfig } = finalConfig,
 					gdpr = site.get('gdpr'),
-					{ incontentAds, hbAds } = incontentAndHbAds;
+					{ incontentAds, hbAds } = incontentAndHbAds,
+					isActiveDFPNetwork = !!(apConfigs.activeDFPNetwork && apConfigs.activeDFPNetwork.length),
+					isActiveDFPCurrencyCode = !!(
+						apConfigs.activeDFPCurrencyCode &&
+						apConfigs.activeDFPCurrencyCode.length &&
+						apConfigs.activeDFPCurrencyCode.length === 3
+					),
+					isPrebidGranularityMultiplier = !!(
+						apConfigs.prebidGranularityMultiplier && Number(apConfigs.prebidGranularityMultiplier)
+					),
+					isActiveDFPCurrencyExchangeRate = !!(
+						apConfigs.activeDFPCurrencyExchangeRate && Number(apConfigs.activeDFPCurrencyExchangeRate)
+					),
+					isValidCurrencyConfig = !!(
+						isActiveDFPNetwork &&
+						isActiveDFPCurrencyCode &&
+						isPrebidGranularityMultiplier &&
+						isActiveDFPCurrencyExchangeRate
+					),
+					computedPrebidCurrencyConfig = {};
+
+				if (isValidCurrencyConfig) {
+					computedPrebidCurrencyConfig = {
+						adServerCurrency: apConfigs.activeDFPCurrencyCode,
+						granularityMultiplier: Number(apConfigs.prebidGranularityMultiplier),
+						rates: {
+							USD: {
+								[apConfigs.activeDFPCurrencyCode]: Number(apConfigs.activeDFPCurrencyExchangeRate)
+							}
+						}
+					};
+				}
+
 				if (site.get('ampSettings')) apConfigs.ampSettings = site.get('ampSettings');
 				if (site.get('medianetId')) apConfigs.medianetId = site.get('medianetId');
+
 				jsFile = _.replace(jsFile, '__AP_CONFIG__', JSON.stringify(apConfigs));
 				jsFile = _.replace(jsFile, /__SITE_ID__/g, site.get('siteId'));
 				jsFile = _.replace(jsFile, '__COUNTRY__', false);
@@ -245,7 +301,11 @@ module.exports = function(site, externalData = {}) {
 				var scripts = generateFinalInitScript(jsFile, uncompressedJsFile)
 					.addService(CC.SERVICES.INCONTENT_ANALYSER, incontentAds, incontentAnalyserScript)
 					.addService(CC.SERVICES.ADPTAGS, adpTagsConfig, adpTagsScript)
-					.addService(CC.SERVICES.HEADER_BIDDING, { hbcf, hbAds: hbAds.concat(hbAdsApTag) }, prebidScript)
+					.addService(
+						CC.SERVICES.HEADER_BIDDING,
+						{ hbcf, hbAds: hbAds.concat(hbAdsApTag), currency: computedPrebidCurrencyConfig },
+						prebidScript
+					)
 					.addService(CC.SERVICES.GDPR, gdpr)
 					.done();
 
