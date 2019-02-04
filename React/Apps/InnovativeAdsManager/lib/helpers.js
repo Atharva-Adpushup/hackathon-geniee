f6be070390a660a5c58960855f351601d0453401
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-02-04 15:53:54 +0530

    Added Comments to Pagegroup Filtering

--- React/Apps/InnovativeAdsManager/lib/helpers.js
+++ React/Apps/InnovativeAdsManager/lib/helpers.js
@@ -11,6 +11,11 @@ function copyToClipBoard(content) {
 }
 
 function pagegroupFiltering(channels, platform, format, meta, listMode = false, currentPagegroups = []) {
+	/*
+		Getting pagegroups based on current platform
+		Input: ['DESKTOP:HOME', 'DESKTOP:POST', 'MOBILE:HOME']
+		Output: ['DESKTOP:HOME', 'DESKTOP:POST']
+	*/
 	const filteredPagegroupsByPlatform = channels.filter(channel => {
 		const re = new RegExp(platform, 'ig');
 		return channel.match(re);
@@ -19,6 +24,12 @@ function pagegroupFiltering(channels, platform, format, meta, listMode = false, 
 
 	let types;
 
+	/*
+		Finding to which group, ad belongs to
+		['stickyLeft', 'stickyRight', 'docked']
+		['stickyTop', 'stickyBottom']
+		['inView']
+	*/
 	if (INTERACTIVE_ADS_TYPES.VERTICAL.includes(format)) {
 		types = INTERACTIVE_ADS_TYPES.VERTICAL;
 	} else if (INTERACTIVE_ADS_TYPES.HORIZONTAL.includes(format)) {
@@ -27,6 +38,21 @@ function pagegroupFiltering(channels, platform, format, meta, listMode = false, 
 		types = INTERACTIVE_ADS_TYPES.OTHER;
 	}
 
+	/*
+		Iterating over all the filtered pagegroups and finding which all pagegroups need to be disabled as in
+		(All) Channels: ['DESKTOP:HOME', 'DESKTOP:POST', 'MOBILE:POST']
+		Platform: desktop
+		Format: stickyTop
+		Meta Pagegroups/Channel (Active ads log): ['DESKTOP-stickyBottom-DESKTOP:HOME']
+		Current Channel/Pagegroups: ['DESKTOP:HOME']
+
+		After processing:
+		Filtered Pagegroups/Channels: ['DESKTOP:HOME', 'DESKTOP:POST']
+		Disabled: ['DESKTOP:HOME']
+
+		i.e. we are trying to create/make a stickyTop ad in pagegroup DESKTOP:HOME but it is not allowed
+		as there is already a stickyBottom (similar type: HORIZONTAL) ad active for same DESKTOP:HOME.
+	*/
 	filteredPagegroupsByPlatform.forEach(pg => {
 		let shouldDisable = false;
 		types.forEach(type => {
