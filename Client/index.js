8ee6d45615841699d5af8bec887fa96fb4d5bfe7
ZahinAdPushup <zahin@adpushup.com>
2019-02-05 18:56:58 +0530

    [Code changes] (Frontend: New App) Redux setup initialised

--- Client/index.js
+++ Client/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import configureStore from './store/index';
 import './scss/index.scss';
 
-render(<App />, document.querySelector('#root'));
+const initialData = {};
+const store = configureStore(initialData);
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);
