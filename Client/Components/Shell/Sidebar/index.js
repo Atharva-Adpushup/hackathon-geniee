3b573a65700e93c98c70baa2ed426f621c99a2e5
ZahinAdPushup <zahin@adpushup.com>
2019-02-06 19:50:52 +0530

    [Code changes] (Frontend: New App) Sidebar activated UI changes done

--- Client/Components/Shell/Sidebar/index.js
+++ Client/Components/Shell/Sidebar/index.js
@@ -1,82 +1,85 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = ({ show }) => (
 	<aside className={`sidebar ${show ? 'sb-show' : 'sb-hide'}`}>
 		<ul className="sb-nav primary-nav">
-			<li>
-				<Link to="/dashboard" className="clearfix">
+			<NavLink to="/dashboard" className="clearfix" activeClassName="active">
+				<li>
 					<span className="link-text-wrap">
 						<span className="link-text">Dashboard</span>
 					</span>
 					<span className="sb-nav-icon-wrap">
 						<FontAwesomeIcon icon="tachometer-alt" className="sb-nav-icon" />
 					</span>
-				</Link>
-			</li>
-			<li>
-				<Link to="/sites" className="clearfix">
+				</li>
+			</NavLink>
+
+			<NavLink to="/sites" className="clearfix" activeClassName="active">
+				<li>
 					<span className="link-text-wrap">
 						<span className="link-text">My Sites</span>
 					</span>
 					<span className="sb-nav-icon-wrap">
 						<FontAwesomeIcon icon="list" className="sb-nav-icon" />
 					</span>
-				</Link>
-			</li>
-			<li>
-				<Link to="/reporting" className="clearfix">
+				</li>
+			</NavLink>
+
+			<NavLink to="/reporting" className="clearfix" activeClassName="active">
+				<li>
 					<span className="link-text-wrap">
 						<span className="link-text">Reporting</span>
 					</span>
 					<span className="sb-nav-icon-wrap">
 						<FontAwesomeIcon icon="chart-area" className="sb-nav-icon" />
 					</span>
-				</Link>
-			</li>
-			<li>
-				<Link to="/byodPanel" className="clearfix">
+				</li>
+			</NavLink>
+
+			<NavLink to="/byodPanel" className="clearfix" activeClassName="active">
+				<li>
 					<span className="link-text-wrap">
 						<span className="link-text">BYOD Panel</span>
 					</span>
 					<span className="sb-nav-icon-wrap">
 						<FontAwesomeIcon icon="desktop" className="sb-nav-icon" />
 					</span>
-				</Link>
-			</li>
+				</li>
+			</NavLink>
 		</ul>
 
 		<div className="cta-btn-wrap">
-			<Link to="/addSite" className="cta-btn">
+			<NavLink to="/addSite" className="cta-btn" activeClassName="active">
 				<span className="cta-btn-txt-wrap">
 					<span className="cta-btn-text">Add New Website</span>
 				</span>
 				<FontAwesomeIcon icon="plus" className="cta-btn-icon" />
-			</Link>
+			</NavLink>
 		</div>
 
 		<ul className="sb-nav secondary-nav">
-			<li>
-				<Link to="/adsTxtManagement" className="clearfix">
+			<NavLink to="/adsTxtManagement" className="clearfix" activeClassName="active">
+				<li>
 					<span className="link-text-wrap">
 						<span className="link-text">Ads.txt Management</span>
 					</span>
 					<span className="sb-nav-icon-wrap">
 						<FontAwesomeIcon icon="align-center" className="sb-nav-icon" />
 					</span>
-				</Link>
-			</li>
-			<li>
-				<Link to="/payment" className="clearfix">
+				</li>
+			</NavLink>
+			<NavLink to="/payment" className="clearfix" activeClassName="active">
+				<li>
 					<span className="link-text-wrap">
 						<span className="link-text">Payments</span>
 					</span>
 					<span className="sb-nav-icon-wrap">
 						<FontAwesomeIcon icon="dollar-sign" className="sb-nav-icon" />
 					</span>
-				</Link>
-			</li>
+				</li>
+			</NavLink>
 		</ul>
 	</aside>
 );
