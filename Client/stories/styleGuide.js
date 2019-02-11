4e1d3d839c6bbf4c067a2a649119c5bfac50eaa1
ZahinAdPushup <zahin@adpushup.com>
2019-02-11 16:39:36 +0530

    [Code changes] (Frontend: New SPA) Storybook: Following changes are done:
    - package-lock JSON: Updated changes
    - Client\.storybook\preview-head HTML: Removed empty spaces and added one utility class name
    - Client\stories\styleGuide JS: Added theme component as a story
    - Client/stories/Components/StyleGuide/Theme JS: Added theme React component

--- Client/stories/styleGuide.js
+++ Client/stories/styleGuide.js
@@ -2,5 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import NamingConvention from './Components/StyleGuide/NamingConvention';
+import Theme from './Components/StyleGuide/Theme';
 
-storiesOf('UI.StyleGuide', module).add('Naming.Convention', () => <NamingConvention />);
+storiesOf('UI.StyleGuide', module)
+	.add('Naming.Convention', () => <NamingConvention />)
+	.add('Theme', () => <Theme />);
