24fb69dd6af08eed1bd7ba9812c1a3804f05a627
ZahinAdPushup <zahin@adpushup.com>
2019-02-18 02:06:53 +0530

    [Code changes] (Frontend: New SPA) App Layout: Added medianet ad network control code conversion implementation

--- Client/Apps/Layout/components/Home/ControlTagConversion.jsx
+++ Client/Apps/Layout/components/Home/ControlTagConversion.jsx
@@ -17,7 +17,15 @@ class ControlTagConversion extends Component {
 			adControlType: 1,
 			siteId: '',
 			inputCode: '',
-			convertedCode: ''
+			convertedCode: '',
+			medianet: {
+				adId: getRandomString(5),
+				adWidth: '',
+				adHeight: '',
+				crId: '',
+				versionId: '',
+				cId: ''
+			}
 		};
 	}
 
@@ -56,19 +64,81 @@ class ControlTagConversion extends Component {
 					console.log(`control ad: ${value}`);
 					this.setState({ adControlType: Number(value) });
 					break;
+
+				case 'adId':
+					console.log(`medianet adId: ${value}`);
+					var medianet = { ...this.state.medianet };
+
+					medianet.adId = value;
+					this.setState({ medianet });
+					break;
+
+				case 'adWidth':
+					console.log(`medianet adWidth: ${value}`);
+					var medianet = { ...this.state.medianet };
+
+					medianet.adWidth = Number(value);
+					this.setState({ medianet });
+					break;
+
+				case 'adHeight':
+					console.log(`medianet adHeight: ${value}`);
+					var medianet = { ...this.state.medianet };
+
+					medianet.adHeight = Number(value);
+					this.setState({ medianet });
+					break;
+
+				case 'crId':
+					console.log(`medianet crId: ${value}`);
+					var medianet = { ...this.state.medianet };
+
+					medianet.crId = Number(value);
+					this.setState({ medianet });
+					break;
+
+				case 'versionId':
+					console.log(`medianet versionId: ${value}`);
+					var medianet = { ...this.state.medianet };
+
+					medianet.versionId = Number(value);
+					this.setState({ medianet });
+					break;
+
+				case 'cId':
+					console.log(`medianet cId: ${value}`);
+					var medianet = { ...this.state.medianet };
+
+					medianet.cId = Number(value);
+					this.setState({ medianet });
+					break;
 			}
 		}
 	}
 
 	getConvertedAdCode() {
 		const { adControlType, siteId, inputCode } = this.state;
+		const {
+			medianet: { adId, adWidth, adHeight, crId, versionId, cId }
+		} = this.state;
+		// Check for ad network toggle UI selection ('All ad networks' or 'Medianet')
+		const isAllAdNetworksSelection = !!(Number(adControlType) === 1);
+
 		const template = CONTROL_CONVERSION_NETWORKS[adControlType].template;
-		const encodedData = getEncodedData(inputCode);
-		const parameterCollection = [
-			{ replacee: '_CODE_', replacer: encodedData },
-			{ replacee: '_SITEID_', replacer: siteId },
-			{ replacee: '_STRING_', replacer: getRandomString(5) }
-		];
+		const parameterCollection = isAllAdNetworksSelection
+			? [
+					{ replacee: '_CODE_', replacer: getEncodedData(inputCode) },
+					{ replacee: '_SITEID_', replacer: siteId },
+					{ replacee: '_STRING_', replacer: getRandomString(5) }
+			  ]
+			: [
+					{ replacee: /___ADID__/g, replacer: `_${adId}` },
+					{ replacee: /__WIDTH__/g, replacer: adWidth },
+					{ replacee: /__HEIGHT__/g, replacer: adHeight },
+					{ replacee: /__CRID__/g, replacer: crId },
+					{ replacee: /__VERSIONID__/g, replacer: versionId },
+					{ replacee: /__CID__/g, replacer: cId }
+			  ];
 		const compiledTemplate = getCompiledTemplate(template, parameterCollection);
 
 		return compiledTemplate;
@@ -88,31 +158,83 @@ class ControlTagConversion extends Component {
 		}
 	}
 
-	renderControlConversionLeftPanel() {
-		const buttonToggle = [
-				{
-					value: 1,
-					text: [CONTROL_CONVERSION_NETWORKS[1].name]
-				},
-				{
-					value: 2,
-					text: [CONTROL_CONVERSION_NETWORKS[2].name]
-				}
-			],
-			{ siteId, inputCode } = this.state;
+	renderMedianetNetworkUI() {
+		const {
+			medianet: { adId, adWidth, adHeight, crId, versionId, cId }
+		} = this.state;
 
 		return (
 			<div className="clearfix">
 				<FieldGroup
-					id="toggle-button-group"
-					label="Select control ad type"
-					type="toggle-button-group"
-					buttonToggle={buttonToggle}
-					onChange={this.handleInputChangeHandler.bind(this, 'adNetworkToggle')}
+					id="input-text-adId"
+					label="Enter ad incremental id"
+					type="text"
+					placeholder="Random alphanumeric values of 5 digits like 12wsa, 6hw5n etc."
+					className=""
+					onChange={this.handleInputChangeHandler.bind(this, 'adId')}
+					value={adId}
 				/>
 
 				<FieldGroup
-					id="input-text-siteId"
+					id="input-number-adWidth"
+					label="Enter ad width"
+					type="number"
+					placeholder="For example 728, 300, 320, 480, 160, 900 etc."
+					className=""
+					onChange={this.handleInputChangeHandler.bind(this, 'adWidth')}
+					value={adWidth}
+				/>
+
+				<FieldGroup
+					id="input-number-adHeight"
+					label="Enter ad height"
+					type="number"
+					placeholder="For example 60, 200, 250, 600 etc."
+					className=""
+					onChange={this.handleInputChangeHandler.bind(this, 'adHeight')}
+					value={adHeight}
+				/>
+
+				<FieldGroup
+					id="input-number-crId"
+					label="Enter ad customer relationship id (crid)"
+					type="number"
+					placeholder="For example 12345678 etc."
+					className=""
+					onChange={this.handleInputChangeHandler.bind(this, 'crId')}
+					value={crId}
+				/>
+
+				<FieldGroup
+					id="input-number-versionId"
+					label="Enter ad version id (versionId)"
+					type="number"
+					placeholder="For example 87654321 etc."
+					className=""
+					onChange={this.handleInputChangeHandler.bind(this, 'versionId')}
+					value={versionId}
+				/>
+
+				<FieldGroup
+					id="input-number-cId"
+					label="Enter ad customer id (cId)"
+					type="number"
+					placeholder="For example 12348765 etc."
+					className=""
+					onChange={this.handleInputChangeHandler.bind(this, 'cId')}
+					value={cId}
+				/>
+			</div>
+		);
+	}
+
+	renderAllAdNetworksUI() {
+		const { siteId, inputCode } = this.state;
+
+		return (
+			<div className="clearfix">
+				<FieldGroup
+					id="input-number-siteId"
 					label="Enter site id"
 					type="number"
 					placeholder="For example 25019, 31000"
@@ -132,6 +254,35 @@ class ControlTagConversion extends Component {
 					onChange={this.handleInputChangeHandler.bind(this, 'inputCode')}
 					value={inputCode}
 				/>
+			</div>
+		);
+	}
+
+	renderControlConversionLeftPanel() {
+		const buttonToggle = [
+			{
+				value: 1,
+				text: [CONTROL_CONVERSION_NETWORKS[1].name]
+			},
+			{
+				value: 2,
+				text: [CONTROL_CONVERSION_NETWORKS[2].name]
+			}
+		];
+		const { adControlType } = this.state,
+			isAllAdNetworksSelection = !!(Number(adControlType) === 1);
+
+		return (
+			<div className="clearfix">
+				<FieldGroup
+					id="toggle-button-group"
+					label="Select control ad type"
+					type="toggle-button-group"
+					buttonToggle={buttonToggle}
+					onChange={this.handleInputChangeHandler.bind(this, 'adNetworkToggle')}
+				/>
+
+				{isAllAdNetworksSelection ? this.renderAllAdNetworksUI() : this.renderMedianetNetworkUI()}
 
 				<CustomButton
 					variant="primary"
@@ -159,10 +310,7 @@ class ControlTagConversion extends Component {
 
 				{convertedCode ? (
 					<Row>
-						<Col sm={6} md={12} lg={12} className="u-padding-0">
-							<pre>{convertedCode}</pre>
-						</Col>
-						<Col sm={6} md={12} lg={12} className="u-padding-0">
+						<Col sm={6} md={12} lg={12} className="u-padding-0 u-margin-b4">
 							<CustomButton
 								variant="secondary"
 								className=""
@@ -170,6 +318,9 @@ class ControlTagConversion extends Component {
 							>
 								Copy to Clipboard
 							</CustomButton>
+						</Col>
+						<Col sm={6} md={12} lg={12} className="u-padding-0">
+							<pre>{convertedCode}</pre>
 						</Col>
 					</Row>
 				) : null}
