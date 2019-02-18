dbe91c6f595db38cce07e32dc4f05fb9adf00502
ZahinAdPushup <zahin@adpushup.com>
2019-02-18 18:53:52 +0530

    [Code changes] (Frontend: New SPA) App Layout: Added UI finishing touch

--- Client/Apps/Layout/constants/index.js
+++ Client/Apps/Layout/constants/index.js
@@ -1,8 +1,5 @@
 const COMPONENT_TITLES = {
-	1: 'Layout Testing',
-	2: 'Ads List'
-	// 4: 'Ads Txt Config',
-	// 1: 'AdPushup Header Code'
+	1: 'Layout Testing'
 };
 
 const CONTROL_CONVERSION_NETWORKS = {
