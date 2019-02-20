9442f61ff2c872c569c066da81b792041272de8c
ZahinAdPushup <zahin@adpushup.com>
2019-02-20 07:12:13 +0530

    [Code changes] (Frontend: New SPA) App MySites: Card body dummy UI added

--- Client/Pages/Sites/components/index.jsx
+++ Client/Pages/Sites/components/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Panel } from 'react-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -20,7 +21,7 @@ class MySites extends Component {
 			<ActionCard title="My Sites">
 				<div className="u-padding-h4 u-padding-v5 aligner aligner--row aligner--wrap">
 					<Card
-						type="info"
+						type="success"
 						headerClassName="card-header"
 						headerChildren={
 							<div className="aligner aligner--row">
@@ -37,7 +38,23 @@ class MySites extends Component {
 								</OverlayTooltip>
 							</div>
 						}
-						bodyChildren={<div>Body text</div>}
+						bodyClassName="card-body"
+						bodyChildren={
+							<Panel className="panel--transparent u-margin-b3">
+								<Panel.Heading className="u-margin-0 u-padding-0">Setup status</Panel.Heading>
+								<Panel.Body className="u-padding-h0 u-padding-b0 u-padding-t2">
+									<div className="aligner aligner--row">
+										<span className="aligner-item">Site is active</span>
+										<a
+											className="aligner-item aligner aligner--hEnd"
+											href="//localhost:8080/dashboard"
+										>
+											Complete Setup
+										</a>
+									</div>
+								</Panel.Body>
+							</Panel>
+						}
 						footerChildren={<div>Footer text</div>}
 					/>
 				</div>
