9a2936ebec24fce7793fed641a39c7c7d504803b
ZahinAdPushup <zahin@adpushup.com>
2019-02-20 08:26:29 +0530

    [Code changes] (Frontend: New SPA) App MySites: Card footer dummy UI added

--- Client/Pages/Sites/components/index.jsx
+++ Client/Pages/Sites/components/index.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import { Panel, Button } from 'react-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faThumbsUp, faChartArea, faCog } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import ActionCard from '../../../Components/ActionCard/index';
 import OverlayTooltip from '../../../Components/OverlayTooltip/index';
 import Card from '../../../Components/Layout/Card';
 
-library.add(faCheckCircle);
+library.add(faCheckCircle, faThumbsUp, faChartArea, faCog);
 
 class MySites extends Component {
 	constructor(props) {
@@ -44,7 +44,10 @@ class MySites extends Component {
 								<Panel.Heading className="u-margin-0 u-padding-0">Setup status</Panel.Heading>
 								<Panel.Body className="u-padding-h0 u-padding-b0 u-padding-t2">
 									<div className="aligner aligner--row">
-										<span className="aligner-item">Site is active</span>
+										<span className="aligner-item">
+											<FontAwesomeIcon icon="check-circle" className="u-margin-r2" />
+											Setup completed
+										</span>
 										<a
 											className="aligner-item aligner aligner--hEnd"
 											href="//localhost:8080/dashboard"
@@ -55,7 +58,19 @@ class MySites extends Component {
 								</Panel.Body>
 							</Panel>
 						}
-						footerChildren={<div>Footer text</div>}
+						footerClassName="card-footer u-padding-0"
+						footerChildren={
+							<div className="aligner aligner--row">
+								<Button className="aligner-item aligner aligner--hStart aligner--vCenter">
+									<FontAwesomeIcon icon="chart-area" className="u-margin-r2" />
+									View Reports
+								</Button>
+								<Button className="aligner-item aligner aligner--hEnd aligner--vCenter">
+									<FontAwesomeIcon icon="cog" className="u-margin-r2" />
+									Manage Site
+								</Button>
+							</div>
+						}
 					/>
 				</div>
 			</ActionCard>
