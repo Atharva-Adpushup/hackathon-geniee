987a14a52fe763ca0821b22565d308de5ccd11b0
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-03-01 15:34:06 +0530

    Made Custom Message dismissble and updated React version

--- Client/Components/CustomMessage/index.jsx
+++ Client/Components/CustomMessage/index.jsx
@@ -15,42 +15,46 @@ const getClassnames = type => {
 };
 
 const CustomMessage = props => {
-	// const [visible, setVisible] = useState(true);
-	const { header, message, type, rootClassNames } = props;
+	const [visible, setVisible] = useState(true);
+	const { header, message, type, rootClassNames, dismissible } = props;
 	const classNames = getClassnames(type);
 
-	return (
+	return visible ? (
 		<div
 			className={`custom-message u-margin-b2 u-padding-v4 u-padding-h3 ${classNames} ${rootClassNames}`}
 		>
 			<h3 className="aligner aligner--row u-padding-h4 u-text-bold u-margin-0">
 				<span className="aligner-item aligner aligner--hStart">{header}</span>
-				<div className="aligner-item aligner aligner--hEnd">
-					<FontAwesomeIcon
-						icon="times"
-						className="u-cursor-pointer"
-						onClick={() => setVisible(false)}
-					/>
-				</div>
+				{dismissible ? (
+					<div className="aligner-item aligner aligner--hEnd">
+						<FontAwesomeIcon
+							icon="times"
+							className="u-cursor-pointer"
+							onClick={() => setVisible(false)}
+						/>
+					</div>
+				) : null}
 			</h3>
 			<hr className="u-margin-v3" />
 			<p className="u-margin-0 u-padding-h4" dangerouslySetInnerHTML={{ __html: message }} />
 		</div>
-	);
+	) : null;
 };
 
 CustomMessage.propTypes = {
 	header: PropTypes.string,
 	message: PropTypes.string,
 	type: PropTypes.string,
-	rootClassNames: PropTypes.string
+	rootClassNames: PropTypes.string,
+	dismissible: PropTypes.bool
 };
 
 CustomMessage.defaultProps = {
 	header: 'Header',
 	message: 'Message',
 	type: 'info',
-	rootClassNames: ''
+	rootClassNames: '',
+	dismissible: false
 };
 
 export default CustomMessage;
