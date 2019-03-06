d087f1884dbdd5786a43bef18c0da5a9fe02ce75
ZahinAdPushup <zahin@adpushup.com>
2019-03-06 21:27:47 +0530

    [Code changes] (Frontend: New SPA) Global App: Breadcrumbs implementation started

--- Client/Components/Shell/index.js
+++ Client/Components/Shell/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import Breadcrumbs from 'react-router-dynamic-breadcrumbs';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Loader from '../Loader/index';
+import { getTruthyArray } from '../../helpers/commonFunctions';
 
 class Shell extends React.Component {
 	state = { isSidebarOpen: true };
@@ -22,9 +24,33 @@ class Shell extends React.Component {
 		</div>
 	);
 
+	getRoutes = collection => {
+		const dynamicParamsArray = [':siteId'];
+
+		return collection.reduce((accumulator, value) => {
+			const { path } = value.props;
+			let { name } = value.props;
+
+			dynamicParamsArray.forEach(param => {
+				const isParamInPath = !!path.match(param);
+
+				if (!isParamInPath) {
+					return true;
+				}
+
+				name = `${name} ${param}`;
+				return true;
+			});
+
+			accumulator[path] = name;
+			return accumulator;
+		}, {});
+	};
+
 	render() {
 		const { isSidebarOpen } = this.state;
 		const { children, fetched } = this.props;
+		const routes = this.getRoutes(children);
 
 		return (
 			<Grid fluid>
@@ -35,7 +61,10 @@ class Shell extends React.Component {
 				</Row>
 				<Row className="sidebar-main-wrap">
 					<Sidebar show={isSidebarOpen} />
-					<main className="main-content">{fetched ? children : this.renderLoader()}</main>
+					<main className="main-content">
+						<Breadcrumbs mappedRoutes={routes} />
+						{fetched ? children : this.renderLoader()}
+					</main>
 				</Row>
 			</Grid>
 		);
