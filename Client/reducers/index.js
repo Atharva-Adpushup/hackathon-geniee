4c3051589aa1f9ceefcb317412326b638644f76f
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-03-06 17:56:04 +0530

    Resetting Global state on logout

--- Client/reducers/index.js
+++ Client/reducers/index.js
@@ -1,6 +1,16 @@
 import { combineReducers } from 'redux';
+import { USER_ACTIONS } from '../constants/global';
 import apps from './apps/index';
 import global from './global/index';
 import routes from './routes/index';
 
-export default combineReducers({ apps, global, routes });
+const appReducer = combineReducers({ apps, global, routes });
+const rootReducer = (state, action) => {
+	if (action.type === USER_ACTIONS.LOGOUT_USER) {
+		state = undefined;
+	}
+
+	return appReducer(state, action);
+};
+
+export default rootReducer;
