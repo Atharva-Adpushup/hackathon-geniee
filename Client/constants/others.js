516b4d474e54a770605ac2b9ebf7685f4feceb46
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-03-11 16:54:57 +0530

    ESLint Fixes

--- Client/constants/others.js
+++ Client/constants/others.js
@@ -1,13 +1,14 @@
 const ADSTXT_SITE_LIST_HEADERS = [
-		{ displayText: 'Domain', key: 'Domain' },
-		{ displayText: 'Status', key: 'status' },
-		{ displayText: 'Action', key: 'action' }
-	],
-	ADSTXT_STATUS = {
-		0: 'Entries Upto Date',
-		1: 'Missing Entries',
-		2: 'No Entries Found',
-		3: 'No Ads.txt Found'
-	};
+	{ displayText: 'Domain', key: 'Domain' },
+	{ displayText: 'Status', key: 'status' },
+	{ displayText: 'Action', key: 'action' }
+];
+
+const ADSTXT_STATUS = {
+	0: 'Entries Upto Date',
+	1: 'Missing Entries',
+	2: 'No Entries Found',
+	3: 'No Ads.txt Found'
+};
 
 export { ADSTXT_SITE_LIST_HEADERS, ADSTXT_STATUS };
