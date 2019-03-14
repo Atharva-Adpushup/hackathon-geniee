a7572e40f02559ff22c889dc8bc64b1b27b881ad
TanveeAdpushup <tanvee.gujral@adpushup.com>
2019-03-14 14:29:16 +0530

    initial control layout

---
+++ Client/Apps/Reporting/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Panel from './components/Panel';
+import '../../scss/apps/reporting/index.scss';
+
+const App = props => <Panel {...props} />;
+
+export default App;
