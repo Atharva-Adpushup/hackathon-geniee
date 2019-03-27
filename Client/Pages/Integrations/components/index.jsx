04de32f2b120cbca8af0dc3fcc97fab3d98813a0
ZahinAdPushup <zahin@adpushup.com>
2019-03-27 17:40:00 +0530

    [Code changes] (Frontend: New SPA) Google Account Integration: UI functionality complete with dummy data

--- Client/Pages/Integrations/components/index.jsx
+++ Client/Pages/Integrations/components/index.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Nav, NavItem } from 'react-bootstrap';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import ActionCard from '../../../Components/ActionCard/index';
 import HeroFeature from '../../../Components/Layout/HeroFeature';
 import CustomButton from '../../../Components/CustomButton/index';
 import { INTG_NAV_ITEMS, INTG_NAV_ITEMS_INDEXES, INTG_NAV_ITEMS_VALUES } from '../constants/index';
 
+library.add(faCheckCircle);
+
 class Integrations extends Component {
 	state = {
-		redirectUrl: ''
+		redirectUrl: '',
+		isGoogleOauthConnected: false
+	};
+
+	$window = window;
+
+	componentDidMount() {
+		this.addPostMessageListener();
+	}
+
+	addPostMessageListener = () => {
+		const { $window } = this;
+
+		// TODO: Implement an elegant way to bind class object to its method than .bind(this)
+		$window.addEventListener('message', this.handlePostMessageHandler.bind(this), false);
 	};
 
 	getActiveTab = () => {
@@ -17,6 +38,33 @@ class Integrations extends Component {
 		} = this.props;
 
 		return activeTab;
+	};
+
+	openGoogleOauthWindow = () => {
+		const { $window } = this;
+		const { screen } = $window;
+		const x = screen.width / 2 - 700 / 2;
+		const y = screen.height / 2 - 450 / 2;
+		$window.open(
+			'/api/user/requestGoogleOAuth',
+			'Oauth Request',
+			`height=485,width=700,left=${x},top=${y}`
+		);
+	};
+
+	handleClickHandler = inputParam => {
+		const { target } = inputParam;
+
+		const { name } = target;
+
+		switch (name) {
+			case 'connectGoogleAccountButton':
+				this.openGoogleOauthWindow();
+				break;
+
+			default:
+				break;
+		}
 	};
 
 	handleNavSelect = value => {
@@ -35,8 +83,62 @@ class Integrations extends Component {
 		this.setState({ redirectUrl });
 	};
 
+	handlePostMessageHandler(event) {
+		const isStringData = !!(typeof event.data === 'string');
+		const parsedData = isStringData ? JSON.parse(event.data) : event.data;
+		const isValidData = !!(
+			Object.keys(parsedData).length === 2 &&
+			parsedData.cmd &&
+			parsedData.data &&
+			parsedData.data.adsenseEmail &&
+			parsedData.data.pubId
+		);
+
+		if (!isValidData) {
+			return false;
+		}
+
+		console.log('Got post message data', parsedData);
+		this.setState({ isGoogleOauthConnected: true });
+
+		return true;
+	}
+
+	renderAccountConnectedUI = () => (
+		<React.Fragment>
+			<FontAwesomeIcon size="5x" icon="check-circle" className="u-margin-b4" />
+			<h3 className="u-text-bold">Your Google account is connected.</h3>
+		</React.Fragment>
+	);
+
+	renderDefaultDescriptionUI = () => (
+		<React.Fragment>
+			<h3 className="u-margin-t3 u-margin-b4 u-text-bold">Connect your Google account</h3>
+			<p className="u-margin-b4 text-center">
+				Connecting your Google account with AdPushup helps us to access your inventory and reports
+				for all Google services and manage them conveniently.
+			</p>
+			<CustomButton
+				variant="primary"
+				className=""
+				name="connectGoogleAccountButton"
+				onClick={this.handleClickHandler}
+			>
+				Get Started
+			</CustomButton>
+		</React.Fragment>
+	);
+
 	renderContent = () => {
 		const activeTab = this.getActiveTab();
+		const { isGoogleOauthConnected } = this.state;
+		const leftChildrenUI = isGoogleOauthConnected
+			? this.renderAccountConnectedUI()
+			: this.renderDefaultDescriptionUI();
+		const computedRootClassName = isGoogleOauthConnected
+			? 'u-padding-v5 u-padding-h4 u-bg-color-success'
+			: 'u-padding-v5 u-padding-h4';
+		const computedLeftChildrenClassName = isGoogleOauthConnected ? 'u-color-success' : '';
 
 		switch (activeTab) {
 			default:
@@ -44,24 +146,9 @@ class Integrations extends Component {
 				// return <div className="u-padding-v5 u-padding-h5">Integrations will be done here</div>;
 				return (
 					<HeroFeature
-						rootClassName="u-padding-v5 u-padding-h4"
-						leftChildren={
-							<React.Fragment>
-								<h3 className="u-margin-t3 u-margin-b4 u-text-bold">Connect your Google account</h3>
-								<p className="u-margin-b4 text-center">
-									Connecting your Google account with AdPushup helps us to access your inventory and
-									reports for all Google services and manage them conveniently.
-								</p>
-								<CustomButton
-									variant="primary"
-									className=""
-									name="connectGoogleAccountButton"
-									onClick={() => {}}
-								>
-									Get Started
-								</CustomButton>
-							</React.Fragment>
-						}
+						rootClassName={computedRootClassName}
+						leftChildrenClassName={computedLeftChildrenClassName}
+						leftChildren={leftChildrenUI}
 						imageUrl="/assets/images/onboarding/3.png"
 						imageAlt="Connect Google Account"
 					/>
