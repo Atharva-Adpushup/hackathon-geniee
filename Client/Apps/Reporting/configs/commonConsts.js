4541a8d64d38b6744c3ce24fecc732237c1cd9ca
TanveeAdpushup <tanvee.gujral@adpushup.com>
2019-03-27 14:37:13 +0530

    reportimg changes

--- Client/Apps/Reporting/configs/commonConsts.js
+++ Client/Apps/Reporting/configs/commonConsts.js
@@ -19,4 +19,72 @@ const REPORTS_NAV_ITEMS = {
 	}
 };
 
-export { REPORTS_NAV_ITEMS, REPORTS_NAV_ITEMS_INDEXES, REPORTS_NAV_ITEMS_VALUES };
+const dimensions = {
+		site: { display_name: 'Site' },
+		device: { display_name: 'Device Type' },
+		bidder: { display_name: 'Bidder' },
+		country: { display_name: 'Country' },
+		demand: { display_name: 'Demand Source' },
+		days: { display_name: 'Days' },
+		weeks: { display_name: 'Weeks' },
+		months: { display_name: 'Months' },
+		cummulative: { display_name: 'Cummulative' }
+	},
+	filters = {
+		site: { display_name: 'Site' },
+		device: { display_name: 'Device Type' },
+		bidder: { display_name: 'Bidder' }
+	},
+	filtersValues = {
+		site: [
+			{
+				id: 1,
+				value: 'abc.com'
+			},
+			{
+				id: 2,
+				value: 'pqr.com'
+			},
+			{
+				id: 3,
+				value: 'xyz.com'
+			}
+		],
+		device: [
+			{
+				id: 1,
+				value: 'Desktop'
+			},
+			{
+				id: 2,
+				value: 'Mobile'
+			},
+			{
+				id: 3,
+				value: 'Tablet'
+			}
+		],
+		bidder: [
+			{
+				id: 1,
+				value: 'C1X'
+			},
+			{
+				id: 2,
+				value: 'Criteo'
+			},
+			{
+				id: 3,
+				value: 'Pubmatic'
+			}
+		]
+	};
+
+export {
+	REPORTS_NAV_ITEMS,
+	REPORTS_NAV_ITEMS_INDEXES,
+	REPORTS_NAV_ITEMS_VALUES,
+	dimensions,
+	filters,
+	filtersValues
+};
