04868699800d8c6efddc4c949e7dd3d0b4d844bf
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-06-03 17:45:15 +0530

    Nav styling updated to match overall theme styling, added containers directory

--- Client/Apps/OpsPanel/components/Settings/index.jsx
+++ Client/Apps/OpsPanel/components/Settings/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Tab, Nav, NavItem, Row, Col } from 'react-bootstrap';
+import Sites from './Sites';
 
 const Settings = () => (
 	<div className="u-padding-v4">
 		<Tab.Container id="ops-panel-settings-container" defaultActiveKey="sites">
 			<Row className="clearfix">
 				<Col sm={2}>
-					<Nav bsStyle="pills" stacked>
+					<Nav bsStyle="pills" bsClass="ap-nav-pills nav" stacked>
 						<NavItem eventKey="sites">Sites</NavItem>
 						<NavItem eventKey="account">Account</NavItem>
 					</Nav>
 				</Col>
 				<Col sm={10}>
 					<Tab.Content animation>
-						<Tab.Pane eventKey="sites">Sites Info</Tab.Pane>
+						<Tab.Pane eventKey="sites">
+							<Sites />
+						</Tab.Pane>
 						<Tab.Pane eventKey="account">Account Info</Tab.Pane>
 					</Tab.Content>
 				</Col>
