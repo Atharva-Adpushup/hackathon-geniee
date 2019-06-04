130b61ff23e70d9303410a3f59f1b4e72cf7fbfb
ArunAdPushup <arun.dsouza@adpushup.com>
2019-06-04 12:04:22 +0530

    Minor refactoring in feedback module

--- services/genieeAdSyncService/adpTagsv2/modules/adpTags/hbScript/src/feedback.js
+++ services/genieeAdSyncService/adpTagsv2/modules/adpTags/hbScript/src/feedback.js
@@ -10,19 +10,21 @@ var feedback = {
         }
 
         slot.feedbackSent = true;
+        var winner = slot.feedback.winner || constants.DEFAULT_WINNER;
+        var winningRevenue = slot.feedback.winningRevenue || 0;
         var feedbackData = {
             siteId: config.SITE_ID,
             siteDomain: adp.config.siteDomain,
             bids: [
                 {
-                    bidder: slot.feedback.winner || constants.DEFAULT_WINNER,
-                    revenue: slot.feedback.winningRevenue || 0
+                    bidder: winner,
+                    revenue: winningRevenue
                 }
             ],
             mode: adp.config.mode,
             errorCode: constants.ERROR_CODES.NO_ERROR,
-            winner: slot.feedback.winner || config.DEFAULT_WINNER,
-            winningRevenue: slot.feedback.winningRevenue || 0,
+            winner: winner,
+            winningRevenue: winningRevenue,
             winnerAdUnitId: slot.feedback.winnerAdUnitId || null,
             timedOutBidders: [],
             sectionId: slot.sectionId,
