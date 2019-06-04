0ae11ae25ac68a2d2602608f380188ce4d1a683b
ravijagga <mr.ravi.jagga@gmail.com>
2019-06-04 16:02:19 +0530

    Created Manage Inventory API

--- Client/Apps/HeaderBidding/components/AdSizeSelector.jsx
+++ Client/Apps/HeaderBidding/components/AdSizeSelector.jsx
@@ -69,13 +69,15 @@ class AdSizeSelector extends React.Component {
 	render() {
 		const { activeKey } = this.state;
 		return (
-			<Row className="clearfix">
-				<Col sm={4}>
+			<Row className="clearfix add-size-wrap">
+				<Col sm={4} className="add-size-tabs">
 					<Nav bsStyle="pills" stacked activeKey={activeKey} onSelect={this.handleNavSelect}>
 						{this.renderTabs()}
 					</Nav>
 				</Col>
-				<Col sm={8}>{this.renderTabContent()}</Col>
+				<Col sm={8} className="add-size-content">
+					{this.renderTabContent()}
+				</Col>
 			</Row>
 		);
 	}
