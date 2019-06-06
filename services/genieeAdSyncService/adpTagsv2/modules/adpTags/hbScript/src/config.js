75aa473eb1bc0cd5e9a4e854fe604ff9ad4e08c2
ArunAdPushup <arun.dsouza@adpushup.com>
2019-06-06 16:29:26 +0530

    Sample config added

--- services/genieeAdSyncService/adpTagsv2/modules/adpTags/hbScript/src/config.js
+++ services/genieeAdSyncService/adpTagsv2/modules/adpTags/hbScript/src/config.js
@@ -7,8 +7,36 @@ var config = {
             "300x250": ["ADP_37780_300X250_e342d0a9-0089-42fe-8897-49342aa190e5"]
         },
         hbConfig: {
-            bidderAdUnits: {
-                "300x250": []
+            "conversant": {
+                "name": "Conversant",
+                "isApRelation": true,
+                "isPaused": false,
+                "sizeLess": false,
+                "reusable": true,
+                "bids": "net",
+                "config": {
+                    "300x50": {
+                        "secure": 1,
+                        "site_id": "565"
+                    },
+                    "120x600": {
+                        "secure": 1,
+                        "site_id": "565464"
+                    }
+                }
+            },
+            "33Across": {
+                "name": "33 Across",
+                "isApRelation": true,
+                "isPaused": false,
+                "sizeLess": true,
+                "reusable": true,
+                "bids": "gross",
+                "revenueShare": "23",
+                "config": {
+                    "productId": "inview",
+                    "siteId": "345345"
+                }
             }
         }
     }, //__INVENTORY__
