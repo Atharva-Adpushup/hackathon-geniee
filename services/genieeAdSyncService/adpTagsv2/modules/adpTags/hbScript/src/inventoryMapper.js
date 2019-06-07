4c05e749a931a15ca4c9b9c441ed457775a9dec2
ArunAdPushup <arun.dsouza@adpushup.com>
2019-06-07 18:03:09 +0530

    New inventory mapping enabled

--- services/genieeAdSyncService/adpTagsv2/modules/adpTags/hbScript/src/inventoryMapper.js
+++ services/genieeAdSyncService/adpTagsv2/modules/adpTags/hbScript/src/inventoryMapper.js
@@ -2,64 +2,64 @@
 
 var adp = require('./adp');
 var inventoryMapper = {
-    //  inventoryMapper: function (size, optionalParam) {
-    //         // Reset inventory as default if site is SPA
-    //         if (adp.config.isSPA) {
-    //             inventory = adp.$.extend(true, {}, adp.adpTags.defaultInventory);
-    //         }
+    inventoryMapper: function (size, optionalParam) {
+        // Reset inventory as default if site is SPA
+        if (adp.config.isSPA) {
+            inventory = adp.$.extend(true, {}, adp.adpTags.defaultInventory);
+        }
 
-    //         var width = size[0];
-    //         var height = size[1];
-    //         var size = width + 'x' + height;
-    //         var dfpAdUnit = null;
-    //         var availableSlots = inventory.dfpAdUnits[size];
-    //         var bidders = [];
-    //         var hbConfig = inventory.hbConfig;
+        var width = size[0];
+        var height = size[1];
+        var size = width + 'x' + height;
+        var dfpAdUnit = null;
+        var availableSlots = inventory.dfpAdUnits[size];
+        var bidders = [];
+        var hbConfig = inventory.hbConfig;
 
-    //         if (optionalParam.headerBidding && hbConfig && Object.keys(hbConfig).length) {
-    //             var updatedSize = size;
-    //             if (optionalParam.overrideActive && optionalParam.overrideSizeTo) {
-    //                 updatedSize = optionalParam.overrideSizeTo;
-    //             }
+        if (optionalParam.headerBidding && hbConfig && Object.keys(hbConfig).length) {
+            var updatedSize = size;
+            if (optionalParam.overrideActive && optionalParam.overrideSizeTo) {
+                updatedSize = optionalParam.overrideSizeTo;
+            }
 
-    //             Object.keys(hbConfig).forEach(function (bidder) {
-    //                 var bidderData = hbConfig[bidder];
+            Object.keys(hbConfig).forEach(function (bidder) {
+                var bidderData = hbConfig[bidder];
 
-    //                 if (!bidderData.isPaused) {
-    //                     if (bidderData.sizeLess) {
-    //                         bidders.push({
-    //                             bidder: bidder,
-    //                             params: bidderData.config
-    //                         });
-    //                     }
+                if (!bidderData.isPaused) {
+                    if (bidderData.sizeLess) {
+                        bidders.push({
+                            bidder: bidder,
+                            params: bidderData.config
+                        });
+                    }
 
-    //                     if (!bidderData.sizeLess && bidderData.reusable) {
-    //                         bidders.push({
-    //                             bidder: bidder,
-    //                             params: bidderData.config[updatedSize]
-    //                         });
-    //                     }
-    //                 }
-    //             });
-    //         }
+                    if (!bidderData.sizeLess && bidderData.reusable) {
+                        bidders.push({
+                            bidder: bidder,
+                            params: bidderData.config[updatedSize]
+                        });
+                    }
+                }
+            });
+        }
 
-    //         if (availableSlots.length) {
-    //             if (optionalParam.dfpAdunit && availableSlots.indexOf(optionalParam.dfpAdunit) !== -1) {
-    //                 if (optionalParam.isManual) {
-    //                     dfpAdUnit = optionalParam.dfpAdunit;
-    //                 } else {
-    //                     dfpAdUnit = availableSlots.splice(availableSlots.indexOf(optionalParam.dfpAdunit), 1)[0];
-    //                 }
-    //             } else {
-    //                 dfpAdUnit = inventory.dfpAdUnits[size].pop();
-    //             }
-    //         }
+        if (availableSlots.length) {
+            if (optionalParam.dfpAdunit && availableSlots.indexOf(optionalParam.dfpAdunit) !== -1) {
+                if (optionalParam.isManual) {
+                    dfpAdUnit = optionalParam.dfpAdunit;
+                } else {
+                    dfpAdUnit = availableSlots.splice(availableSlots.indexOf(optionalParam.dfpAdunit), 1)[0];
+                }
+            } else {
+                dfpAdUnit = inventory.dfpAdUnits[size].pop();
+            }
+        }
 
-    //         return {
-    //             dfpAdUnit: dfpAdUnit,
-    //             bidders: bidders
-    //         };
-    //     },
+        return {
+            dfpAdUnit: dfpAdUnit,
+            bidders: bidders
+        };
+    },
 
     set: function (inventory, size, optionalParam) {
         // Reset inventory as default if site is SPA
