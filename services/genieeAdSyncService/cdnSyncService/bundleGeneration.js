d429156f073c0c46b2e3cf5f837a8f20ef71147e
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-06-11 22:36:45 +0530

    Updated Webpack Server Config, added Babel Loader

--- services/genieeAdSyncService/cdnSyncService/bundleGeneration.js
+++ services/genieeAdSyncService/cdnSyncService/bundleGeneration.js
@@ -16,45 +16,41 @@ function init(site, config) {
 		if (!statuses || !Object.keys(statuses).length) {
 			return reject(new AdPushupError(`Invalid Service Statuses for site: ${site.get('siteId')}`));
 		}
-		const compiler = webpack([
-			{
-				entry: {
-					bundle: path.join(__dirname, '..', 'adpushup.js', 'main.js')
-				},
-				output: {
-					path: path.join(__dirname, buildPath),
-					filename: 'bundle.js'
-				},
-				module: {
-					rules: [
-						{
-							test: /.jsx?$/,
-							loader: 'babel-loader',
-							exclude: /node_modules/,
-							options: {
-								presets: ['@babel/preset-env', '@babel/preset-react'],
-								plugins: [
-									'@babel/plugin-syntax-dynamic-import',
-									'@babel/plugin-proposal-class-properties',
-									'@babel/plugin-proposal-export-namespace-from',
-									'@babel/plugin-proposal-throw-expressions',
-									'@babel/plugin-transform-react-jsx-source'
-								]
-							}
-						},
-						{
-							test: /.css?$/,
-							loader: ['style-loader', 'css-loader']
-						},
-						{
-							test: /\.(eot|svg|ttf|woff|woff2)$/,
-							loader: 'url-loader'
+		const compiler = webpack({
+			entry: path.join(__dirname, '..', 'adpushup.js', 'main.js'),
+			output: {
+				path: path.join(__dirname, buildPath),
+				filename: 'bundle.js'
+			},
+			module: {
+				rules: [
+					{
+						test: /.jsx?$/,
+						loader: 'babel-loader',
+						exclude: /node_modules/,
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react'],
+							plugins: [
+								'@babel/plugin-syntax-dynamic-import',
+								'@babel/plugin-proposal-class-properties',
+								'@babel/plugin-proposal-export-namespace-from',
+								'@babel/plugin-proposal-throw-expressions',
+								'@babel/plugin-transform-react-jsx-source'
+							]
 						}
-					]
-				},
-				plugins: [new webpack.DefinePlugin({ ...statuses, SITE_ID: JSON.stringify(siteId) })]
-			}
-		]);
+					},
+					{
+						test: /.css?$/,
+						loader: ['style-loader', 'css-loader']
+					},
+					{
+						test: /\.(eot|svg|ttf|woff|woff2)$/,
+						loader: 'url-loader'
+					}
+				]
+			},
+			plugins: [new webpack.DefinePlugin({ ...statuses, SITE_ID: JSON.stringify(siteId) })]
+		});
 		new webpack.ProgressPlugin().apply(compiler);
 		compiler.run((err, stats) => {
 			return err ? reject(err) : resolve();
