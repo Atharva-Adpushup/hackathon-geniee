bc3fc328527eab6b95bd2645c5241d0be80c2e46
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-07-02 15:34:25 +0530

    Updated Page Titles

--- Client/Apps/OpsPanel/index.jsx
+++ Client/Apps/OpsPanel/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import OpsPanel from './components/index';
 import '../../scss/apps/opsPanel/index.scss';
 
-const App = props => <OpsPanel {...props} />;
+const App = props => (
+	<React.Fragment>
+		<Helmet>
+			<title>Ops Panel</title>
+		</Helmet>
+		<OpsPanel {...props} />
+	</React.Fragment>
+);
 
 export default App;
