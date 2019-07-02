9502732bcc62d3bba96fc6d579eaab6e8560ea57
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-07-02 12:22:07 +0530

    Added BackupAds Creation Tool

--- apiControllers/dataController.js
+++ apiControllers/dataController.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const atob = require('atob');
+
 const userModel = require('../models/userModel');
 const siteModel = require('../models/siteModel');
 const utils = require('../helpers/utils');
@@ -6,6 +8,9 @@ const CC = require('../configs/commonConsts');
 const httpStatus = require('../configs/httpStatusConsts');
 const pagegroupCreationAutomation = require('../services/pagegroupCreationAutomation');
 const AdpushupError = require('../helpers/AdPushupError');
+const { checkParams, errorHandler, verifyOwner } = require('../helpers/routeHelpers');
+const { sendSuccessResponse } = require('../helpers/commonFunctions');
+const upload = require('../helpers/uploadToCDN');
 
 const router = express.Router();
 
@@ -78,6 +83,35 @@ router
 					return res.status(httpStatus.BAD_REQUEST).send({ error: e.message[0].message });
 				return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error: e.message });
 			});
+	})
+	.post('/createBackupAd', (req, res) => {
+		const { siteId, content, format } = req.body;
+		const randomId = utils.randomString();
+
+		function cwd(ftp) {
+			const path = `/${siteId}/ads/`;
+			return ftp.cwd(path).catch(() => ftp.mkdir(path).then(() => ftp.cwd(path)));
+		}
+
+		return checkParams(['siteId', 'content'], req, 'post')
+			.then(() => verifyOwner(siteId, req.user.email))
+			.then(() => {
+				const decodedContent = atob(content);
+				return upload(cwd, {
+					content: decodedContent,
+					filename: `${randomId}.${format}`
+				});
+			})
+			.then(() => {
+				sendSuccessResponse(
+					{
+						message: 'Operation Successful',
+						url: `https://cdn.adpushup.com/${siteId}/ads/${randomId}.${format}`
+					},
+					res
+				);
+			})
+			.catch(err => errorHandler(err, res, httpStatus.INTERNAL_SERVER_ERROR));
 	});
 
 module.exports = router;
