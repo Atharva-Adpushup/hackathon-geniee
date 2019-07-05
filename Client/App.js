e45a33fba0267711de9e8ef7164b67995cbbf0a9
ravijagga <mr.ravi.jagga@gmail.com>
2019-07-05 16:07:44 +0530

    Added GTM Script in header

--- Client/App.js
+++ Client/App.js
@@ -1,4 +1,6 @@
+/* eslint-disable func-names */
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { hot } from 'react-hot-loader/root';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
@@ -77,9 +79,31 @@ library.add(
 );
 
 const App = () => (
-	// <div id="app">
-	// </div>
 	<ErrorBoundary>
+		<Helmet>
+			{/* Google Tag Manager */}
+			<script>
+				{(function(w, d, s, l, i) {
+					// eslint-disable-next-line no-param-reassign
+					w[l] = w[l] || [];
+					w[l].push({
+						'gtm.start': new Date().getTime(),
+						event: 'gtm.js'
+					});
+					const f = d.getElementsByTagName(s)[0];
+
+					const j = d.createElement(s);
+
+					// eslint-disable-next-line eqeqeq
+					const dl = l != 'dataLayer' ? `&l=${l}` : '';
+					j.async = true;
+					j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
+					f.parentNode.insertBefore(j, f);
+				})(window, document, 'script', 'dataLayer', 'GTM-NMS9P3Q')}
+			</script>
+			{/* End Google Tag Manager */}
+		</Helmet>
+
 		<NotificationContainer />
 		<Routes />
 	</ErrorBoundary>
