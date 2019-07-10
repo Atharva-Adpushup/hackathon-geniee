412e7e398d7a98ea2d66faa062300c1d3767765f
TanveeAdpushup <gujral_tanvi@yahoo.com>
2019-07-10 13:55:25 +0530

    bug fixes

--- Client/Pages/Dashboard/index.js
+++ Client/Pages/Dashboard/index.js
@@ -3,13 +3,14 @@ import { Helmet } from 'react-helmet';
 
 import DashboardContainer from './containers/index';
 import '../../scss/pages/dashboard/index.scss';
+import { dashboardWidgets } from './configs/commonConsts';
 
 const Dashboard = props => (
 	<Fragment>
 		<Helmet>
 			<title>Dashboard</title>
 		</Helmet>
-		<DashboardContainer {...props} />
+		<DashboardContainer {...props} widgetsList={dashboardWidgets} />
 	</Fragment>
 );
 
--- Client/Pages/ManageSite/components/quickSnapshot.jsx
+++ Client/Pages/ManageSite/components/quickSnapshot.jsx
@@ -1,14 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import SiteWidgets from '../../Dashboard/containers/index';
 import '../../../scss/pages/dashboard/index.scss';
+import { siteWidgets } from '../constants/index';
 
 const QuickSnapshot = props => {
 	const { siteId } = props;
-	return (
-		<Fragment>
-			<SiteWidgets reportType="site" siteId={siteId} />
-		</Fragment>
-	);
+	return <SiteWidgets reportType="site" siteId={siteId} widgetsList={siteWidgets} />;
 };
 
 export default QuickSnapshot;
