ccc178832f82a33d42192605c0fd6ea85401b075
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-07-22 16:06:46 +0530

    User Bug fix

--- Client/Pages/ManageSite/containers/ManageSiteContainer.js
+++ Client/Pages/ManageSite/containers/ManageSiteContainer.js
@@ -4,7 +4,7 @@ import ManageSite from '../components/index';
 const mapStateToProps = (state, ownProps) => {
 	const { user } = state.global;
 	return {
-		user: user.fetched,
+		user: user.data,
 		...ownProps
 	};
 };
