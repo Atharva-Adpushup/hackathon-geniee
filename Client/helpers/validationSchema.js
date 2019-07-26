21200c64eceee06c383f30b369aa9afd2ef14c91
ZahinAdPushup <zahin@adpushup.com>
2019-07-26 17:29:49 +0530

    [Code changes] (Frontend: New Console) Updated password length schema

--- Client/helpers/validationSchema.js
+++ Client/helpers/validationSchema.js
@@ -22,18 +22,18 @@ module.exports = {
 				},
 				{
 					name: 'oldPassword',
-					message: 'Enter old password between 6 and 32',
-					value: { min: 6, max: 32 }
+					message: 'Enter old password between 6 and 50',
+					value: { min: 6, max: 50 }
 				},
 				{
 					name: 'password',
-					message: 'Enter password between 6 and 32',
-					value: { min: 6, max: 32 }
+					message: 'Enter password between 6 and 50',
+					value: { min: 6, max: 50 }
 				},
 				{
 					name: 'confirmPassword',
-					message: 'Enter confirm password between 6 and 32',
-					value: { min: 6, max: 32 }
+					message: 'Enter confirm password between 6 and 50',
+					value: { min: 6, max: 50 }
 				}
 			],
 			isEmail: [{ name: 'email', message: 'Enter email in name@example.com format', value: '' }],
