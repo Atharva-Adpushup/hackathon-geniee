ca5c5d448344ae9a026511d0b45d95274a3edee4
ravijagga <mr.ravi.jagga@gmail.com>
2019-07-29 15:03:11 +0530

    Merged hb app changes

---
+++ configs/hbVideoParamsMap.js
@@ -0,0 +1,37 @@
+module.exports = {
+	conversant: {
+		params: {
+			mimes: ['video/mp4', 'video/x-flv']
+		}
+	},
+	rubicon: {
+		params: {
+			video: {
+				playerWidth: 'IAB_WIDTH',
+				playerHeight: 'IAB_HEIGHT'
+			}
+		}
+	},
+	ix: {
+		params: {
+			video: {
+				mimes: ['video/mp4', 'video/x-flv'],
+				minduration: 0,
+				maxduration: 60,
+				protocols: [2, 3, 5, 6],
+				skippable: false
+			}
+		}
+	},
+	pubmatic: {
+		params: {
+			mimes: ['video/mp4', 'video/x-flv'],
+			skippable: false,
+			minduration: 0,
+			maxduration: 60,
+			playbackmethod: 2,
+			api: [1, 2, 3, 4, 5],
+			protocols: [1, 2, 3, 4, 5, 6]
+		}
+	}
+};
