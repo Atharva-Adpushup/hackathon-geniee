327e0abbbaa566f32db04cf6085bcce696b178cd
yash.garg <yash.garg@adpushup.com>
2019-08-27 14:42:12 +0530

    sites mapping

--- Client/Apps/OpsPanel/components/SiteMapping/index.jsx
+++ Client/Apps/OpsPanel/components/SiteMapping/index.jsx
@@ -1,34 +1,158 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
-import { CSVLink, CSVDownload } from 'react-csv';
+import { Glyphicon, Row } from 'react-bootstrap';
 
 import axiosInstance from '../../../../helpers/axiosInstance';
+import FilterBox from '../../../../Components/FilterBox';
+import Empty from '../../../../Components/Empty/index.jsx';
+import Loader from '../../../../Components/Loader/index';
 
 class SiteMapping extends Component {
 	state = {
 		data: [],
-		// downloadData: []
+		filteredData: [],
+		isLoading: false,
+		selectAll: false,
+		checked: [],
+		selectedData : []
 	};
 
 	componentDidMount() {
+		this.setState({ isLoading: true });
 		return axiosInstance
 			.get('https://jsonplaceholder.typicode.com/comments')
-			.then(res => this.setState({ data: res.data }));
+			.then(res => this.setState({ data: res.data, filteredData: res.data, isLoading: false }))
+			.catch(err => {
+				console.log(err);
+				this.setState({ isLoading: false });
+			});
 	}
 
-	// download = () => {
-	// 	let downloadFilteredData = this.reactTable.getResolvedState().sortedData;
-	// 	let filterdata = downloadFilteredData.map(
-	// 		({ _original, _index, _subRows, _nestingLevel, ...downloadFilteredData }) =>
-	// 			downloadFilteredData
-	// 	);
-	// 	this.setState({ downloadData: filterdata });
-	// };
-
-	render() {
+	handleChange = e => {
 		const { data } = this.state;
+		var selectAll = !this.state.selectAll;
+		this.setState({ selectAll: selectAll });
+		var checkedCopy = [];
+		data.forEach(() => {
+			checkedCopy.push(selectAll);
+		});
+		this.setState({
+			checked: checkedCopy
+		});
+	};
+
+	handleSingleCheckboxChange = index => {
+		const  { data} = this.state;
+		var checkedCopy = this.state.checked;
+		checkedCopy[index] = !this.state.checked[index];
+		if (checkedCopy[index] === false) {
+			this.setState({ selectAll: false });
+		}
+
+		this.setState({
+			checked: checkedCopy,
+			selectedData : data[index] 
+
+		});
+
+		console.log(this.state.selectedData)
+	};
+
+	getFilterBoxValues = key => {
+		const { data } = this.state;
+		const values = [...new Set([...data].map(data => data[key]))];
+
+		return values.map(value => ({ name: value }));
+	};
+
+	handleFilters = filters => {
+		const { data } = this.state;
+		let filteredData = [...data];
+
+		for (const prop in filters) {
+			const filter = filters[prop];
+			// eslint-disable-next-line no-continue
+			if (filter.values.length)
+				filteredData = filteredData.filter(value => filter.values.indexOf(value[prop]) !== -1);
+		}
+
+		this.setState({ filteredData });
+	};
+
+	renderFilterComponent() {
+		return (
+			<FilterBox
+				onFilter={this.onFilter}
+				availableFilters={[
+					{
+						name: 'User ID',
+						prop: 'postId',
+						values: this.getFilterBoxValues('postId')
+					},
+					{
+						name: 'ID',
+						prop: 'id',
+						values: this.getFilterBoxValues('id')
+					},
+					{
+						name: 'Title',
+						prop: 'name',
+						values: this.getFilterBoxValues('name')
+					},
+					{
+						name: 'Content',
+						prop: 'body',
+						values: this.getFilterBoxValues('body')
+					},
+					{
+						name: 'Email',
+						prop: 'email',
+						values: this.getFilterBoxValues('email')
+					}
+				]}
+				handleFilters={this.handleFilters}
+				className="u-margin-v5 u-margin-h4 "
+			/>
+		);
+	}
+
+	exportData = () => {
+		return (
+			<a
+				style={{
+					height: 33,
+					paddingTop: 8
+				}}
+				className="btn btn-lightBg btn-default btn-blue-line pull-right u-margin-r2 u-margin-t5"
+			>
+				<Glyphicon glyph="download-alt u-margin-r2" />
+				Export Report
+			</a>
+		);
+	};
+	render() {
+		const { isLoading, filteredData } = this.state;
 		const columns = [
+			{
+				Header: (
+					<input type="checkbox" onChange={this.handleChange} checked={this.state.selectAll} />
+				),
+				Cell: row => (
+					<input
+						type="checkbox"
+						defaultChecked={this.state.checked[row.index]}
+						checked={this.state.checked[row.index]}
+						onChange={() => this.handleSingleCheckboxChange(row.index)}
+					/>
+				),
+				sortable: false,
+				filterable: false,
+				width: 100,
+				maxWidth: 100,
+				minWidth: 100
+			},
+
 			{
 				Header: 'User ID',
 				accessor: 'postId',
@@ -45,37 +169,40 @@ class SiteMapping extends Component {
 			},
 			{
 				Header: 'Title',
-				accessor: 'name',
-				
+				accessor: 'name'
 			},
 			{
 				Header: 'Content',
-				accessor: 'body',
-				
+				accessor: 'body'
 			},
 			{
 				Header: 'Email',
-				accessor: 'email',
+				accessor: 'email'
 			}
 		];
-	
+
+		if (isLoading) return <Loader height="800px" classNames="u-margin-v3" />;
 
 		return (
-			<div>
-				{/* <button onClick={this.download} style={{ marginLeft: '10%', marginBottom: '10' }}>
-					<CSVLink data={downloadData}>Download me</CSVLink>
-				</button> */}
+			<React.Fragment>
+				<Row>
+					<div className="col-md-10">{this.renderFilterComponent()}</div>
+					<div className="col-md-2">{this.exportData()}</div>
+				</Row>
 
-				<ReactTable
-					ref={r => (this.reactTable = r)}
-					columns={columns}
-					data={data}
-					filterable = {false}
-					sortable ={false}
-					showPaginationTop
-					showPaginationBottom={false}
-				/>
-			</div>
+				{!filteredData || filteredData.length === 0 ? (
+					<Empty message=" No Data found " />
+				) : (
+					<ReactTable
+						columns={columns}
+						data={filteredData}
+						filterable={false}
+						showPaginationTop
+						showPaginationBottom={false}
+						className="u-padding-h3 u-padding-v2"
+					/>
+				)}
+			</React.Fragment>
 		);
 	}
 }
