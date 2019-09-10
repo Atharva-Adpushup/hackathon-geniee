ae798ea43211f9afa92368b330256eeb7e2f1fc1
Zahin Omar Alwa <zahin@adpushup.com>
2019-09-10 17:47:33 +0530

    [Code changes] (Frontend: Console React SPA) OpsPanel QuickSnapshot: Made datatable sortable and abstracted out its sass file

--- Client/Pages/Dashboard/components/SitewiseReport.jsx
+++ Client/Pages/Dashboard/components/SitewiseReport.jsx
@@ -30,7 +30,8 @@ function computeTableData(data, props) {
 				tableHeader.push({
 					title: metrics[col].display_name,
 					prop: col,
-					position: metrics[col].position + 1
+					position: metrics[col].position + 1,
+					sortable: true
 				});
 			}
 		});
@@ -45,7 +46,8 @@ function computeTableData(data, props) {
 			tableHeader.push({
 				title: 'Website',
 				prop: 'siteName',
-				position: 1
+				position: 1,
+				sortable: true
 			});
 		}
 
@@ -93,6 +95,33 @@ class SitewiseReport extends React.Component {
 
 	renderTable() {
 		const { tableBody, tableHeader } = this.state;
+		const onSortFunction = {
+			network_net_revenue(columnValue) {
+				if (typeof columnValue === 'string') return parseFloat(columnValue.replace(/[,$]/g, ''));
+				return columnValue;
+			},
+			network_gross_revenue(columnValue) {
+				if (typeof columnValue === 'string') return parseFloat(columnValue.replace(/[,$]/g, ''));
+				return columnValue;
+			},
+			adpushup_page_views(columnValue) {
+				if (typeof columnValue === 'string') return parseFloat(columnValue.replace(/,/g, ''));
+				return columnValue;
+			},
+			network_impressions(columnValue) {
+				if (typeof columnValue === 'string') return parseFloat(columnValue.replace(/,/g, ''));
+				return columnValue;
+			},
+			network_ad_ecpm(columnValue) {
+				if (typeof columnValue === 'string') return parseFloat(columnValue.replace(/[,$]/g, ''));
+				return columnValue;
+			},
+			adpushup_page_cpm(columnValue) {
+				if (typeof columnValue === 'string') return parseFloat(columnValue.replace(/[,$]/g, ''));
+				return columnValue;
+			}
+		};
+
 		return tableBody && tableBody.length > 0 ? (
 			<Datatable
 				tableHeader={tableHeader}
@@ -100,6 +129,7 @@ class SitewiseReport extends React.Component {
 				rowsPerPage={10}
 				rowsPerPageOption={[20, 30, 40, 50]}
 				keyName="reportTable"
+				onSort={onSortFunction}
 			/>
 		) : (
 			<div className="text-center">No Record Found.</div>
