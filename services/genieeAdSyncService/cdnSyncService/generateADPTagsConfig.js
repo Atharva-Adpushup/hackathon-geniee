708aef0189f3106f69c4f96f360eac273af82c02
ravijagga <mr.ravi.jagga@gmail.com>
2019-09-12 18:58:23 +0530

    Moved prebid bidder alias to network tree

--- services/genieeAdSyncService/cdnSyncService/generateADPTagsConfig.js
+++ services/genieeAdSyncService/cdnSyncService/generateADPTagsConfig.js
@@ -5,6 +5,7 @@ const _ = require('lodash');
 const CB_ERRORS = require('couchbase').errors;
 const couchBase = require('../../../configs/config').couchBase;
 const { docKeys } = require('../../../configs/commonConsts');
+const { getBiddersFromNetworkTree } = require('./commonFunctions');
 
 const dbHelper = couchbaseService(
 	`couchbase://${couchBase.HOST}`,
@@ -26,20 +27,41 @@ const generateDfpConfig = adpTags => {
 const generateHBConfig = siteId => {
 	// TODO: HB: review empty hbConfig
 	const emptyResponse = { hbcf: {} };
-	return dbHelper
-		.getDoc(`${docKeys.hb}${siteId}`)
-		.then(hbDoc => {
-			if (!Object.keys(hbDoc.value).length) {
-				return emptyResponse;
-			}
-			return hbDoc.value;
-		})
-		.catch(err => {
+
+	return Promise.join(
+		dbHelper.getDoc(`${docKeys.hb}${siteId}`).catch(err => {
 			if (err.code == CB_ERRORS.keyNotFound) {
 				return emptyResponse;
 			}
 			throw err;
-		});
+		}),
+		getBiddersFromNetworkTree()
+	).then(([hbDoc, biddersFromNetworkTree]) => {
+		if (!Object.keys(hbDoc.value).length) {
+			return emptyResponse;
+		}
+
+		const usedBidders = hbDoc.value.hbcf;
+		const activeUsedBidders = {};
+		for (const bidderCode in usedBidders) {
+			if (
+				usedBidders.hasOwnProperty(bidderCode) &&
+				!usedBidders[bidderCode].isPaused &&
+				biddersFromNetworkTree[bidderCode] &&
+				biddersFromNetworkTree[bidderCode].isActive
+			) {
+				activeUsedBidders[bidderCode] = usedBidders[bidderCode];
+
+				if (biddersFromNetworkTree[bidderCode].alias) {
+					activeUsedBidders[bidderCode].alias = biddersFromNetworkTree[bidderCode].alias;
+				}
+			}
+		}
+
+		hbDoc.value.hbcf = activeUsedBidders;
+
+		return hbDoc.value;
+	});
 };
 
 const genrateConfig = (adpTags, siteId) => {
