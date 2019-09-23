8b681bc41a48ab5f12c5de7c772a713c4a1dd928
yash.garg <yash.garg@adpushup.com>
2019-09-23 16:20:21 +0530

    minor changes

--- Client/Apps/OpsPanel/components/SiteMapping/index.jsx
+++ Client/Apps/OpsPanel/components/SiteMapping/index.jsx
@@ -77,7 +77,7 @@ class SiteMapping extends Component {
 		this.setState({
 			checked: checkedCopy,
 			selectedData,
-			selectAll: selectedData.length === filteredData.length
+			selectAll: selectedData.length === filteredData.length ? true : false
 		});
 	};
 
@@ -89,7 +89,7 @@ class SiteMapping extends Component {
 	};
 
 	handleFilterValues = arr => {
-		const array = [];
+		let array = [];
 		arr.map(({ name, prop }) => {
 			array.push({ name, prop, values: this.getFilterBoxValues(prop) });
 		});
@@ -149,16 +149,15 @@ class SiteMapping extends Component {
 		const { filteredData: { length = 0 } = {} } = this.state;
 
 		if (length <= 5) return 5;
-		if (length <= 10) return 10;
-		if (length <= 20) return 20;
-		if (length <= 50) return 50;
+		else if (length <= 10) return 10;
+		else if (length <= 20) return 20;
+		else if (length <= 50) return 50;
 		return 50;
 	};
 
 	renderContent = () => {
 		const { filteredData, isError, selectAll, checked } = this.state;
 		const dataWithIcon = clonedeep(filteredData);
-
 		const columns = [
 			// {
 			// 	Header: <input type="checkbox" onChange={this.handleChange} checked={selectAll} />,
@@ -178,7 +177,7 @@ class SiteMapping extends Component {
 		];
 
 		if (isError) return <CustomError />;
-		if (filteredData.length === 0) return <Empty message=" No Data found " />;
+		else if (filteredData.length === 0) return <Empty message=" No Data found " />;
 
 		return (
 			<ReactTable
@@ -224,8 +223,7 @@ class SiteMapping extends Component {
 	render() {
 		const { isLoading, filteredData, selectedData, fileName } = this.state;
 		if (isLoading) return <Loader height="600px" classNames="u-margin-v3" />;
-		let csvData;
-		let downloadLink;
+		let csvData, downloadLink;
 		try {
 			csvData = btoa(JSON.stringify(selectedData.length === 0 ? filteredData : selectedData));
 			downloadLink = `${REPORT_DOWNLOAD_ENDPOINT}?data=${csvData}&fileName=${fileName}`;
