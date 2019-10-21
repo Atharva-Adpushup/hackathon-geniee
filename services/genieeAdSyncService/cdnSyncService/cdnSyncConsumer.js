ef7137e37fd78b5f6ccaa466aa9014bc20a45cb4
Ravi Jagga <mr.ravi.jagga@gmail.com>
2019-10-21 15:31:38 +0530

    disbaled beta cdn syncing for india.com

--- services/genieeAdSyncService/cdnSyncService/cdnSyncConsumer.js
+++ services/genieeAdSyncService/cdnSyncService/cdnSyncConsumer.js
@@ -17,7 +17,7 @@ const prebidGeneration = require('./prebidGeneration');
 const isNotProduction =
 	config.environment.HOST_ENV === 'development' || config.environment.HOST_ENV === 'staging';
 const request = require('request-promise');
-const disableSiteCdnSyncList = [38333];
+const disableSiteCdnSyncList = [38333, 39468];
 // NOTE: Above 'disableSiteCdnSyncList' array is added to prevent site specific JavaScript CDN sync
 // as custom generated Javascript files will replace their existing live files for new feature testing purposes.
 // Websites: autocarindia (38333, It is running adpushup lite for which script is uploaded to CDN manually, for now)
