36198a3985c1e6e8e938be6141b0abe11af6b300
yash.garg <yash.garg@adpushup.com>
2019-12-26 13:03:46 +0530

    react table shared component

---
+++ Client/Components/CustomReactTable/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactTable from 'react-table';
+import PropTypes from 'prop-types';
+
+const CustomReactTable = ({
+	columns,
+	data,
+	pageSizeOptions,
+	defaultPageSize,
+	minRows,
+	sortable,
+	filterable
+}) => (
+	<div>
+		<div> Hi </div>
+	</div>
+);
+
+export default CustomReactTable;
