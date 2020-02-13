a72c639d8366d660f45fa46e6594e936d90c2689
RahulAdpushup <rahul.ranjan@adpushup.com>
2020-02-13 17:09:58 +0530

    [Fix] adManagerSyncService: added comments in the sample config file

--- services/adManagerSyncService/config.sample.js
+++ services/adManagerSyncService/config.sample.js
@@ -1,14 +1,17 @@
 module.exports = {
     appName: 'adManagerSyncService',
     dfpApiVersion: 'v201902',
-    serviceStatusPingDelayMs: 1000,
+    serviceStatusPingDelayMs: 1000, // this should not be changed
+    serviceStatusDocExpiryDays: 30, // expiration in days for status doc
+    // database where service will store its status doc
     serviceStatusDb: {
         host: 'localhost',
         port: 8091,
         username: 'admin',
         password: 'asd12345',
-        bucketName: 'AppBucket'
+        bucketName: 'AppBucket' // on staging and production this should be apLocalBucket, needs to be indexed
     },
+    // database where user docs are queried for active dfp network and ntwk docs will be stored
     db: {
         host: 'localhost',
         port: 8091,
@@ -16,13 +19,14 @@ module.exports = {
         password: 'asd12345',
         bucketName: 'AppBucket',
     },
+    // logging database
     logger: {
         db: {
             host: 'localhost',
             port: 8091,
             username: 'admin',
             password: 'asd12345',
-            bucketName: 'AppBucket'
+            bucketName: 'AppBucket' // this should be apGlobalBucket on staging and production
         },
         logExpiryDays: 30,
         serviceName: 'AdManagerSyncService'
