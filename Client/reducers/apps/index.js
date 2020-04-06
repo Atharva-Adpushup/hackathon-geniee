c819f75d0f5bee56edfa49b51b5b31a68ae1709f
yash.garg <yash.garg@adpushup.com>
2020-04-06 16:48:16 +0530

    amp hb ui with sample data

--- Client/reducers/apps/index.js
+++ Client/reducers/apps/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 import apTag from './apTag';
 import innovativeAds from './innovativeAds';
 import headerBidding from './headerBidding';
+import amp from './amp';
 
-export default combineReducers({ apTag, innovativeAds, headerBidding });
+export default combineReducers({ apTag, innovativeAds, headerBidding, amp });
