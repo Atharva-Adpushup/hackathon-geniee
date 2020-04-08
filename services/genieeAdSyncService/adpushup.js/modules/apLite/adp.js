f268bc2e0d8bda244596edfd3ffd9245ea8a69b9
AnilAdPushup <anil.panghal@adpushup.com>
2020-04-08 11:10:51 +0530

    updated min timeout value for HB refresh to be 0
    ADD - HB Analytics Suite (UI | Script changes | Test cases)
    REFACTOR - Get Slot Refresh Details function
    ADD - Prefix 'AP' to utils.log function

--- services/genieeAdSyncService/adpushup.js/modules/apLite/adp.js
+++ services/genieeAdSyncService/adpushup.js/modules/apLite/adp.js
@@ -173,6 +173,7 @@ var $ = require('../../libs/jquery'),
 												refreshSlot = apLiteAdUnit.refreshSlot,
 												refreshInterval = apLiteAdUnit.refreshInterval,
 												formats = apLiteAdUnit.formats,
+												adType = (apLiteAdUnit.formatData && apLiteAdUnit.formatData.type) || null,
 												adpSlot = this.createAdpSlot(
 													gptSlotElementId,
 													dfpAdUnitName,
