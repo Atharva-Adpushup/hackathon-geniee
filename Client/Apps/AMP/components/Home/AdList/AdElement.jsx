cdb2c1a3df9cbcc8d2de18f63ccb3059cea4eaaf
yash.garg <yash.garg@adpushup.com>
2020-04-15 17:09:25 +0530

    AMP Ads UI Modifiction and tried implementing dfp syncing(not working for now)

--- Client/Apps/AMP/components/Home/AdList/AdElement.jsx
+++ Client/Apps/AMP/components/Home/AdList/AdElement.jsx
@@ -42,6 +42,9 @@ class AdElement extends Component {
 
 	disableAd() {
 		const { isActive } = this.state;
+		const {
+			doc: { ad }
+		} = this.props;
 		const message = isActive
 			? 'Are you sure you want to archive this ad?'
 			: 'Are you sure you want to unarchive this ad?';
@@ -50,7 +53,7 @@ class AdElement extends Component {
 				{
 					isActive: !isActive
 				},
-				() => this.updateWrapper({ isActive: !isActive, archivedOn: +new Date() })
+				() => this.updateWrapper({ ad: { ...ad, isActive: !isActive, archivedOn: +new Date() } })
 			);
 		}
 	}
@@ -74,9 +77,21 @@ class AdElement extends Component {
 	);
 
 	renderAdDetails() {
-		const { updateAd, networkConfig, user, siteId, doc } = this.props;
+		const { updateAd, networkConfig, user, siteId, doc, networkCode } = this.props;
+		const {
+			dfpSyncingStatus: { completedOn }
+		} = doc;
+
 		const { ad, id, name } = doc;
-		const { width, height, isMultiSize, isRefreshEnabled, refreshInterval = 30, type } = ad;
+		const {
+			width,
+			height,
+			isMultiSize,
+			isRefreshEnabled,
+			refreshInterval = 30,
+			type,
+			networkData: { dfpAdunitCode }
+		} = ad;
 		const {
 			showLazyload,
 			showNetworkDetails,
@@ -103,6 +118,8 @@ class AdElement extends Component {
 					.replace(/__HEIGHT__/, height)
 					.replace(/__REFRESH_INTERVAL__/, refresh)
 					.replace(/__MULTI_SIZE__/, multiSize)
+					.replace(/__NETWORK_CODE__/, networkCode)
+					.replace(/__AD_UNIT_CODE__/, dfpAdunitCode)
 			: null;
 
 		// if (showNetworkDetails) {
@@ -211,7 +228,11 @@ class AdElement extends Component {
 						{this.renderInformation('Status', ad.isActive ? 'Active' : 'Archived')}
 					</div>
 				) : null}
-				<pre style={{ wordBreak: 'break-word' }}>{code}</pre>{' '}
+				<pre style={{ wordBreak: 'break-word' }}>
+					{dfpAdunitCode && completedOn
+						? code
+						: 'DFP Sync service is ruuning. Code will be available here once it is completed.'}
+				</pre>{' '}
 				{user.isSuperUser && ad.type !== 'amp' ? (
 					<React.Fragment>
 						<CustomButton
@@ -228,9 +249,11 @@ class AdElement extends Component {
 						>
 							Edit Refresh
 						</CustomButton>
-						<CopyButtonWrapperContainer content={code} className="u-margin-t3 pull-right">
-							<CustomButton variant="secondary">Copy AdCode</CustomButton>
-						</CopyButtonWrapperContainer>
+						{dfpAdunitCode && completedOn ? (
+							<CopyButtonWrapperContainer content={code} className="u-margin-t3 pull-right">
+								<CustomButton variant="secondary">Copy AdCode</CustomButton>
+							</CopyButtonWrapperContainer>
+						) : null}
 					</React.Fragment>
 				) : null}
 			</div>
