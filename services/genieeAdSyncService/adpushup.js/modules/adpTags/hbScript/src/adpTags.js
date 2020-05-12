67a6ee972eb17dcc2a481add691b6f18ad859bc5
AnilAdPushup <anil.panghal@adpushup.com>
2020-05-12 06:27:03 +0530

    [FIX] No sizes set on responsive units where defineSlot finishes before unit's xpath comes into DOM

--- services/genieeAdSyncService/adpushup.js/modules/adpTags/hbScript/src/adpTags.js
+++ services/genieeAdSyncService/adpushup.js/modules/adpTags/hbScript/src/adpTags.js
@@ -84,9 +84,7 @@ var adpTags = {
 			var slotId = optionalParam.dfpAdunit;
 			var isResponsive = optionalParam.isResponsive;
 			var sectionName = optionalParam.sectionName;
-			var computedSizes =
-				constants.AD_SIZE_MAPPING.IAB_SIZES.BACKWARD_COMPATIBLE_MAPPING[size.join(',')] ||
-				[];
+			// var computedSizes = constants.AD_SIZE_MAPPING.IAB_SIZES.BACKWARD_COMPATIBLE_MAPPING[size.join(',')] || [];
 			var services = optionalParam.services;
 			var formats =
 				(Array.isArray(optionalParam.formats) &&
@@ -102,13 +100,14 @@ var adpTags = {
 					: constants.PREBID.TIMEOUT;
 			var adType = optionalParam.adType;
 
+			/*
 			if (isResponsive) {
 				computedSizes = responsiveAds.getAdSizes(optionalParam.adId).collection;
 			}
 
 			computedSizes =
 				computedSizes && computedSizes.length ? computedSizes.concat([]).reverse() : size;
-
+			*/
 			var adpSlot = {
 				slotId: slotId,
 				optionalParam: optionalParam,
@@ -119,7 +118,7 @@ var adpTags = {
 				activeDFPNetwork: utils.getActiveDFPNetwork(),
 				size: size,
 				sectionName: sectionName,
-				computedSizes: computedSizes,
+				//computedSizes: computedSizes,
 				isResponsive: isResponsive,
 				containerId: containerId,
 				timeout: timeout,
@@ -138,16 +137,46 @@ var adpTags = {
 				refreshCount: 0
 			};
 
-			gpt.defineSlot(window.googletag, adpSlot);
+			this.computeSizesAndDefineSlot(adpSlot);
+
 			this.adpSlots[containerId] = adpSlot;
 
 			return this.adpSlots[containerId];
 		},
+		computeSizesAndDefineSlot: function(adpSlot) {
+			var computedSizes;
+			if (adpSlot.isResponsive) {
+				// for a responsive slot the ad container has to be in the DOM for size computation.
+				if (document.getElementById(adpSlot.containerId)) {
+					computedSizes = responsiveAds.getAdSizes(adpSlot.optionalParam.adId).collection;
+				} else {
+					// return without defining gpt slot since we can not compute applicable sizes
+					return;
+				}
+			} else {
+				computedSizes =
+					constants.AD_SIZE_MAPPING.IAB_SIZES.BACKWARD_COMPATIBLE_MAPPING[
+						adpSlot.size.join(',')
+					] || [];
+			}
+
+			adpSlot.computedSizes =
+				computedSizes && computedSizes.length
+					? computedSizes.concat([]).reverse()
+					: adpSlot.size;
+
+			gpt.defineSlot(window.googletag, adpSlot);
+		},
 		defineSlot: function(containerId, size, placement, optionalParam) {
 			var optionalParam = optionalParam || {};
 			var slot = this.createSlot(containerId, size, placement, optionalParam);
-
-			this.queSlotForBidding(slot);
+			/**
+			 * only case where computedSizes will not be set on slot is where it's a responsive slot and the ad container is not in the DOM
+			 * So, there is no point doing header bidding on this slot without known sizes
+			 */
+			if (slot.computedSizes) {
+				this.queSlotForBidding(slot);
+			}
 			return slot;
 		},
 		processQue: function() {
@@ -166,6 +195,15 @@ var adpTags = {
 				slot.pageGroup = adp.config.pageGroup;
 				slot.platform = adp.config.platform;
 
+				/**
+				 * only case where computedSizes will not be set on slot is where it's a responsive slot and the ad container was not in the DOM at the time of slot creation.
+				 * Now, at the time of display call, ad container must be in the DOM.
+				 * So, now we can compute the sizes and initiate header bidding on this slot.
+				 */
+				if (!slot.computedSizes) {
+					this.computeSizesAndDefineSlot(slot);
+					this.queSlotForBidding(slot);
+				}
 				gpt.renderSlot(window.googletag, slot);
 			}
 		}
