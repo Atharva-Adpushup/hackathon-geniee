1298eb1ad9435ca6d7c8e94951aedec45e47a85b
AnilAdPushup <anil.panghal@adpushup.com>
2020-06-25 18:47:26 +0530

    updated civic's cookie-control.js version to latest to support IAB TCF2.0

--- configs/commonConsts.js
+++ configs/commonConsts.js
@@ -122,7 +122,7 @@ module.exports = {
 		INACTIVE: 0
 	},
 	COOKIE_CONTROL_SCRIPT_TMPL:
-		'!function(){var o=document.createElement("script");o.src="https://cc.cdn.civiccomputing.com/8.0/cookieControl-8.0.min.js",document.head.appendChild(o);var n=setInterval(function(){window.CookieControl&&(clearInterval(n),CookieControl.load(__COOKIE_CONTROL_CONFIG__))},10)}();',
+		'!function(){var o=document.createElement("script");o.src="https://cc.cdn.civiccomputing.com/9/cookieControl-9.2.min.js",document.head.appendChild(o);var n=setInterval(function(){window.CookieControl&&(clearInterval(n),CookieControl.load(__COOKIE_CONTROL_CONFIG__))},10)}();',
 	GDPR: {
 		compliance: false,
 		cookieControlConfig: {
