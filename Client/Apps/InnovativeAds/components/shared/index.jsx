f4bf2320a16dc3762dceae55d390a6a23e42805d
Sriram R <sriramr083@gmail.com>
2020-07-02 18:22:33 +0530

    added placeholder for input in FormatOptions -> Innovative Ads

--- Client/Apps/InnovativeAds/components/shared/index.jsx
+++ Client/Apps/InnovativeAds/components/shared/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Col } from '@/Client/helpers/react-bootstrap-imports';
 import FieldGroup from '../../../../Components/Layout/FieldGroup';
 
-const CustomInput = ({ size, id, label, type, name, handler, value }) => (
+const CustomInput = ({ size, id, label, type, name, handler, value, placeholder }) => (
 	<Col md={size}>
 		<FieldGroup
 			id={id}
 			label={label}
 			type={type}
 			name={name}
-			placeholder={label}
+			placeholder={placeholder || label}
 			className="u-padding-v4 u-padding-h4"
 			onChange={handler}
 			value={value}
