cd560cd4a40225c1a24154fb25e70cbc74baaac5
Mayank Madan <maddiemadan@gmail.com>
2020-07-08 13:49:43 +0530

    Moved prebidServer host to configs/config.js

--- services/genieeAdSyncService/adpushup.js/modules/adpTags/hbScript/src/config.js
+++ services/genieeAdSyncService/adpushup.js/modules/adpTags/hbScript/src/config.js
@@ -1,4 +1,5 @@
 // Site config
+var globalConfig = require("../../../../../../../configs/config");
 
 var config = {
 	SITE_ID: __SITE_ID__,
@@ -8,8 +9,8 @@ var config = {
 	SIZE_MAPPING: __SIZE_MAPPING__,
 	S2S_CONFIG: {
 		S2S_ENABLED: true,
-		S2S_AUCTION_ENDPOINT: "https://amp.adpushup.com/prebidserver/openrtb2/auction",
-		S2S_SYNC_ENDPOINT: "https://amp.adpushup.com/prebidserver/cookie_sync",
+		S2S_AUCTION_ENDPOINT: `${globalConfig.prebidServer.host}prebidserver/openrtb2/auction`,
+		S2S_SYNC_ENDPOINT: `${globalConfig.prebidServer.host}prebidserver/cookie_sync`,
 		S2S_TIMEOUT: 5000,
 		S2S_ACCOUNT_ID: "1001"
 	}
