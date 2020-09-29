78fad4e48137c74ee1a783a151b04652f6b43247
harpreetAdPushUp <harpreet.singh@adpushup.com>
2020-09-29 17:11:01 +0530

    Add: backend node end points for gwtting utm data

--- apiControllers/urlReportsController.js
+++ apiControllers/urlReportsController.js
@@ -4,47 +4,88 @@ const CC = require('../configs/commonConsts');
 
 const router = express.Router();
 
-router.get('/getCustomStats', (req, res) => {
-	const {
-		query: {
-			isSuperUser = false,
-			fromDate,
-			toDate,
-			interval,
+const HTTP_STATUSES = require('../configs/httpStatusConsts');
+const { sendErrorResponse } = require('../helpers/commonFunctions');
+
+router
+	.get('/getCustomStats', (req, res) => {
+		const {
+			query: {
+				isSuperUser = false,
+				fromDate,
+				toDate,
+				interval,
+				// eslint-disable-next-line camelcase
+				page_size,
+				page,
+				siteid,
+				dimension
+			}
+		} = req;
+		const isValidParams = !!(
+			(siteid || isSuperUser) &&
+			fromDate &&
+			toDate &&
+			interval &&
 			// eslint-disable-next-line camelcase
-			top_select_criteria,
-			// eslint-disable-next-line camelcase
-			page_size,
-			page,
+			page_size &&
+			page &&
 			siteid
-		}
-	} = req;
-	const isValidParams = !!(
-		(siteid || isSuperUser) &&
-		fromDate &&
-		toDate &&
-		interval &&
-		// eslint-disable-next-line camelcase
-		top_select_criteria &&
-		// eslint-disable-next-line camelcase
-		page_size &&
-		page &&
-		siteid
-	);
+		);
 
-	if (isValidParams) {
-		return request({
-			uri: `https://staging.adpushup.com/CentralReportingWebService${CC.URL_REPORT_PATH}`,
-			json: true,
-			qs: req.query
-		})
-			.then(response => {
-				if (response.code === 1 && response.data) return res.send(response.data);
-				return res.send({});
+		const reportPath = dimension === 'url' ? CC.URL_REPORT_PATH : CC.UTM_REPORT_PATH;
+		if (isValidParams) {
+			return request({
+				uri: `https://staging.adpushup.com/CentralReportingWebService${reportPath}`,
+				json: true,
+				qs: req.query
 			})
-			.catch(err => res.send({ err }));
-	}
+				.then(response => {
+					if (response.code === 1 && response.data) return res.send(response.data);
+					return res.send({});
+				})
+				.catch(err => res.send({ err }));
+		}
 
-	return res.send({});
-});
+		return res.send({});
+	})
+	.get('/getMetaData', (req, res) => {
+		const {
+			query: { sites = '', isSuperUser = false, product }
+		} = req;
+		const isValidParams = !!(sites || isSuperUser);
+
+		if (isValidParams) {
+			const params = { siteid: sites, isSuperUser, product };
+
+			return request({
+				uri: `https://staging.adpushup.com/CentralReportingWebService${CC.ANALYTICS_METAINFO_URL}`,
+				json: true,
+				qs: params
+			})
+				.then(response => {
+					const { code = -1, data } = response;
+					if (code !== 1) return Promise.reject(new Error(response.data));
+					return response.code === 1 && data ? res.send(data) && data : res.send({});
+				})
+				.catch(err => {
+					const { message: errorMessage } = err;
+
+					const {
+						message = 'Something went wrong',
+						code = HTTP_STATUSES.INTERNAL_SERVER_ERROR
+					} = errorMessage;
+					return sendErrorResponse(
+						{
+							message
+						},
+						res,
+						code
+					);
+				});
+		}
+
+		return res.send({});
+	});
+
 module.exports = router;
