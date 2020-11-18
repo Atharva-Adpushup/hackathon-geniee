2048fc42a3d8497549392666a8efe073a9872dbd
amitdasgupta <amitdsgupta007@gmail.com>
2020-11-18 15:55:10 +0530

    Fixed Layout editor component title bug

--- Client/Apps/Layout/index.jsx
+++ Client/Apps/Layout/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import DocumentTitle from 'react-document-title';
 import Home from './components/Home/index';
 import '../../scss/apps/layout/index.scss';
 
-const App = props => <Home {...props} />;
-
+const App = props => (
+	<React.Fragment>
+		<DocumentTitle title="Layout Editor" />
+		<Home {...props} />
+	</React.Fragment>
+);
 export default App;
