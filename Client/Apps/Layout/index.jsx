2c745dec9309dcb2fd86aba9ecf25b52373aac67
amitdasgupta <amitdsgupta007@gmail.com>
2020-11-26 10:22:33 +0530

    Updated components to get title as a prop

--- Client/Apps/Layout/index.jsx
+++ Client/Apps/Layout/index.jsx
@@ -3,10 +3,13 @@ import DocumentTitle from 'react-document-title';
 import Home from './components/Home/index';
 import '../../scss/apps/layout/index.scss';
 
-const App = props => (
-	<React.Fragment>
-		<DocumentTitle title="Layout Editor" />
-		<Home {...props} />
-	</React.Fragment>
-);
+const App = props => {
+	const { title } = props;
+	return (
+		<React.Fragment>
+			<DocumentTitle title={title} />
+			<Home {...props} />
+		</React.Fragment>
+	);
+};
 export default App;
--- Client/Apps/Reporting/index.jsx
+++ Client/Apps/Reporting/index.jsx
@@ -4,11 +4,14 @@ import DocumentTitle from 'react-document-title';
 import ReportContainer from './containers/index';
 import '../../scss/apps/reporting/index.scss';
 
-const App = props => (
-	<React.Fragment>
-		<DocumentTitle title="Reporting" />
-		<ReportContainer {...props} />
-	</React.Fragment>
-);
+const App = props => {
+	const { title } = props;
+	return (
+		<React.Fragment>
+			<DocumentTitle title={title} />
+			<ReportContainer {...props} />
+		</React.Fragment>
+	);
+};
 
 export default App;
