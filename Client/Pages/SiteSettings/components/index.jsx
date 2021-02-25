9e795d1e17e29734b57f4947d8c78ab89f0b0367
Divyanshu Bhatnagar <divyanshu.bhatnagar@adpushup.com>
2021-02-25 15:33:28 +0530

    [ADD] - Crossorigin attribute to script tag that fetches adpushup.js to support Error Handler

--- Client/Pages/SiteSettings/components/index.jsx
+++ Client/Pages/SiteSettings/components/index.jsx
@@ -40,6 +40,7 @@ class SiteSettings extends Component {
 (function(w, d) {
 	var s = d.createElement('script');
 	s.src = '//cdn.adpushup.com/${siteId}/adpushup.js';
+	s.crossOrigin='anonymous'; 
 	s.type = 'text/javascript'; s.async = true;
 	(d.getElementsByTagName('head')[0] || d.getElementsByTagName('body')[0]).appendChild(s);
 })(window, document);
