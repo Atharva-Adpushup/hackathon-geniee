5031eff90640a7170a24da3091e39a4403f815f7
harpreetAdPushup <harpreet.singh@adpushup.com>
2021-03-09 12:03:54 +0530

    cron job setup code

--- services/crons/partnersPanelAnomalyDetectionCronService.js
+++ services/crons/partnersPanelAnomalyDetectionCronService.js
@@ -1,10 +1,14 @@
+var cron = require('node-cron');
+
+const CC = require('../../configs/commonConsts');
 const criteo = require('../partnersPanelAnomaliesDetectionService/Criteo');
 const OFT = require('../partnersPanelAnomaliesDetectionService/OFT');
 const Pubmatic = require('../partnersPanelAnomaliesDetectionService/Pubmatic');
 const IndexExchange = require('../partnersPanelAnomaliesDetectionService/IndexExchange');
 const Sovrn = require('../partnersPanelAnomaliesDetectionService/Sovrn');
 const { appBucket } = require('../../helpers/routeHelpers');
-const constants = require('../../configs/commonConsts');
+// const constants = require('../../configs/commonConsts');
+const emailer = require('../partnersPanelAnomaliesDetectionService/emailer');
 
 function getSitesFromDB() {
 	// select distinct siteId, siteDomain from AppBucket where meta().id like "site::%";
@@ -25,29 +29,40 @@ function getSitesFromDB() {
 	return siteListPromise;
 }
 
-getSitesFromDB()
-	.then(sitesData => {
-		return Promise.all([
-			// criteo(sitesData)
-			OFT(sitesData)
-			// Pubmatic(sitesData)
-			// IndexExchange(sitesData)
-			// Sovrn(sitesData)
-		])
-		.catch(err => {
-			console.log(err, 'err in service');
-		});
-	})
-	.then(result => {
-		if (result instanceof Error) {
-			console.error(result);
+function startParntersPanelsAnomaliesDetectionService() {
+	getSitesFromDB()
+		.then(sitesData => {
+			return Promise.all([
+				// criteo(sitesData),
+				OFT(sitesData),
+				// Pubmatic(sitesData),
+				// IndexExchange(sitesData),
+				// Sovrn(sitesData)
+			])
+			.catch(err => {
+				console.log(err, 'err in service');
+			});
+		})
+		.then(result => {
+			if (result instanceof Error) {
+				console.error(result);
+				process.exit(1);
+			} else {
+				console.log(result, 'result in main index.js');
+				process.exit(0);
+			}
+		})
+		.catch(async err => {
+			console.error(err);
+			await emailer.serviceErrorNotificationMailService({
+				partner: 'Main Service',
+				error:err
+			})
+
 			process.exit(1);
-		} else {
-			console.log(result, 'result in main index.js');
-			process.exit(0);
-		}
-	})
-	.catch(err => {
-		console.error(err);
-		process.exit(1);
-	});
+		});
+}
+
+startParntersPanelsAnomaliesDetectionService();
+// const JOB_NAME = 'AnomaliesDetection';
+// cron.schedule(JOB_NAME, CC.cronSchedule.partnerPanelAnomaliesDetectionService, startParntersPanelsAnomaliesDetectionService);
