bff23c63d622ff467718123da6c5c42472535aba
harpreetAdPushup <harpreet.singh@adpushup.com>
2021-05-05 14:19:25 +0530

    API wrapper and detection for OpenX, Criteo, OFT, Pubmatic, IX and partial Sovrn

---
+++ services/partnersPanelAnomaliesDetectionService/CustomError.js
@@ -0,0 +1,8 @@
+function CustomError(message) {
+	this.name = 'CustomError';
+	this.message = message;
+	this.stack = new Error().stack;
+}
+CustomError.prototype = new Error();
+
+module.exports = CustomError;
