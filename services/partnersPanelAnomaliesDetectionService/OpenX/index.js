abd7e977f9a74704b61e45421f97e0773b2b34b0
harpreetAdPushup <harpreet.singh@adpushup.com>
2021-08-17 13:08:20 +0530

    remove: unwanted package fs

--- services/partnersPanelAnomaliesDetectionService/OpenX/index.js
+++ services/partnersPanelAnomaliesDetectionService/OpenX/index.js
@@ -4,7 +4,6 @@ var moment = require('moment-timezone');
 const OAuth = require('oauth-1.0a');
 const crypto = require('crypto');
 const util = require('util');
-var fs = require('fs');
 
 const requestPromise = util.promisify(request);
 const partnerAndAdpushpModel = require('../PartnerAndAdpushpModel');
