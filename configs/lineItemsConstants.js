61125a00a4f7ba38dda6710dae05ef0c6fe66f81
Mayank Madan <mayank.madan@adpushup.com>
2021-09-17 14:37:14 +0530

    Added comments in lineitemconstants

--- configs/lineItemsConstants.js
+++ configs/lineItemsConstants.js
@@ -1,5 +1,9 @@
 // Used in client as well
-// isDisabled is for 
+// Flags:
+// isDisabled: If set to true, type would not be visible in UI
+// isMandatory: If set to true, the type will be become mandatory and auto enabled network wide
+// toBeFetchedFromGAM: If set to true, the type will be fetched from GAM by adManagerSyncService. Its set to false for line item types like custom and header bidding (custom types)
+// groupedSeparately: If set to true, its line items would not be in adpushup.config.lineItems but they would be grouped separately in adpushup.config.separatelyGroupedLineItems
 
 const LINE_ITEM_TYPES = [
     {
@@ -64,7 +68,7 @@ const LINE_ITEM_TYPES = [
         name: 'Header Bidding',
         value: 'HEADER_BIDDING', // can be added in ntwk doc manually for any manual addition of line items
         key: 'HEADER_BIDDING',
-        isDisabled: true, // Its disabled so that its not visible in UI. It will be added to the lineitem list in generateAdNetworkConfig
+        isDisabled: true, // Its disabled so that its not visible in UI. It will be added to the separatelyGroupedLineItems in generateAdNetworkConfig
         isMandatory: true,
         groupedSeparately: true,
         toBeFetchedFromGAM: false, // These are fetched in PRICE_PRIORITY
