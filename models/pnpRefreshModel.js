a87e4c872db2a86f1ecc00a86d7656349b0afa9f
Rishav Chaudhary <50170240+rishavch2104@users.noreply.github.com>
2021-10-08 14:42:33 +0530

    fixed validation issues

--- models/pnpRefreshModel.js
+++ models/pnpRefreshModel.js
@@ -1,79 +1,58 @@
+
 const model = require('../helpers/model');
 const couchbase = require('../helpers/couchBaseService');
 const { docKeys } = require('../configs/commonConsts');
 
 const PnP = model.extend(function() {
-	this.keys = [
-		'siteId',
-		'pnpSiteId',
-		'adUnits',
-		'lineItems',
-		'native',
-		'outstream',
-		'filledInsertionTrigger',
-		'unfilledInsertionTrigger',
-		'blacklistedLineItems',
-		'refreshType',
-		'formats'
-	];
-	this.clientKeys = [
-		'siteId',
-		'pnpSiteId',
-		'adUnits',
-		'lineItems',
-		'blacklistedLineItems',
-		'native',
-		'outstream',
-		'filledInsertionTrigger',
-		'unfilledInsertionTrigger',
-		'refreshType',
-		'formats'
-	];
 
-	this.validations = {
-		required: ['siteId', 'pnpSiteId', 'adUnits', 'refreshType']
-	};
+    this.keys = ['siteId', 'pnpSiteId', 'adUnits', 'lineItems','blacklistedLineItems', 'native', 'outstream', 'filledInsertionTrigger', 'unfilledInsertionTrigger', 'refreshType', 'formats'];
+    this.clientKeys = ['siteId', 'pnpSiteId', 'adUnits', 'lineItems', 'blacklistedLineItems','native', 'outstream', 'filledInsertionTrigger', 'unfilledInsertionTrigger', 'refreshType', 'formats'];
 
-	this.classMap = {};
-	this.defaults = {
-		filledInsertionTrigger: 30,
-		unfilledInsertionTrigger: 30,
-		adUnits: [],
-		lineItems: [],
-		blacklistedLineItems: [],
-		refreshType: 'activeTab',
-		native: false,
-		outstream: false
-	};
+    this.validations = {
+        required: ['siteId', 'pnpSiteId', 'adUnits', 'refreshType']
+    }
 
-	this.constructor = function(data, cas) {
-		if (!data.siteId) throw new Error('PnP model requires siteId');
-		this.key = `${docKeys.pnpRefresh}${data.siteId}`;
-		this.super(data, cas ? true : false);
-		this.casValue = cas;
-	};
+    this.classMap = {};
+    this.defaults = {
+        filledInsertionTrigger: 30,
+        unfilledInsertionTrigger: 30,
+        adUnits: [],
+        lineItems: [],
+		blacklistedLineItems:[],
+        refreshType: 'activeTab',
+        native: false,
+        outstream: false
+    };
+
+    this.constructor = function(data, cas) {
+        if (!data.siteId) throw new Error('PnP model requires siteId');
+        this.key = `${docKeys.pnpRefresh}${data.siteId}`;
+        this.super(data, cas ? true: false);
+        this.casValue = cas;
+    }
+
 });
 
 const API = {
-	getPnPConfig: siteId =>
-		couchbase
-			.connectToAppBucket()
-			.then(appBucket => appBucket.getAsync(`${docKeys.pnpRefresh}${siteId}`))
-			.then(({ value, cas }) => new PnP(value, cas)),
-	upsertPnPConfig: (siteId, config) => {
-		return API.getPnPConfig(siteId)
-			.then(pnpConfig => {
-				pnpConfig.setAll(config);
-				return pnpConfig.save();
-			})
-			.catch(err => {
-				if (err && err.code === 13) {
-					const pnp = new PnP({ ...config, siteId });
-					return pnp.save();
-				}
-				throw new Error(err);
-			});
-	}
-};
+    getPnPConfig: siteId =>
+        couchbase
+            .connectToAppBucket()
+            .then(appBucket => appBucket.getAsync(`${docKeys.pnpRefresh}${siteId}`))
+            .then(({value, cas}) => new PnP(value, cas)),
+    upsertPnPConfig: (siteId, config) => {
+        return API.getPnPConfig(siteId)
+            .then(pnpConfig => {
+                pnpConfig.setAll(config);
+                return pnpConfig.save();
+            })
+            .catch(err => {
+                if (err && err.code === 13) {
+                    const pnp = new PnP({ ...config, siteId });
+                    return pnp.save();
+                }
+                throw new Error(err);
+            });
+    }
+}
 
 module.exports = API;
--- validations/opsValidations.js
+++ validations/opsValidations.js
@@ -1,41 +1,41 @@
 const Yup = require('yup');
 
 const adUnitValidationSchema = Yup.object().shape({
-	name: Yup.string().required(),
-	code: Yup.string().required(),
-	isActive: Yup.boolean(),
-	platform: Yup.string()
-		.oneOf(['DESKTOP', 'MOBILE', 'TABLET'])
-		.required(),
-	width: Yup.string().required(),
-	height: Yup.string().required()
+    name: Yup.string().required(),
+    code: Yup.string().required(),
+    isActive: Yup.boolean(),
+    platform: Yup.string().oneOf(['DESKTOP', 'MOBILE', 'TABLET']).required(),
+    width: Yup.string().required(),
+    height: Yup.string().required()
 });
 
 const lineItemValidationSchema = Yup.object().shape({
-	id: Yup.string().required()
+    id: Yup.string().required(),
 });
 
 const pnpConfigValidation = {
-	siteId: Yup.string().required('SiteId is requird'),
-	pnpSiteId: Yup.string().required('PnP siteId is required'),
-	adUnits: Yup.array()
-		.of(adUnitValidationSchema)
-		.min(1),
-	lineItems: Yup.array().of(lineItemValidationSchema),
-	blacklistedLineItems: Yup.array().of(lineItemValidationSchema),
-	native: Yup.boolean(),
-	outstream: Yup.boolean(),
-	filledInsertionTrigger: Yup.number()
-		.min(30)
-		.optional(),
-	unfilledInsertionTrigger: Yup.number()
-		.min(5)
-		.optional(),
-	refreshType: Yup.string()
-		.oneOf(['activeView', 'activeTab', 'bgRefresh'])
-		.required()
+    siteId: Yup
+        .string()
+        .required('SiteId is requird'),
+    pnpSiteId: Yup
+        .string()
+        .required('PnP siteId is required'),
+    adUnits: Yup
+        .array().of(adUnitValidationSchema)
+        .min(1),
+    lineItems: Yup
+        .array()
+        .of(lineItemValidationSchema),
+	blacklistedLineItems: Yup
+        .array()
+        .of(lineItemValidationSchema),	
+    native: Yup.boolean(),
+    outstream: Yup.boolean(),
+    filledInsertionTrigger: Yup.number().min(30).optional(),
+    unfilledInsertionTrigger: Yup.number().min(5).optional(),
+    refreshType: Yup.string().oneOf(['activeView', 'activeTab']).required()
 };
 
 module.exports = {
-	pnpConfigValidation
-};
+    pnpConfigValidation
+} 
