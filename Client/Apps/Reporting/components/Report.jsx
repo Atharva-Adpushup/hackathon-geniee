1759b87de2301efc956f3955466191fba4c98ee2
Sumant Sharma <vinodsharma@Sumant-Sharma.local>
2023-12-21 16:58:46 +0530

    updated a try ctach block to address the issue of getControlChangedParams
    version bump

--- Client/Apps/Reporting/components/Report.jsx
+++ Client/Apps/Reporting/components/Report.jsx
@@ -42,6 +42,7 @@ import {
 	REPORT_TYPES
 } from '../configs/commonConsts';
 import MixpanelHelper from '../../../helpers/mixpanel';
+import CustomError from '../../../Components/CustomError/index';
 
 class Report extends Component {
 	constructor(props) {
@@ -286,43 +287,49 @@ class Report extends Component {
 	};
 
 	getControlChangedParams = (controlParams, metricsList) => {
-		const { selectedDimension = [], selectedFilters, reportType } = controlParams;
-		const { reportsMeta } = this.props;
-		const { dimension: dimensionList, filter: filterList } = reportsMeta.data;
-		let disabledFilter = [];
-		let disabledDimension = [];
-		let disabledMetrics = [];
-		for (const dimension of selectedDimension) {
-			const dimensionObj = dimensionList[dimension];
+		try {
+			const { selectedDimension = [], selectedFilters, reportType } = controlParams;
+			const { reportsMeta } = this.props;
+			const { dimension: dimensionList, filter: filterList } = reportsMeta.data;
+			let disabledFilter = [];
+			let disabledDimension = [];
+			let disabledMetrics = [];
+			for (const dimension of selectedDimension) {
+				const dimensionObj = dimensionList[dimension];
 
-			if (dimensionObj) {
-				disabledFilter = dimensionObj.disabled_filter || disabledFilter;
-				disabledDimension = dimensionObj.disabled_dimension || disabledDimension;
-				disabledMetrics = dimensionObj.disabled_metrics || disabledMetrics;
+				if (dimensionObj) {
+					disabledFilter = dimensionObj.disabled_filter || disabledFilter;
+					disabledDimension = dimensionObj.disabled_dimension || disabledDimension;
+					disabledMetrics = dimensionObj.disabled_metrics || disabledMetrics;
+				}
 			}
+			Object.keys(selectedFilters).forEach(selectedFilter => {
+				const filterObj = filterList[selectedFilter];
+				if (filterObj && !isEmpty(selectedFilters[selectedFilter])) {
+					disabledFilter = union(filterObj.disabled_filter, disabledFilter);
+					disabledDimension = union(filterObj.disabled_dimension, disabledDimension);
+					disabledMetrics = union(filterObj.disabled_metrics, disabledMetrics);
+				}
+			});
+
+			const updatedControlList = this.disableControl(
+				disabledFilter,
+				disabledDimension,
+				disabledMetrics,
+				metricsList,
+				reportType
+			);
+
+			return {
+				dimensionList: updatedControlList.updatedDimensionList,
+				filterList: updatedControlList.updatedFilterList,
+				metricsList: updatedControlList.metricsList
+			};
+		} catch (err) {
+			const { reportsMeta } = this.props;
+			const issue = 'reporting siteid issue';
+			throw new CustomError(err, { controlParams, reportsMeta, metricsList, issue });
 		}
-		Object.keys(selectedFilters).forEach(selectedFilter => {
-			const filterObj = filterList[selectedFilter];
-			if (filterObj && !isEmpty(selectedFilters[selectedFilter])) {
-				disabledFilter = union(filterObj.disabled_filter, disabledFilter);
-				disabledDimension = union(filterObj.disabled_dimension, disabledDimension);
-				disabledMetrics = union(filterObj.disabled_metrics, disabledMetrics);
-			}
-		});
-
-		const updatedControlList = this.disableControl(
-			disabledFilter,
-			disabledDimension,
-			disabledMetrics,
-			metricsList,
-			reportType
-		);
-
-		return {
-			dimensionList: updatedControlList.updatedDimensionList,
-			filterList: updatedControlList.updatedFilterList,
-			metricsList: updatedControlList.metricsList
-		};
 	};
 
 	formateReportParams = () => {
